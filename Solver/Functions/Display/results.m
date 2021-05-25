f128e98462de5584b1b1d861a23c4cf5bf34376f
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-05-25 11:18:25 +0200

    Added function display results

---
+++ Solver/Functions/Display/results.m
@@ -0,0 +1,7 @@
+function results(app, i)
+    if ~strcmp(app.PD.ProblemType,'SHOCK_R') && ~strcmp(app.PD.ProblemType,'DET_OVERDRIVEN')
+        displayresults(app.PS.strR{i},app.PS.strP{i},app.PD.ProblemType,app.C.mintol_display,app.S.namespecies);
+    elseif ~strcmp(app.PD.ProblemType,'DET_OVERDRIVEN')
+        displayresults(app.PS.strR{i},app.PS.str2{i},app.PS.strP{i},app.PD.ProblemType,app.C.mintol_display,app.S.namespecies); % Display all results SHOCK_R
+    end
+end

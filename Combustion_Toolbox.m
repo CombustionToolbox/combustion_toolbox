0105073d5ed46b4229bb5e8d6035d4dd6fd34e90
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-05-27 21:33:27 +0200

    Miscelaneous

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -18,29 +18,34 @@
 % * Marcos Vera Coello,  Universidad Carlos III de Madrid (UC3M)
 %
 % Last update: 25-May-2021 10:27
-%% LOAD DATABASES, GLOBAL PARAMETERS AND MINORS PRODUCTS
+%% INITIALIZATION
 addpath(genpath(pwd));
 % app = App();
 % app = App('Soot formation');
-app = App('HC/02/N2 EXTENDED');
+% app = App('HC/02/N2 extended');
+app = App('HC/02/N2 rich');
 % app = App('Hydrogen');
-% app = App('NASA ALL');
+% app = App('Nasa all');
 % app = App('Cbgrb'); 
-% app = App('AIR'); 
-app.Misc.save_Excel = false;
+% app = App('air'); 
 %% REACTION: COMPLETE OR INCOMPLETE
 % app.PD.CompleteOrIncomplete = 'complete';
-app.PD.CompleteOrIncomplete = 'incomplete'; 
+app.PD.CompleteOrIncomplete = 'incomplete';
+%% TUNNING AND MISCELANEOUS PARAMETERS
+app.Misc.save_Excel = false;
 app.TN.factor_c = 1; % factor_c = 1 (default).
-%% SOLVER: SEGREGATED OR GIBBS_MIN
-app.PD.FLAG_GIBBS = true; % false: segregated; true: gibbs minimization
+%% SOLVER: SEGREGATED, GIBBS OR GIBBS_SOOT
+app.PD.method = 'SEGREGATED';
+% app.PD.method = 'GIBBS';
+% app.PD.method = 'GIBBS_SOOT';
 %% CHECK SPECIES MINOR PRODUCTS --> SELECTED DATABASE (strThProp)
 [app.strThProp,app.E,app.S,app.M,app.C] = check_database(app.strMaster,app.strThProp,app.E,app.S,app.M,app.C);
 %% PROBLEM CONDITIONS
 app.PD.TR.value = 300;
 % app.PD.TR.vector.value = 300:50:700;
 app.PD.pR.value = 1;
-app.PD.phi.value = 0.5:0.01:2;
+app.PD.phi.value = 0.5:0.01:4.5;
+% app.PD.phi.value = 3;
 %% INITIALIZATION
 app = Initialize(app);
 %% PROBLEM TYPE
@@ -54,7 +59,7 @@ switch app.PD.ProblemType
         % app.PD.pR.value = logspace(0,2,20); app.PD.phi.value = 1*ones(1,length(app.PD.pR.value));
     case 'SP' % * SP: Isentropic (i.e., adiabatic) compression/expansion to a specified p
         app.PD.ProblemType = 'SP';
-        app.PD.pP.value = 10:10:50; app.PD.phi.value = 1*ones(1,length(app.PD.pP.value));
+        app.PD.pP.value = 10:1:50; app.PD.phi.value = 1*ones(1,length(app.PD.pP.value));
 %         app.PD.pP.value = 10*ones(1,length(app.PD.phi.value));
     case 'TV' % * TV: Equilibrium composition at defined T and constant v
         app.PD.ProblemType = 'TV';
@@ -66,7 +71,7 @@ switch app.PD.ProblemType
     case 'SV' % * SV: Isentropic (i.e., fast adiabatic) compression/expansion to a specified v
         app.PD.ProblemType = 'SV';
         % REMARK! vP_vR > 1 --> expansion, vP_vR < 1 --> compression
-        app.PD.vP_vR.value = 0.5:0.1:2; app.PD.phi.value = 1*ones(1,length(app.PD.vP_vR.value));
+        app.PD.vP_vR.value = 0.5:0.01:2; app.PD.phi.value = 1*ones(1,length(app.PD.vP_vR.value));
     case 'SHOCK_I' % * SHOCK_I: CALCULATE PLANAR INCIDENT SHOCK WAVE
         app.PD.ProblemType = 'SHOCK_I';
         app.PD.u1.value = 400:200:2000; app.PD.phi.value = ones(1,length(app.PD.u1.value));
@@ -86,11 +91,12 @@ tic
 for i=app.C.l_phi:-1:1 % Evading preallocate struct
 %% DEFINE FUEL
 % app.PD.S_Fuel = {'CH4','C2H6','C3H8'}; app.PD.N_Fuel = [0.85;0.1;0.05]; 
-app.PD.S_Fuel = {'CH4'}; app.PD.N_Fuel = 1;
-% app.PD.S_Fuel = {'C2H2_acetylene'}; app.PD.N_Fuel = 1; 
+% app.PD.S_Fuel = {'CH4'}; app.PD.N_Fuel = 1;
+app.PD.S_Fuel = {'C2H2_acetylene'}; app.PD.N_Fuel = 1; 
 % app.PD.S_Fuel = {'C3H8'}; app.PD.N_Fuel = 1;    
 % app.PD.S_Fuel = {'C6H6'}; app.PD.N_Fuel = 1; 
 % app.PD.S_Fuel = {'C2H4'}; app.PD.N_Fuel = 1;
+% app.PD.S_Fuel = {'H2'}; app.PD.N_Fuel = 1;
 
 app = Define_F(app);
 %% DEFINE OXIDIZER
@@ -110,7 +116,8 @@ end
 %% DISPLAY MOLAR FRACTION VS EQUIVALENCE RATIO
 app.M.display_species = {};
 % app.M.display_species = {'CO','CO2','H','HO2','H2','H2O','N2','O2'};
-% app.M.display_species = {'CO','CO2','H','HO2','H2','H2O','NO','NO2','N2','O','OH','O2','Cbgrb'};
-closing(app,app.PS.strP,app.PD.phi.value,app.M.display_species,app.Misc.timer_0,app.S.LS,app.C.mintol_display,app.PD.ProblemType);
+app.M.display_species = {'CO','CO2','H','HO2','H2','H2O','NO','NO2','N2','O','OH','O2','Cbgrb'};
+% app.M.display_species = {'Cbgrb','CO2','CO','HNC','HCN','H2','OH','H2O','O2','CH4'};
+closing(app);
 %% EXCEL I/O
 ExportExcel(app);

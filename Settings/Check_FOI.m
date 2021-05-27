55ef2446e1a0bcfea6179335636442a8168e7430
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-05-27 21:27:58 +0200

    Added function Check_FOI

---
+++ Settings/Check_FOI.m
@@ -0,0 +1,20 @@
+function app = Check_FOI(app, FOI_species)
+    if app.Misc.FLAG_FOI
+        app.Misc.FLAG_FOI = false;
+        FLAG = false;
+        for i=1:numel(FOI_species)
+            if ~strcmp(app.S.LS, FOI_species(i)) 
+                app.M.minors_products = [app.M.minors_products, FOI_species(i)];
+                app.M.ind_minor = [app.M.ind_minor, app.M.ind_minor(end) + 1];
+                app.M.Lminors = numel(app.M.minors_products);
+                FLAG = true;
+            end
+        end
+        if FLAG
+            app.S.ind_nswt = [];
+            app.S.ind_swt = [];
+            app = Initialize(app);
+        end
+        app.Misc.FLAG_FIRST = false;
+    end
+end

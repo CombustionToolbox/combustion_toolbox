c98bd85e773606535ccdd4000b988766a138ffdf
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-05-28 11:41:47 +0200

    Fixed bug no minors species

--- Settings/Check_FOI.m
+++ Settings/Check_FOI.m
@@ -3,9 +3,13 @@ function app = Check_FOI(app, FOI_species)
         app.Misc.FLAG_FOI = false;
         FLAG = false;
         for i=1:numel(FOI_species)
-            if ~strcmp(app.S.LS, FOI_species(i)) 
+            if ~strcmp(app.S.LS, FOI_species(i))                 
                 app.M.minors_products = [app.M.minors_products, FOI_species(i)];
-                app.M.ind_minor = [app.M.ind_minor, app.M.ind_minor(end) + 1];
+                try 
+                    app.M.ind_minor = [app.M.ind_minor, app.M.ind_minor(end) + 1];
+                catch
+                    app.M.ind_minor = 1;
+                end
                 app.M.Lminors = numel(app.M.minors_products);
                 FLAG = true;
             end

bf553fcb21b4e277c38261e6bd90b1a6ae301849
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-06-02 10:02:29 +0200

    Added compatibility with GUI

--- Settings/self/App/Initialize.m
+++ Settings/self/App/Initialize.m
@@ -144,7 +144,7 @@ function self = OH_major(self)
 end
 
 function PT = Ask_problem(self)
-    if self.Misc.FLAG_FOI
+    if self.Misc.FLAG_FOI && ~self.Misc.FLAG_GUI
         try
         fn = {'TP','HP','SP','TV','EV','SV','SHOCK_I','SHOCK_R','DET','DET_OVERDRIVEN'};
         [indx, ~] = listdlg('PromptString','Select a problem:',...
@@ -155,6 +155,34 @@ function PT = Ask_problem(self)
             error('Problem type not selected.')
         end
     else
+        if self.Misc.FLAG_GUI
+            self = get_PT_GUI(self);
+        end
         PT = self.PD.ProblemType;
+    end
+end
+
+function self = get_PT_GUI(self)
+    switch self.ProblemType.Value
+        case '1' % * TP: Equilibrium composition at defined T and p
+            self.PD.ProblemType = 'TP';
+        case '2' % * HP: Adiabatic T and composition at constant p
+            self.PD.ProblemType = 'HP';
+        case '3' % * SP: Isentropic (i.e., adiabatic) compression/expansion to a specified p
+            self.PD.ProblemType = 'SP';
+        case '4' % * TV: Equilibrium composition at defined T and constant v
+            self.PD.ProblemType = 'TV';  
+        case '5' % * EV: Equilibrium composition at Adiabatic T and constant v
+            self.PD.ProblemType = 'EV';
+        case '6' % * SV: Isentropic (i.e., fast adiabatic) compression/expansion to a specified v
+            self.PD.ProblemType = 'SV';
+        case '7' % * SHOCK_I: CALCULATE PLANAR INCIDENT SHOCK WAVE
+            self.PD.ProblemType = 'SHOCK_I';
+        case '8' % * SHOCK_R: CALCULATE PLANAR POST-REFLECTED SHOCK STATE
+            self.PD.ProblemType = 'SHOCK_R';
+        case '9' % * DET: CALCULATE CHAPMAN-JOUGET STATE (CJ UPPER STATE)
+            self.PD.ProblemType = 'DET';
+        case '10' % * DET: CALCULATE OVERDRIVEN CHAPMAN-JOUGET STATE (CJ UPPER STATE)
+            self.PD.ProblemType = 'DET_OVERDRIVEN';
     end
 end

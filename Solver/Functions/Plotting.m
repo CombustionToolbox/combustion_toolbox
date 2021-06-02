6e9878eb6ccc86f5797e205ab2fd8ab674ec0319
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-06-02 10:05:02 +0200

    Added new nomenclature

--- Solver/Functions/Plotting.m
+++ Solver/Functions/Plotting.m
@@ -9,18 +9,18 @@ function Plotting(app,display_species,flag,flag_TP)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % help Plotting
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if numel(app.PD.phi.Value)>1 && all(app.PD.phi.Value(2:end) ~= app.PD.phi.Value(1)) && flag
+if numel(app.PD.phi.value)>1 && all(app.PD.phi.value(2:end) ~= app.PD.phi.value(1)) && flag
     if isempty(display_species)
-        displaysweepresults(app.PS.strP,app.PD.phi.Value,app.S.NameSpecies,app.C.mintol_display,'Equivalence Ratio $\phi$');
+        displaysweepresults(app.PS.strP,app.PD.phi.value,app.S.NameSpecies,app.C.mintol_display,'Equivalence Ratio $\phi$');
     else
-        displaysweepresults(app.PS.strP,app.PD.phi.Value,app.S.NameSpecies,app.C.mintol_display,'Equivalence Ratio $\phi$',display_species);
+        displaysweepresults(app.PS.strP,app.PD.phi.value,app.S.NameSpecies,app.C.mintol_display,'Equivalence Ratio $\phi$',display_species);
     end
     if ~any(strcmp(app.ProblemType.Value,{'1','4'}))
         app.Misc.config.tit = app.Reactants.Items{sscanf(app.Reactants.Value,'%d')};
         app.Misc.config.labelx = 'Equivalence Ratio $\phi [-]$';
         app.Misc.config.labely = 'Temperature $T [K]$';
-        plot_figure(app.PD.phi.Value,app.PS.strP,'phi','T',app.Misc.config,app.Reaction.Items{sscanf(app.Reaction.Value,'%d')});
-%         display_Tp_phi(app.PS.strP,app.PD.phi.Value,app.Reactants.Items{sscanf(app.Reactants.Value,'%d')},app.Reaction.Items{sscanf(app.Reaction.Value,'%d')});
+        plot_figure(app.PD.phi.value,app.PS.strP,'phi','T',app.Misc.config,app.Reaction.Items{sscanf(app.Reaction.Value,'%d')});
+%         display_Tp_phi(app.PS.strP,app.PD.phi.value,app.Reactants.Items{sscanf(app.Reactants.Value,'%d')},app.Reaction.Items{sscanf(app.Reaction.Value,'%d')});
     end
 end
 if flag_TP

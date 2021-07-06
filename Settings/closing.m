043d94e74a8ffe087597f2422a1f846ef4a8c1c8
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-07-06 18:43:56 +0200

    Rewritten function

--- Settings/closing.m
+++ Settings/closing.m
@@ -15,30 +15,31 @@ toc(timer_0);
 app.Misc.config.tit = ProblemType;
 
 if numel(phi)>1 && all(phi(2:end) ~= phi(1)) && ~strcmp(ProblemType,'DET_OVERDRIVEN')
-    if isempty(display_species) 
-        displaysweepresults(strP,phi,LS,mintol_display,'Equivalence Ratio $\phi$');
+    app.Misc.config.labelx = 'Equivalence Ratio $\phi$';
+    app.Misc.config.labely = 'Molar fraction $X_i$';
+    if isempty(display_species)
+        displaysweepresults(strP, phi, LS, mintol_display, app.Misc.config);
     else
-        displaysweepresults(strP,phi,LS,mintol_display,'Equivalence Ratio $\phi$',display_species);
+        displaysweepresults(strP, phi, LS, mintol_display, app.Misc.config, display_species);
     end
     if ~any(strcmp(ProblemType,{'TP','TV'}))
-        app.Misc.config.labelx = 'Equivalence Ratio $\phi [-]$';
         app.Misc.config.labely = 'Temperature $T [K]$';
-        plot_figure(app.PD.phi.value,app.PS.strP,'phi','T',app.Misc.config,app.PD.CompleteOrIncomplete);
+        plot_figure(phi,strP,'phi','T',app.Misc.config,app.PD.CompleteOrIncomplete);
     end
 elseif any(strcmp(ProblemType,{'SP'}))
         app.Misc.config.labelx = 'Pressure $p [bar]$';
         app.Misc.config.labely = 'Temperature $T [K]$';
-        plot_figure(app.PD.pP.value,app.PS.strP,'p','T',app.Misc.config,app.PD.CompleteOrIncomplete);
+        plot_figure(app.PD.pP.value,strP,'p','T',app.Misc.config,app.PD.CompleteOrIncomplete);
 elseif any(strcmp(ProblemType,{'SV'}))
         app.Misc.config.labelx = 'Volume ratio $v_P/v_R$';
         app.Misc.config.labely = 'Temperature $T [K]$';
-        plot_figure(app.PD.vP_vR.value,app.PS.strP,'v_P/v_R','T',app.Misc.config,app.PD.CompleteOrIncomplete);     
+        plot_figure(app.PD.vP_vR.value,strP,'v_P/v_R','T',app.Misc.config,app.PD.CompleteOrIncomplete);     
 elseif any(strcmp(ProblemType,{'SHOCK_I'}))
         app.Misc.config.labelx = 'Incident velocity $u_1$';
         app.Misc.config.labely = 'Temperature $T [K]$';
-        plot_figure(app.PD.u1.value,app.PS.strP,'u','T',app.Misc.config,app.PD.CompleteOrIncomplete); 
+        plot_figure(app.PD.u1.value,strP,'u','T',app.Misc.config,app.PD.CompleteOrIncomplete); 
 elseif numel(phi)>1 && all(phi(2:end) == phi(1))
     app.Misc.config.labelx = 'Critical Equivalence Ratio $\phi_c$';
     app.Misc.config.labely = 'Temperature $T [K]$';
-    plot_figure(app.PS.strP,app.PS.strP,'phi_c','T',app.Misc.config,app.PD.CompleteOrIncomplete);
+    plot_figure(strP,strP,'phi_c','T',app.Misc.config,app.PD.CompleteOrIncomplete);
 end

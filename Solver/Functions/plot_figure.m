4b63be160198a31c0ee56410cab6c496f630fbbe
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-07-12 14:37:13 +0200

    Added possibility of specify axes
    Former-commit-id: f33dab535ad1d2eafe7dce383b88b80f10f80bad

--- Solver/Functions/plot_figure.m
+++ Solver/Functions/plot_figure.m
@@ -1,4 +1,4 @@
-function plot_figure(varargin)
+function ax = plot_figure(varargin)
 if nargin == 4
     if isstruct(varargin{1})
         x = struct2vector(varargin{1},varargin{3});
@@ -29,7 +29,7 @@ if nargin == 5
     end
     config = varargin{5};
 end
-if nargin == 6
+if nargin > 5
     if iscell(varargin{1})
         x = cell2vector(varargin{1},varargin{3});
     else
@@ -43,26 +43,36 @@ if nargin == 6
     config = varargin{5};
     leg = varargin(6);
 end
-
+if nargin > 6
+    ax = varargin(7);
+    ax = ax{1,1};
+else
+    ax = set_figure(config, x, y);
+end
+if nargin == 8
+    legend_name = varargin(8);
+    legend_name = legend_name{1,1};
+end
 %%% CHECK TIT FOR LATEX
 config.tit = strrep(config.tit,'%','\%');
 %%%
 % Plot configuration
-f = figure;
-set(f,'units','normalized','innerposition',[0.1 0.1 0.9 0.8],...
-    'outerposition',[0.1 0.1 0.9 0.8])
-set(axes,'LineWidth',config.linewidth,'FontSize',config.fontsize-2,'BoxStyle','full')
-
-grid on; box on; hold on; axis tight
-
-xlabel(config.labelx,'FontSize',config.fontsize,'interpreter','latex');
-ylabel(config.labely,'FontSize',config.fontsize,'interpreter','latex');
-xlim([min(x),1.02*max(x)])
-ylim([min(y),1.02*max(y)])
-plot(x,y,'LineWidth',config.linewidth);
-if nargin == 4
-    legend(leg,'FontSize',config.fontsize-2,'Location','northeast','interpreter','latex');
+plot(ax, x, y, 'LineWidth', config.linewidth);
+if nargin == 8
+    legend(ax, legend_name,'FontSize',config.fontsize-2,'Location','northeastoutside','interpreter','latex');
 end
-title({strcat('$',config.tit,'$')},'Interpreter','latex','FontSize',config.fontsize+4);
+end
 
+function ax = set_figure(config, x, y)
+    f = figure;
+    set(f,'units','normalized','innerposition',[0.1 0.1 0.9 0.8],...
+        'outerposition',[0.1 0.1 0.9 0.8])
+    ax = axes(f);
+    set(ax,'LineWidth',config.linewidth,'FontSize',config.fontsize-2,'BoxStyle','full')
+    grid(ax, 'on'); box(ax, 'on'); hold(ax, 'on'); axis(ax, 'tight');
+    xlabel(ax, config.labelx,'FontSize',config.fontsize,'interpreter','latex');
+    ylabel(ax, config.labely,'FontSize',config.fontsize,'interpreter','latex');
+    title({strcat('$',config.tit,'$')},'Interpreter','latex','FontSize',config.fontsize+4);
+% 	xlim(ax, [min(x),1.02*max(x)])
+%     ylim(ax, [min(y),1.02*max(y)])
 end

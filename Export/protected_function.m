ec0ffba51e89e5a8410238d03d2cfbcf2e0d9f27
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-07-12 13:30:26 +0200

    Included new folders

--- Export/protected_function.m
+++ Export/protected_function.m
@@ -1,6 +1,6 @@
 fun1 = fullfile('combustion_toolbox','*.m');
 fun2 = fullfile('combustion_toolbox','Databases','*.m');
-fun3 = fullfile('combustion_toolbox','Display','*.m');
+fun3 = fullfile('combustion_toolbox','Solver','Functions','Display','*.m');
 fun4 = fullfile('combustion_toolbox','Export','*.m');
 fun5 = fullfile('combustion_toolbox','GUI','*.m');
 fun6 = fullfile('combustion_toolbox','NASA_database','*.m');
@@ -8,5 +8,14 @@ fun7 = fullfile('combustion_toolbox','Settings','*.m');
 fun8 = fullfile('combustion_toolbox','Solver','Chemical Equilibrium','*.m');
 fun9 = fullfile('combustion_toolbox','Solver','Functions','*.m');
 fun10 = fullfile('combustion_toolbox','Solver','Shocks and detonations','*.m');
-
-pcode(fun1,fun2,fun3,fun4,fun5,fun6,fun7,fun8,fun9,fun10,'-inplace')
+fun11 = fullfile('combustion_toolbox','Settings','self','App','*.m');
+fun12 = fullfile('combustion_toolbox','Settings','self','Constants','*.m');
+fun13 = fullfile('combustion_toolbox','Settings','self','Elements','*.m');
+fun14 = fullfile('combustion_toolbox','Settings','self','MinorsProducts','*.m');
+fun15 = fullfile('combustion_toolbox','Settings','self','Miscelaneous','*.m');
+fun16 = fullfile('combustion_toolbox','Settings','self','ProblemDescription','*.m');
+fun17 = fullfile('combustion_toolbox','Settings','self','ProblemSolution','*.m');
+fun18 = fullfile('combustion_toolbox','Settings','self','Species','*.m');
+fun19 = fullfile('combustion_toolbox','Settings','self','TunningProperties','*.m');
+pcode(fun1,fun2,fun3,fun4,fun5,fun6,fun7,fun8,fun9,fun10,fun11,fun12,...
+    fun13,fun14,fun15,fun16,fun17,fun18,fun19)

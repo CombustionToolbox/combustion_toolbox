e428bc1ccd2aa8f13421cbf73c4946799cfd55ff
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-08-01 02:10:01 +0200

    Decreased tolerances
    Former-commit-id: 832e6f3f64314562d98ccf99ce7ae72ebb23fa3e

--- Settings/self/TunningProperties/TunningProperties.m
+++ Settings/self/TunningProperties/TunningProperties.m
@@ -3,7 +3,7 @@ function self = TunningProperties()
     self.factor_c = 1.0;
     % SHOCK and DET code
     self.guess = [2000,2000,0,1.5,2]; % Initial guess
-    self.ERRFT = 1e-4; % Tolerances
+    self.ERRFT = 1e-6; % Tolerances
     self.ERRFU = 1e-4; % Tolerances
-    self.ERRFV = 1e-4; % Tolerances
+    self.ERRFV = 1e-6; % Tolerances
 end

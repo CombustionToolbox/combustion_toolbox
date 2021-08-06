d8811133b56380573788fa895cdf32312f6c8f34
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-08-06 11:44:44 +0200

    miscellaneous

--- NASA_database/species_DeT.m
+++ NASA_database/species_DeT.m
@@ -1,8 +1,9 @@
 function DeTi = species_DeT(Species,T,strThProp)
+% global strThProp
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Calculates the thermal internal energy (in J/mol) for the specified 
+% Calculates the thermal internal energy (in kJ/mol) for the specified 
 % chemical species (Species) at the specified temperature (T)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-DeTi = strThProp.(Species).DeTcurve(T);
+DeTi = strThProp.(Species).DeTcurve(T)/1000;
--- NASA_database/species_DhT.m
+++ NASA_database/species_DhT.m
@@ -1,8 +1,9 @@
 function DhTi = species_DhT(Species,T,strThProp)
+% global strThProp
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Calculates the thermal enthalpy (in J/mol) for the specified 
+% Calculates the thermal enthalpy (in kJ/mol) for the specified 
 % chemical species (Species) at the specified temperature (T)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-DhTi = strThProp.(Species).DhTcurve(T);
+DhTi = strThProp.(Species).DhTcurve(T)/1000;

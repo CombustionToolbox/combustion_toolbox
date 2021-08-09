bc869aebbd7b8c99e2f43a6576ce754976da3961
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-08-09 11:07:38 +0200

    fixed bugs
    Former-commit-id: eaa67a60ec930f9da4fbd2da8f6e50af2538eff9

---
+++ Solver/Chemical Equilibrium/equilibrate_T.m
@@ -0,0 +1,19 @@
+function strP = equilibrate_T(self, strR, pP, TP)
+    % Compute number of moles 
+    [N, DeltaNP] = equilibrium(self, pP, TP, strR);
+    % Compute properties of all species
+    P = SetSpecies(self, self.S.LS, N(:, 1), TP);
+
+    if strfind(self.PD.ProblemType, 'P') == 2
+        strP = ComputeProperties(self, P, pP, TP);
+    else
+        NP = sum(P(:, 1) .* (1 - P(:, 10)));
+        pP = compute_pressure(self, strR, TP, NP);
+        strP = ComputeProperties(self, P, pP, TP);
+    end    
+    strP.error_moles = DeltaNP;
+end
+
+function pP = compute_pressure(self, strR, TP, N)
+    pP = (N * TP * self.C.R0 / (strR.v/1e3)) / 1e5;
+end

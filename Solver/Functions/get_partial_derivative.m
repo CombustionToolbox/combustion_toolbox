967e1b6213b7d48311fd9048d1f7062efcdeb6b6
Alberto Cuadra Lara <albertocuadralara@gmail.com>
2021-08-09 11:07:23 +0200

    fixed bugs
    Former-commit-id: 1791a35695edd9a5c05a2f3480435ba3c65c6386

--- Solver/Functions/get_partial_derivative.m
+++ Solver/Functions/get_partial_derivative.m
@@ -1,11 +1,11 @@
-function value = get_partial_derivative(str)
-    if strcmpi(ProblemType, 'HP')
+function value = get_partial_derivative(self, str)
+    if strcmpi(self.PD.ProblemType, 'HP')
         value = str.cP;
-    elseif strcmpi(ProblemType, 'EV')
+    elseif strcmpi(self.PD.ProblemType, 'EV')
         value = str.cV;
-    elseif strcmpi(ProblemType, 'SP')
+    elseif strcmpi(self.PD.ProblemType, 'SP')
         value = str.cP / str.T;
-    elseif strcmpi(ProblemType, 'SV')
+    elseif strcmpi(self.PD.ProblemType, 'SV')
         value = str.cV / str.T;
     end
     value = value * 1e-3; % units
--- Solver/Functions/root_finding/newton/newton.m
+++ Solver/Functions/root_finding/newton/newton.m
@@ -1,12 +1,14 @@
 function [x, ERR] = newton(self, strR, pP, attr_name, x0)
     % Newton-Raphson method for finding roots
-    
+    if any(strcmpi(self.PD.ProblemType, {'TP', 'TV'}))
+        x = get_transformation(self, 'TP');
+        ERR = 0;
+        return
+    end
     it = 0; ERR = 1.0;
-    self.TN.tol0 = self.TN.self.TN.tol0;
-    self.TN.itMax = self.TN.self.TN.itMax;
     while abs(ERR) > self.TN.tol0 && it < self.TN.itMax
         it = it + 1;
-        f0, fprime0 = get_ratio_newton(self, strR, pP, attr_name, x0);
+        [f0, fprime0] = get_ratio_newton(self, strR, pP, attr_name, x0);
         x = abs(x0 - f0 / fprime0);
         
         f = get_gpoint(self, strR, pP, attr_name, x);
@@ -19,6 +21,6 @@ end
 %%% NESTED FUNCTIONS
 function [f, fprime] = get_ratio_newton(self, strR, pP, attr_name, x)
     strP = equilibrate_T(self, strR, pP, x);
-    f = (getattr(strP, attr_name) - getattr(strR, attr_name));
-    fprime = get_partial_derivative(strP);
+    f = strP.(attr_name) - strR.(attr_name);
+    fprime = get_partial_derivative(self, strP);
 end

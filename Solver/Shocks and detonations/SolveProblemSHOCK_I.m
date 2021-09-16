9b01da737e1a19c52c42be8f7e3ca3a204d6f1a8
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-16 18:00:14 +0200

    Update: compatible with new notation
    Former-commit-id: 680bedb93f9d5db16922fa861e53730d303ae58a

--- Solver/Shocks and detonations/SolveProblemSHOCK_I.m
+++ Solver/Shocks and detonations/SolveProblemSHOCK_I.m
@@ -49,7 +49,7 @@ p = p/1e5;                     % pressure downstream        [bar]
 T = T1*p*V/(p1*V1);            % Temperature downstream     [K]
 % COMPUTE PROPERTIES OF THE PRODUCTS AT THE GIVEN CONDITIONS
 % state;
-strP = state(app, strR, r, T, phi, pP);
+strP = state(app, strR, r, T, pP);
 h = strP.h/strP.mi*1e3; % enthalpy upstream   [J/kg]
 p = strP.p;             % pressure downstream [bar]
 u = u1*r1/r;            % velocity downstream [m/s]. Continuity equation
@@ -73,7 +73,7 @@ while((abs(deltaT) > TN.ERRFT*T) || (abs(deltaV) > TN.ERRFV*V))
     Vper = V;
     rper = 1/Vper;
 
-    strP = state(app, strR, rper, Tper, phi, pP);
+    strP = state(app, strR, rper, Tper, pP);
     uper = u1*r1/rper;
     hper = strP.h/strP.mi*1e3;
     pper = strP.p;
@@ -90,7 +90,7 @@ while((abs(deltaT) > TN.ERRFT*T) || (abs(deltaV) > TN.ERRFV*V))
     Vper = V + DV;
     rper = 1/Vper;
 
-    strP = state(app, strR, rper, Tper, phi, pP);
+    strP = state(app, strR, rper, Tper, pP);
     uper = u1*r1/rper;
     hper = strP.h/strP.mi*1e3;
     pper = strP.p;
@@ -132,7 +132,7 @@ while((abs(deltaT) > TN.ERRFT*T) || (abs(deltaV) > TN.ERRFV*V))
     V = V + deltaV;
     r = 1/V;
     
-    strP = state(app, strR, r, T, phi, pP);
+    strP = state(app, strR, r, T, pP);
     h = strP.h/strP.mi*1e3;
     p = strP.p;
     u = u1*r1/r;
@@ -186,11 +186,11 @@ str2.h = h2*str2.mi/1e3;
 str2.error_problem = max(abs(deltaT),abs(deltaV));
 end
 
-function strP = state(app, strR, r, T, phi, pP)
+function strP = state(self, strR, r, T, pP)
 % Calculate frozen state given T & rho
 strR.v = strR.mi/r*1e3;
-TP = T; % vP = vR (computed from R);
+self.PD.TP.value = T;
 % Equilibrium composition at defined T and constant v
-app.PD.ProblemType = 'TV';
-strP = SolveProblemTP_TV(app, strR, phi, pP, TP);
+self.PD.ProblemType = 'TV';
+strP = equilibrate(self, strR, pP);
 end

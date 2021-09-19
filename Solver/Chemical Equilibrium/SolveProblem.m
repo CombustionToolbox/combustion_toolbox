37df7b3b1e5f9cd273e368be6bc868d29705f68b
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-19 23:21:59 +0200

    Update: rewrite new incident shock routine

--- Solver/Chemical Equilibrium/SolveProblem.m
+++ Solver/Chemical Equilibrium/SolveProblem.m
@@ -5,7 +5,7 @@ switch self.PD.ProblemType
         u1 = self.PD.u1.value(i);
         if strcmp(self.PD.ProblemType,'SHOCK_I')
             if i==self.C.l_phi
-                [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1);
+%                 [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1);
                 [self.PS.strR{i}, self.PS.strP{i}] = shock_incident(self, self.PS.strR{i}, u1);
             else
                 [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I_fast(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1, self.PS.strP{i+1});

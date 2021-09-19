04163d6e01c696e8634580070660714104c459e2
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-20 00:12:26 +0200

    Add: fast routine to resolve incident shock waves

--- Solver/Chemical Equilibrium/SolveProblem.m
+++ Solver/Chemical Equilibrium/SolveProblem.m
@@ -5,10 +5,11 @@ switch self.PD.ProblemType
         u1 = self.PD.u1.value(i);
         if strcmp(self.PD.ProblemType,'SHOCK_I')
             if i==self.C.l_phi
-%                 [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1);
-                [self.PS.strR{i}, self.PS.strP{i}] = shock_incident(self, self.PS.strR{i}, u1);
+                [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1);
+%                 [self.PS.strR{i}, self.PS.strP{i}] = shock_incident(self, self.PS.strR{i}, u1);
             else
                 [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I_fast(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1, self.PS.strP{i+1});
+%                 [self.PS.strR{i}, self.PS.strP{i}] = shock_incident_fast(self, self.PS.strR{i}, u1, self.PS.strP{i+1});
             end
         else
             if i==self.C.l_phi

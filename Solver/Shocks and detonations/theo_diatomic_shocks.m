043a4e71eee8b71021073a330fb4ef3144775dae
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-27 16:38:38 +0200

    Update: comment
    Former-commit-id: c1cd8f424fe1c7fdeeb262dab3eca3d0e31a02ce

--- Solver/Shocks and detonations/theo_diatomic_shocks.m
+++ Solver/Shocks and detonations/theo_diatomic_shocks.m
@@ -1,4 +1,4 @@
-function [R, P, M1, M2, K3D] = theo_diatomic_shocks(varargin)
+function [R, P, M1, M2] = theo_diatomic_shocks(varargin)
     global Infty steps
     Infty = 1e6; steps = 1e6;
     % Get parameters
@@ -15,64 +15,64 @@ function [R, P, M1, M2, K3D] = theo_diatomic_shocks(varargin)
     c2qc1sq = 5/7 .* T .* (1 + alpha + alpha_R + ((1 + alpha + alpha_T) .* (2.*(1 + alpha) - alpha_R .* (1 - 2.*epsilon_v + 2.*theta_d./T))) ./ (5 + alpha + 2.*(1 - alpha) .* epsilon_v.^2 .* exp(theta_v./T) + alpha_T .* (1 - 2.*epsilon_v + 2.*theta_d./T)));
     M1 = f_M1(R, P); 
     M2 = f_M2(M1, R, c2qc1sq); 
-    P0 = @(gamma) f_P0(R); 
-    
-    R_per = f_R(alpha, T - 1/1000, Br, theta_v, theta_d);
-    P_per = f_P(alpha, T - 1/1000, R_per);
-    Gammas = f_Gammas(M1, R, P, R_per, P_per);
-    
-    thetaOfzeta = f_thetaOfzeta(R, M2);
-    
-    sigma_a = f_sigma_a(R, M2, Gammas);
-    sigma_b = f_sigma_b(M2, Gammas);
-    sigma_c = f_sigma_c(R, M2, Gammas);
-    
-    pi_l1 = f_pi_l1(R, M2, sigma_b, sigma_c);
-    pi_l2 = f_pi_l2(R, M2, sigma_b, sigma_c);
-    pi_s  = f_pi_s(R, M2, sigma_b, sigma_c);
-    
-    ka = f_ka(M2);
-    wa = f_wa(M2);
-    
-    Delta_ua = f_Delta_ua(ka, wa, pi_s);
-    Delta_va = f_Delta_va(wa, pi_s);
-    
-    Omega_1 = f_Omega_1(R, M2);
-    Omega_2 = f_Omega_2(R, M2, Gammas);
-    Delta_Omega_l1 = f_Delta_Omega_l1(pi_l1, Omega_1, Omega_2);
-    Delta_Omega_l2 = f_Delta_Omega_l2(pi_l2, Omega_2);
-    Delta_Omega_s  = f_Delta_Omega_s(pi_s, Omega_1, Omega_2);
-    
-    Delta = f_Delta(M2);
-    
-    Delta_u_l1 = f_Delta_u_l1(Delta_Omega_l1, Delta);
-    Delta_u_l2 = f_Delta_u_l2(Delta_Omega_l2, Delta);
-    Delta_u_s  = f_Delta_u_s(Delta_Omega_s  , Delta);
-    Delta_u    = @(zeta) f_Delta_u(Delta_u_l1, Delta_u_l2, Delta_u_s, zeta);
-    
-    Delta_v_l1 = f_Delta_v_l1(M2, Delta_Omega_l1, Delta);
-    Delta_v_l2 = f_Delta_v_l2(M2, Delta_Omega_l2, Delta);
-    Delta_v_s  = f_Delta_v_s(M2, Delta_Omega_s, Delta);
-    Delta_v    = @(zeta) f_Delta_v(Delta_v_l1, Delta_v_l2, Delta_v_s, zeta);
-    
-    PDF_3D = f_PDF_3D(R, M2);
-    
-    L3Drl = f_L3Drl(Delta_u_l1, Delta_u_l2, PDF_3D, NT);
-    L3Drs = f_L3Drs(Delta_u_s, PDF_3D, NT);
-    L3Dr  = f_L3Dr(L3Drl, L3Drs);
-    L3Da  = f_L3Da(Delta_ua, PDF_3D, NT);
-    L3D   = f_L3D(L3Dr, L3Da);
-    
-    T3Drl = f_T3Drl(Delta_v_l1, Delta_v_l2, PDF_3D, NT);
-    T3Drs = f_T3Drs(Delta_v_s, PDF_3D, NT);
-    T3Dr  = f_T3Dr(T3Drl, T3Drs);
-    T3Da  = f_T3Da(Delta_va, PDF_3D, NT);
-    T3D   = f_T3D(T3Dr, T3Da);
-    
-    K3D = f_K3D(L3D, T3D);
+%     P0 = @(gamma) f_P0(R); 
+%     
+%     R_per = f_R(alpha, T - 1/1000, Br, theta_v, theta_d);
+%     P_per = f_P(alpha, T - 1/1000, R_per);
+%     Gammas = f_Gammas(M1, R, P, R_per, P_per);
+%     
+%     thetaOfzeta = f_thetaOfzeta(R, M2);
+%     
+%     sigma_a = f_sigma_a(R, M2, Gammas);
+%     sigma_b = f_sigma_b(M2, Gammas);
+%     sigma_c = f_sigma_c(R, M2, Gammas);
+%     
+%     pi_l1 = f_pi_l1(R, M2, sigma_b, sigma_c);
+%     pi_l2 = f_pi_l2(R, M2, sigma_b, sigma_c);
+%     pi_s  = f_pi_s(R, M2, sigma_b, sigma_c);
+%     
+%     ka = f_ka(M2);
+%     wa = f_wa(M2);
+%     
+%     Delta_ua = f_Delta_ua(ka, wa, pi_s);
+%     Delta_va = f_Delta_va(wa, pi_s);
+%     
+%     Omega_1 = f_Omega_1(R, M2);
+%     Omega_2 = f_Omega_2(R, M2, Gammas);
+%     Delta_Omega_l1 = f_Delta_Omega_l1(pi_l1, Omega_1, Omega_2);
+%     Delta_Omega_l2 = f_Delta_Omega_l2(pi_l2, Omega_2);
+%     Delta_Omega_s  = f_Delta_Omega_s(pi_s, Omega_1, Omega_2);
+%     
+%     Delta = f_Delta(M2);
+%     
+%     Delta_u_l1 = f_Delta_u_l1(Delta_Omega_l1, Delta);
+%     Delta_u_l2 = f_Delta_u_l2(Delta_Omega_l2, Delta);
+%     Delta_u_s  = f_Delta_u_s(Delta_Omega_s  , Delta);
+%     Delta_u    = @(zeta) f_Delta_u(Delta_u_l1, Delta_u_l2, Delta_u_s, zeta);
+%     
+%     Delta_v_l1 = f_Delta_v_l1(M2, Delta_Omega_l1, Delta);
+%     Delta_v_l2 = f_Delta_v_l2(M2, Delta_Omega_l2, Delta);
+%     Delta_v_s  = f_Delta_v_s(M2, Delta_Omega_s, Delta);
+%     Delta_v    = @(zeta) f_Delta_v(Delta_v_l1, Delta_v_l2, Delta_v_s, zeta);
+%     
+%     PDF_3D = f_PDF_3D(R, M2);
+%     
+%     L3Drl = f_L3Drl(Delta_u_l1, Delta_u_l2, PDF_3D, NT);
+%     L3Drs = f_L3Drs(Delta_u_s, PDF_3D, NT);
+%     L3Dr  = f_L3Dr(L3Drl, L3Drs);
+%     L3Da  = f_L3Da(Delta_ua, PDF_3D, NT);
+%     L3D   = f_L3D(L3Dr, L3Da);
+%     
+%     T3Drl = f_T3Drl(Delta_v_l1, Delta_v_l2, PDF_3D, NT);
+%     T3Drs = f_T3Drs(Delta_v_s, PDF_3D, NT);
+%     T3Dr  = f_T3Dr(T3Drl, T3Drs);
+%     T3Da  = f_T3Da(Delta_va, PDF_3D, NT);
+%     T3D   = f_T3D(T3Dr, T3Da);
+%     
+%     K3D = f_K3D(L3D, T3D);
     
 %     % Definitions ideal case (no dissociation / frozen chemistry)
-    R_ideal  = @(T) 3*(1 - T^(-1)) * (1 + sqrt((1 + T^(-1)) / (9*(1 - T^(-1))^2)));
+%     R_ideal  = @(T) 3*(1 - T^(-1)) * (1 + sqrt((1 + T^(-1)) / (9*(1 - T^(-1))^2)));
 %     P_ideal  = @(T) R_ideal(T) * T;
 %     c2qc1sq_ideal = @(T) T;
 %     M1_ideal = f_M1(R_ideal, P_ideal);

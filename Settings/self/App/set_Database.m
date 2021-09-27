03573f5fc014398879e7c7e9d5230aef1c4cd8fd
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-27 21:48:08 +0200

    Update: routine contained element

--- Settings/self/App/set_Database.m
+++ Settings/self/App/set_Database.m
@@ -5,4 +5,12 @@ function self = set_Database(self, reducedDB)
     self.strThProp = GenerateDatabase(self.strMaster);
     self.S.LS_DB = fieldnames(self.strThProp); 
     self.S.NS_DB = numel(self.S.LS_DB);
+    self.S.formula_DB = get_formula(self.S.LS_DB, self.strThProp);
+end
+
+% NESTED FUNCTIONS
+function formula_DB = get_formula(LS_DB, DB)
+    for i=length(LS_DB):-1:1
+        formula_DB{i} = DB.(LS_DB{i}).txFormula;
+    end
 end

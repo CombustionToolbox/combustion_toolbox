af926b26728c98c7b9e1dde8f4b2a082625f1124
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-28 21:01:08 +0200

    Update: routine complete combustion for rich cases
    Former-commit-id: 87c0d479e4a89166c71b2f74cb326906289d40d3

--- Solver/Chemical Equilibrium/complete_combustion.m
+++ Solver/Chemical Equilibrium/complete_combustion.m
@@ -24,10 +24,13 @@ else
         N0 = compute_moles_rich_carbon(x, z); 
     elseif phi < phi_c
         % general case of rich mixtures with hydrogens (H) and carbons (C)
-        disp('Not yet'); 
+%         N0 = compute_moles_rich_appr(x, y, z);
+        T  = 1500;
+        N0 = compute_moles_rich(x, y, z, T, self.C.R0, self.strThProp);
     elseif phi >= phi_c
         % general case of rich mixtures with hydrogens (H), carbons (C) and soot
-        disp('Not yet');
+        T  = 1000;
+        N0 = compute_moles_rich_soot(y, z, T, self.C.R0, self.strThProp);
     end
 end
 end
@@ -60,14 +63,49 @@ function N0 = compute_moles_rich_carbon(x, z)
     N0 = [NCO2_0, NCO_0, NH2O_0, NH2_0, NO2_0];
 end
 
+function N0 = compute_moles_rich_appr(x, y, z)
+    z = z/2;
+    NCO2_0 = -x + z;
+    NCO_0  = 2*x - z;
+    NH2O_0 = z;
+    NH2_0  = y/2 - z;
+    NO2_0  = 0;
+    N0 = [NCO2_0, NCO_0, NH2O_0, NH2_0, NO2_0];
+end
+
 function N0 = compute_moles_rich(x, y, z, T, R0, strThProp)
-    DG0 = (species_g0('CO', T, strThProp) + species_g0('H2O', T, strThProp) - species_g0('CO2', T, strThProp))*1000;
-    k4 = exp(-DG0 / (R0*T));
+    DG0 = (species_g0('CO', T, strThProp) + species_g0('H2O', T, strThProp) - species_g0('CO2', T, strThProp)) * 1000;
+    k4  = exp(-DG0 / (R0*T));
 
     NCO_0  = round((1/4)*(6*k4*x+k4*y-2*k4*z-4*x+2*z-sqrt(24*k4*x*z+16*x^2-16*x*z-16*k4*x^2+4*k4^2*x*y-8*k4^2*x*z-4*k4^2*y*z+4*k4*y*z+4*k4^2*x^2+k4^2*y^2+4*k4^2*z^2-8*k4*z^2+4*z^2))/(k4-1),14);
     NCO2_0 =    x - NCO_0;
     NH2O_0 = -2*x + z + NCO_0;
     NH2_0  =  2*x + y/2 - z - NCO_0;
     NO2_0  = 0;
+    N0 = [NCO2_0, NCO_0, NH2O_0, NH2_0, NO2_0];
+end
+
+function N0 = compute_moles_rich_soot(y, z, T, R0, strThProp)
+    DG0 = (species_g0('CO2', T, strThProp) - 2*species_g0('CO', T, strThProp)) * 1000;
+    k7  = exp(-DG0 / (R0*T));
+    DG0 = (species_g0('CO', T, strThProp) + species_g0('H2O', T, strThProp) - species_g0('CO2', T, strThProp)) * 1000;
+    k4  = exp(-DG0 / (R0*T));
+
+    zeta =1;
+    mu = k7/zeta;
+
+    a0 = -2*k4 / zeta - k7 * y + 2*k7 * z;
+    a1 = 2*k7 + 4*k4 * k7;
+    a2 = 4*k7^2 * zeta;
+    a3 = 2*k4 * z / zeta;
+
+    NCO_0 = real(-(a1/(3*a2))-(2^(1/3)*(-a1^2-3*a0*a2))/(3*a2*(-2*a1^3-9*a0*a1*a2+27*a2^2*a3+sqrt(-4*(a1^2+3*a0*a2)^3+(2*a1^3+9*a0*a1*a2-27*a2^2*a3)^2))^(1/3))+(-2*a1^3-9*a0*a1*a2+27*a2^2*a3+sqrt(-4*(a1^2+3*a0*a2)^3+(2*a1^3+9*a0*a1*a2-27*a2^2*a3)^2))^(1/3)/(3*2^(1/3)*a2));
+
+    NCO2_0 = mu * NCO_0^2;
+%     NCgr_0 = x - NCO2_0 - NCO_0; % Not required for our computations
+    NH2O_0 = z - 2*NCO2_0 - NCO_0;
+    NH2_0  = y/2 - NH2O_0;
+    NO2_0  = 0;
+    
     N0 = [NCO2_0, NCO_0, NH2O_0, NH2_0, NO2_0];
 end

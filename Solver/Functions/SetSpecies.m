cfffe5867e1c4905b3b8fdfaaf05a3d526f9409f
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-28 23:40:01 +0200

    Update: misc

--- Solver/Functions/SetSpecies.m
+++ Solver/Functions/SetSpecies.m
@@ -14,8 +14,11 @@ M = self.C.M0.value;
 R0 = self.C.R0;
 
 for n = length(N):-1:1
+    mi = N(n)*self.strThProp.(Species{n}).mm;
+    mmi = self.strThProp.(Species{n}).mm;
     hfi = self.strThProp.(Species{n}).hf/1000;
     efi = self.strThProp.(Species{n}).ef/1000;
+    swtCondensed = self.strThProp.(Species{n}).swtCondensed;
     if length(self.strThProp.(Species{n}).T) > 1
         DhTi = species_DhT(Species{n},T,self.strThProp);
         DeTi = species_DeT(Species{n},T,self.strThProp);
@@ -34,11 +37,7 @@ for n = length(N):-1:1
         cVi = 0;
         s0i = 0;
         pVi = 0;
-    end
-    swtCondensed = self.strThProp.(Species{n}).swtCondensed;
-    mi = N(n)*self.strThProp.(Species{n}).mm;
-    mmi = self.strThProp.(Species{n}).mm;
-        
+    end   
     ind = find_ind(Species(n), self.S.LS);
     M(ind, :) = [N(n), N(n)*[hfi, DhTi, efi, DeTi, cPi, cVi, s0i], pVi, swtCondensed, mi, mmi];
 end

b98fdabe11e44c6eb7f19d7af2ee615dd0d70e87
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-28 23:41:01 +0200

    Solve: fixed typo

--- Solver/Functions/root_finding/steffenson/steff_guess.m
+++ Solver/Functions/root_finding/steffenson/steff_guess.m
@@ -5,7 +5,7 @@ function x = steff_guess(self, strR, pP, attr_name)
     g_l = get_gpoint(self, strR, pP, attr_name, x_l);
     g_r = get_gpoint(self, strR, pP, attr_name, x_r);
     
-    if g_l * g_r > 0 && g_l < g_r
+    if g_l * g_r > 0 && abs(g_l) < abs(g_r)
         x = x_l - 50;
     elseif g_l * g_r > 0 || (isnan(g_l) && isnan(g_r))
         x = self.TN.root_T0;

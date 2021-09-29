61dce1295f975298114d38145eb6c5367633d513
Alberto Cuadra Lara <acuadra@ing.uc3m>
2021-09-29 16:16:37 +0200

    Update: sorted merged cells as FUEL, OXI and INERT
    Former-commit-id: 78c7a0ac9a28aeb5f9d3fb45f9a0a48d8eebd742

--- Settings/functions/Define_FOI.m
+++ Settings/functions/Define_FOI.m
@@ -3,7 +3,7 @@ function self = Define_FOI(self, i)
 R = self.PD.R_Fuel + self.PD.R_Oxidizer + self.PD.R_Inert;
 self.PS.strR{i} = ComputeProperties(self, R, self.PD.pR.value, self.PD.TR.value);
 self.PS.strR{i}.phi = self.PD.phi.value(i);
-self.PS.strR{i}.LS = merged_cells({self.PD.S_Fuel, self.PD.S_Oxidizer, self.PD.S_Inert});
+self.PS.strR{i}.LS  = merged_cells({self.PD.S_Fuel, self.PD.S_Oxidizer, self.PD.S_Inert});
 [~, ind_LS, ~] = intersect(self.PS.strR{i}.LS, self.S.LS);
 self.PS.strR{i}.ind_LS = ind_LS;
 end
@@ -11,8 +11,8 @@ end
 % NESTED FUNCTIONS
 function merged = merged_cells(cells)
     merged = [];
-    for j = length(cells):-1:1
-        for i = length(cells{j}):-1:1
+    for j = 1:length(cells)
+        for i = 1:length(cells{j})
             merged = [merged, cells{i, j}];
         end
     end

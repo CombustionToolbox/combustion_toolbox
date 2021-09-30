f6a409c51eca6689c039362ceb6ee0a97a30e082
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-30 21:29:24 +0200

    Update: misc
    Former-commit-id: 3fd659ab6a2332091c92b4758e47323d191b39e4

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -1,5 +1,5 @@
 %{ 
-COMBUSTION TOOLBOX @v0.3.47
+COMBUSTION TOOLBOX @v0.3.5
 
 Type of problems:
     * TP ------> Equilibrium composition at defined T and p
@@ -18,36 +18,34 @@ Type of problems:
          PhD Candidate - Group Fluid Mechanics
          Universidad Carlos III de Madrid
                   
-Last update Sep 28 2021
+Last update Sep 30 2021
 ---------------------------------------------------------------------- 
 %}
-
 addpath(genpath(pwd));
 
 %% INITIALIZE
-% app = App('Soot formation');
+app = App('Soot formation');
 % app = App('HC/02/N2');
 % app = App('HC/02/N2 extended');
 % app = App('HC/02/N2 rich');
 % app = App('HC/02/N2 propellants');
 % app = App('Ideal_air');
 % app = App('Hydrogen_l');
-app = App({'O2','N2','O','O3','N','NO','NO2','NO3','N2O','N2O3','N2O4','N3', ...
-    'eminus', 'Nplus', 'Nplus', 'NOplus', 'NO2minus', 'NO3minus', 'N2plus', 'N2minus', 'N2Oplus', ...
-     'Oplus', 'Ominus', 'O2plus', 'O2minus'});
+% app = App({'O2','N2','O','O3','N','NO','NO2','NO3','N2O','N2O3','N2O4','N3', ...
+%     'eminus', 'Nminus', 'Nplus', 'NOplus', 'NO2minus', 'NO3minus', 'N2plus', 'N2minus', 'N2Oplus', ...
+%      'Oplus', 'Ominus', 'O2plus', 'O2minus'});
 % app = App({'H2bLb', 'O2bLb'});
 %% PROBLEM CONDITIONS
 app.PD.TR.value  = 300;
-app.PD.pR.value  = 1.01325;
+app.PD.pR.value  = 1 * 1.01325;
 app.PD.phi.value = 0.25:0.01:5;
-app.PD.phi.value = 3.15;
+% app.PD.phi.value = 1;
 %% PROBLEM TYPE
 switch app.PD.ProblemType
     case 'TP' % * TP: Equilibrium composition at defined T and p
         app.PD.ProblemType = 'TP';
         app.PD.pP.value = app.PD.pR.value;
-%         app.PD.TP.value = [300:10:2000];
-        app.PD.TP.value = 1000;
+        app.PD.TP.value = 3000;
     case 'HP' % * HP: Adiabatic T and composition at constant p
         app.PD.ProblemType = 'HP';
         app.PD.pP.value = app.PD.pR.value;
@@ -70,10 +68,10 @@ switch app.PD.ProblemType
     case 'SHOCK_I' % * SHOCK_I: CALCULATE PLANAR INCIDENT SHOCK WAVE
         app.PD.ProblemType = 'SHOCK_I';
         u1 = logspace(2, 5, 500);
-        u1 = u1(u1<16000); u1 = u1(u1>=360);
+        u1 = u1(u1<20000); u1 = u1(u1>=360);
 %         u1 = [356,433,534,658,811,1000,1233,1520,1874,2310,2848,3511,4329,5337,6579,8111,9500,12328,15999,18421,21210,24421,28118,32375,37276,42919,49417,56899,65513];
 %         u1 = linspace(360, 9000, 1000);
-        u1 = 13000;
+%         u1 = 20000;
         app.PD.u1.value = u1; app.PD.phi.value = ones(1,length(app.PD.u1.value));
     case 'SHOCK_R' % * SHOCK_R: CALCULATE PLANAR POST-REFLECTED SHOCK STATE
         app.PD.ProblemType = 'SHOCK_R';
@@ -87,25 +85,25 @@ switch app.PD.ProblemType
         app.PD.ProblemType = 'DET_OVERDRIVEN';  
         app.PD.overdriven.value  = 1:1:5;
 end
-%% CONSTANT
+%% LOOP
 app.C.l_phi = length(app.PD.phi.value);
 tic
 for i=app.C.l_phi:-1:1
-%% DEFINE FUEL
-% app.PD.S_Fuel = {'C2H2_acetylene'}; app.PD.N_Fuel = 1;
+% DEFINE FUEL
+app.PD.S_Fuel = {'H2'}; app.PD.N_Fuel = 1;
 app = Define_F(app);
-%% DEFINE OXIDIZER
+% DEFINE OXIDIZER
 app.PD.S_Oxidizer = {'O2'}; app.PD.N_Oxidizer = app.PD.phi_t/app.PD.phi.value(i);
 app = Define_O(app);
-%% DEFINE DILUENTS/INERTS
+% DEFINE DILUENTS/INERTS
 app.PD.proportion_N2_O2 = 79/21;
 app.PD.S_Inert = {'N2'}; app.PD.N_Inert = app.PD.phi_t/app.PD.phi.value(i) * app.PD.proportion_N2_O2;
 app = Define_I(app);
-%% COMPUTE PROPERTIES
+% COMPUTE PROPERTIES
 app = Define_FOI(app, i);
-%% PROBLEM TYPE
+% PROBLEM TYPE
 app = SolveProblem(app, i);
-%% DISPLAY RESULTS COMMAND WINDOW
+% DISPLAY RESULTS COMMAND WINDOW
 results(app, i);
 end
 toc

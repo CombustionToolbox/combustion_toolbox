2ea50f62837df49ef94fb48d95153e8798848ccd
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-09-30 21:29:05 +0200

    Update:  misc
    Former-commit-id: 7528f007703918c7f563f69e63c54390ee9de7cc

--- Solver/Chemical Equilibrium/SolveProblem.m
+++ Solver/Chemical Equilibrium/SolveProblem.m
@@ -5,29 +5,23 @@ switch self.PD.ProblemType
         u1 = self.PD.u1.value(i);
         if strcmp(self.PD.ProblemType,'SHOCK_I')
             if i==self.C.l_phi
-%                 [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1);
                 [self.PS.strR{i}, self.PS.strP{i}] = shock_incident(self, self.PS.strR{i}, u1);
             else
-%                 [self.PS.strR{i}, self.PS.strP{i}] = SolveProblemSHOCK_I_fast(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1, self.PS.strP{i+1});
                 [self.PS.strR{i}, self.PS.strP{i}] = shock_incident(self, self.PS.strR{i}, u1, self.PS.strP{i+1});
             end
         else
             if i==self.C.l_phi
-%                 [self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}] = SolveProblemSHOCK_R(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1);
                 [self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}] = shock_reflected(self, self.PS.strR{i}, u1);
             else
-%                 [self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}] = SolveProblemSHOCK_R_fast(self, self.PS.strR{i}, self.PD.pR.value, self.PD.TR.value, u1, self.PS.str2{i+1}, self.PS.strP{i+1});
                 [self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}] = shock_reflected(self, self.PS.strR{i}, u1, self.PS.str2{i+1}, self.PS.strP{i+1});
             end
         end
     case {'DET'}
-%         if i==self.C.l_phi
-%             [self.PS.strR{i},self.PS.strP{i},self.TN.guess] = SolveProblemDET(self, self.PS.strR{i},self.PD.pR.value,self.PD.TR.value, self.TN.guess);
+        if i==self.C.l_phi
             [self.PS.strR{i}, self.PS.strP{i}] = cj_detonation(self, self.PS.strR{i});
-%         else
-%             [self.PS.strR{i},self.PS.strP{i},self.TN.guess] = SolveProblemDET(self, self.PS.strR{i},self.PD.pR.value,self.PD.TR.value, self.TN.guess);
-%             [self.PS.strR{i}, self.PS.strP{i}] = cj_detonation(self, self.PS.strR{i}, self.PS.strP{i+1});
-%         end
+        else
+            [self.PS.strR{i}, self.PS.strP{i}] = cj_detonation(self, self.PS.strR{i}, self.PS.strP{i+1});
+        end
     case 'DET_OVERDRIVEN'
         [self.PS.strR{i},self.PS.strP{i},self.TN.guess] = SolveProblemDET_OVERDRIVEN(self, self.PS.strR{i},self.PD.pR.value,self.PD.TR.value,self.TN.guess, self.PD.overdriven.value);
     otherwise

207c758faa42f4079580b28dc801e229da1bbe4d
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-08 21:58:48 +0200

    Update: version
    Former-commit-id: a1c9d77327c1ee3ef4da2ad4a9ef8681cdcc2fb6

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -1,5 +1,5 @@
 %{ 
-COMBUSTION TOOLBOX @v0.3.55
+COMBUSTION TOOLBOX @v0.3.6
 
 Type of problems:
     * TP -----------------> Equilibrium composition at defined T and p
@@ -18,13 +18,13 @@ Type of problems:
          PhD Candidate - Group Fluid Mechanics
          Universidad Carlos III de Madrid
                   
-Last update Oct 07 2021
+Last update Oct 08 2021
 ---------------------------------------------------------------------- 
 %}
 addpath(genpath(pwd));
 
 %% INITIALIZE
-app = App('Soot formation');
+app = App('Soot formation');    
 % app = App('HC/02/N2');
 % app = App('HC/02/N2 extended');
 % app = App('HC/02/N2 rich');
@@ -38,8 +38,8 @@ app = App('Soot formation');
 %% PROBLEM CONDITIONS
 app.PD.TR.value  = 300;
 app.PD.pR.value  = 1 * 1.01325;
-% app.PD.phi.value = 0.25:0.01:5;
-app.PD.phi.value = 1;
+app.PD.phi.value = 0.25:0.01:5;
+% app.PD.phi.value = 0.1;
 %% PROBLEM TYPE
 switch app.PD.ProblemType
     case 'TP' % * TP: Equilibrium composition at defined T and p

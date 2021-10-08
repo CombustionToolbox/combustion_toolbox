6340273c4685f16501becba694b28710096fea0c
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-08 22:28:21 +0200

    Update: misc

--- Solver/Functions/root_finding/print_error_root.m
+++ Solver/Functions/root_finding/print_error_root.m
@@ -2,9 +2,9 @@ function print_error_root(it, itMax, TP, ERR)
     if it == itMax
         fprintf('***********************************************************\n')
         fprintf('Root algorithm not converged \n')
-        fprintf('   Temp  =  %8.2f            \n', TP)
-        fprintf('   Error =  %8.2f            \n', abs(ERR)*100)
-        fprintf('   It    =  %8.d             \n', it)
+        fprintf('   Error       =  %8.2f [%%]  \n', ERR*100)
+        fprintf('   Temperature =  %8.2f [K]  \n', TP)
+        fprintf('   Iterations  =  %8.d [it] \n', it)
         fprintf('***********************************************************\n')
     end
 end

2910327807a1a2abb97155961ec855a7bfa1602c
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-08 22:27:34 +0200

    Add: relative error
    Former-commit-id: e7ecd04d2f5418a3954ad7f01970702b974b93ac

--- Solver/Functions/root_finding/steffenson/get_gpoint.m
+++ Solver/Functions/root_finding/steffenson/get_gpoint.m
@@ -1,4 +1,5 @@
-function gpoint = get_gpoint(self, strR, pP, attr_name, x)
+function [gpoint, gpoint_relative]= get_gpoint(self, strR, pP, attr_name, x)
     strP = equilibrate_T(self, strR, pP, x);
     gpoint = strP.(attr_name) - strR.(attr_name);
+    gpoint_relative = gpoint / strP.(attr_name);
 end

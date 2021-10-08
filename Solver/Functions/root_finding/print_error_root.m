c0a623a368f4a188cbe7e8a29971c72490b0f2cc
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-08 21:56:12 +0200

    Update: misc

--- Solver/Functions/root_finding/print_error_root.m
+++ Solver/Functions/root_finding/print_error_root.m
@@ -1,10 +1,10 @@
 function print_error_root(it, itMax, TP, ERR)
-    if it > itMax
-        print('**********************************\n')
-        print('** Root algorithm not converged **\n')
-        print('** Temp  =  %4.2f               **\n', TP)
-        print('** Error =  %4.2f%%             **\n', abs(ERR)*100)
-        print('** It    =  %4.d                **\n', it)
-        print('**********************************\n')
+    if it == itMax
+        fprintf('***********************************************************\n')
+        fprintf('Root algorithm not converged \n')
+        fprintf('   Temp  =  %8.2f            \n', TP)
+        fprintf('   Error =  %8.2f            \n', abs(ERR)*100)
+        fprintf('   It    =  %8.d             \n', it)
+        fprintf('***********************************************************\n')
     end
 end

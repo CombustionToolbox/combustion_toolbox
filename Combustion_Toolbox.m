17ef9dd8a4bda1782570db1fcd782b5fb4130f7a
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-10 16:48:40 +0200

    Update: misc
    Former-commit-id: 157c76928612b34ace7333d87f73f3f5d491adee

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -37,8 +37,8 @@ app = App('HC/02/N2 extended');
 %% PROBLEM CONDITIONS
 app.PD.TR.value  = 300;
 app.PD.pR.value  = 1 * 1.01325;
-% app.PD.phi.value = 0.25:0.01:5;
-app.PD.phi.value = 1;
+app.PD.phi.value = 0.5:0.01:2;
+% app.PD.phi.value = 1;
 %% PROBLEM TYPE
 switch app.PD.ProblemType
     case 'TP' % * TP: Equilibrium composition at defined T and p

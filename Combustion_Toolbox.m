2108c4729387fc74d3a525b6668a732bd0d66a13
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-11 10:10:57 +0200

    Update: testing
    Former-commit-id: c6a55cad15d90a4f8f161a3d17df748bebfaca4a

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -24,9 +24,9 @@ Last update Oct 10 2021
 addpath(genpath(pwd));
 
 %% INITIALIZE
-% app = App('Soot formation');    
+app = App('Soot formation');
 % app = App('HC/02/N2');
-app = App('HC/02/N2 extended');
+% app = App('HC/02/N2 extended');
 % app = App('HC/02/N2 rich');
 % app = App('HC/02/N2 propellants');
 % app = App('Ideal_air');
@@ -89,7 +89,7 @@ app.C.l_phi = length(app.PD.phi.value);
 tic
 for i=app.C.l_phi:-1:1
 % DEFINE FUEL
-app.PD.S_Fuel = {'CH4'}; app.PD.N_Fuel = 1;
+% app.PD.S_Fuel = {'CH4'}; app.PD.N_Fuel = 1;
 app = Define_F(app);
 % DEFINE OXIDIZER
 app.PD.S_Oxidizer = {'O2'}; app.PD.N_Oxidizer = app.PD.phi_t/app.PD.phi.value(i);

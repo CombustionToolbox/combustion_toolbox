1c8eb0d63e91fc4e0cce51de96246d9297eaf72c
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-13 14:58:18 +0200

    Add: function gui_get_Tolerances
    Former-commit-id: 3c7cc986b47253641b5cb8308037dfc45666430e

---
+++ GUI/Functions/gui_get_Tolerances.m
@@ -0,0 +1,10 @@
+function self = gui_get_Tolerances(self)
+    self.C.tolN = self.TraceoptionEditField.Value;              % Tolerance of the gibbs minimization method
+    self.C.tol0 = self.RootFindingMethodEditField.Value;          % Tolerance of the root finding algorithm
+    self.C.itMax = self.MaxiterationsRFMEditField.Value;          % Max number of iterations - root finding method
+    self.C.root_T0_l = self.RFMT0_LEditField.Value;               % First guess T[K] left branch - root finding method
+    self.C.root_T0_r = self.RFMT0_REditField.Value;               % First guess T[K] right branch - root finding method
+    self.C.root_T0 = self.RFMT0EditField.Value;                   % Guess T[K] if it's of previous range - root finding method
+    self.C.tol_shocks = self.ShocksandDetonationsEditField.Value; % Tolerance of shocks routines
+    self.C.it_shocks = self.MaxiterationsSDEditField.Value;       % Max number of iterations - shocks and detonations
+end

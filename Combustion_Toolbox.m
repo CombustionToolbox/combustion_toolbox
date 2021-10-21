37bd9c61675dc03615bc6dd10befb06a621e8a38
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-21 19:19:00 +0200

    Update: reorganized main function

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -24,71 +24,66 @@ Last update Oct 11 2021
 addpath(genpath(pwd));
 
 %% INITIALIZE
-app = App('Soot formation');
-% app = App('HC/02/N2');
-% app = App('HC/02/N2 extended');
-% app = App('HC/02/N2 rich');
-% app = App('HC/02/N2 propellants');
-% app = App('Ideal_air');
-% app = App('Hydrogen_l');
-% app = App({'O2','N2','O','O3','N','NO','NO2','NO3','N2O','N2O3','N2O4','N3', ...
+self = App('Soot formation');
+% self = App('HC/02/N2');
+% self = App('HC/02/N2 extended');
+% self = App('HC/02/N2 rich');
+% self = App('HC/02/N2 propellants');
+% self = App('Ideal_air');
+% self = App('Hydrogen_l');
+% self = App({'O2','N2','O','O3','N','NO','NO2','NO3','N2O','N2O3','N2O4','N3', ...
 %     'eminus', 'Nminus', 'Nplus', 'NOplus', 'NO2minus', 'NO3minus', 'N2plus', 'N2minus', 'N2Oplus', ...
 %      'Oplus', 'Ominus', 'O2plus', 'O2minus'});
 %% PROBLEM CONDITIONS
-app.PD.TR.value  = 300;
-app.PD.pR.value  = 1 * 1.01325;
-app.PD.phi.value = 0.5:0.01:5;
-% app.PD.phi.value = 1;
+self = set_prop(self, 'TR', 300, 'pR', 1 * 1.01325, 'phi', 0.5:0.01:5);
+% * DEFINE FUEL | default: self.PD.N_Fuel = 1
+self.PD.S_Fuel = {'CH4'};
+%  self.PD.N_Fuel = 1;
+% * DEFINE OXIDIZER | default: self.PD.N_Oxidizer = fun(phi == equivalence ratio)
+self.PD.S_Oxidizer = {'O2'};
+% self.PD.N_Oxidizer = 2;
+% * DEFINE DILUENTS/INERTS | default: self.PD.N_Inert = fun(phi == equivalence ratio)
+self.PD.S_Inert = {'N2'};
+% self.PD.N_Inert = 7.52;
 %% PROBLEM TYPE
-switch app.PD.ProblemType
+switch self.PD.ProblemType
     case 'TP' % * TP: Equilibrium composition at defined T and p
-        app.PD.pP.value = app.PD.pR.value;
-        app.PD.TP.value = 3000;
+        self = set_prop(self, 'pP', self.PD.pR.value, 'TP', 3000);
     case 'HP' % * HP: Adiabatic T and composition at constant p
-        app.PD.pP.value = app.PD.pR.value;
+        self = set_prop(self, 'pP', self.PD.pR.value);
     case 'SP' % * SP: Isentropic (i.e., adiabatic) compression/expansion to a specified p
-        app.PD.pP.value = 10:1:50; app.PD.phi.value = 1*ones(1, length(app.PD.pP.value));
-%         app.PD.pP.value = 10*ones(1, length(app.PD.phi.value));
+        pP = 10:1:50;
+        self = set_prop(self, 'pP', pP, 'phi', self.PD.phi.value(1) * ones(1, length(pP)));
+%         self = set_prop(app, 'pP', 10*ones(1, length(self.PD.phi.value)));
     case 'TV' % * TV: Equilibrium composition at defined T and constant v
-        app.PD.TP.value = 2000;
-        app.PD.pP.value = app.PD.pR.value; % guess
+        self = set_prop(self, 'TP', 2000, 'pP', self.PD.pR.value); % Pressure value is a guess!
     case 'EV' % * EV: Equilibrium composition at Adiabatic T and constant v
-        app.PD.pP.value = app.PD.pR.value;
-        % app.PD.pR.value = logspace(0,2,20); app.PD.phi.value = 1*ones(1,length(app.PD.pR.value));
+        self = set_prop(self, 'pP', self.PD.pR.value);
     case 'SV' % * SV: Isentropic (i.e., fast adiabatic) compression/expansion to a specified v
         % REMARK! vP_vR > 1 --> expansion, vP_vR < 1 --> compression
-        app.PD.vP_vR.value = 0.5:0.01:2; app.PD.phi.value = 1*ones(1, length(app.PD.vP_vR.value));
+        vP_vR = 0.5:0.01:2;
+        self = set_prop(self, 'vP_vR', vP_vR, 'phi', self.PD.phi.value(1) * ones(1, length(vP_vR)));
     case 'SHOCK_I' % * SHOCK_I: CALCULATE PLANAR INCIDENT SHOCK WAVE
         u1 = logspace(2, 5, 500);
         u1 = u1(u1<20000); u1 = u1(u1>=360);
 %         u1 = [356,433,534,658,811,1000,1233,1520,1874,2310,2848,3511,4329,5337,6579,8111,9500,12328,15999,18421,21210,24421,28118,32375,37276,42919,49417,56899,65513];
 %         u1 = linspace(360, 9000, 1000);
 %         u1 = 20000;
-        app.PD.u1.value = u1; app.PD.phi.value = ones(1,length(app.PD.u1.value));
+        self = set_prop(self, 'u1', u1, 'phi', self.PD.phi.value(1) * ones(1, length(u1)));
     case 'SHOCK_R' % * SHOCK_R: CALCULATE PLANAR POST-REFLECTED SHOCK STATE
         u1 = linspace(400, 6000, 1000);
-%         u1 = 2000;
-        app.PD.u1.value = u1; app.PD.phi.value = ones(1,length(app.PD.u1.value));
+        self = set_prop(self, 'u1', u1, 'phi', self.PD.phi.value(1) * ones(1, length(u1)));
     case 'DET' % * DET: CALCULATE CHAPMAN-JOUGET STATE (CJ UPPER STATE)
-%         app.PD.TR_vector.value = app.PD.TR.value;
+%         self.PD.TR_vector.value = self.PD.TR.value;
     case 'DET_OVERDRIVEN' % * DET_OVERDRIVEN: CALCULATE OVERDRIVEN DETONATION
-        app.PD.overdriven.value  = 1:0.1:10; app.PD.phi.value = 1*ones(1,length(app.PD.overdriven.value));
+        overdriven = 1:0.1:10;
+        self = set_prop(self, 'overdriven', overdriven, 'phi', self.PD.phi.value(1) * ones(1, length(overdriven)));
 end
-%% LOOP
-app.C.l_phi = length(app.PD.phi.value);
+%% SOLVE SELECTED PROBLEM
 tic
-for i=app.C.l_phi:-1:1
-% DEFINE FUEL | default: app.PD.N_Fuel = 1
-app.PD.S_Fuel = {'CH4'};
-% DEFINE OXIDIZER | default: app.PD.N_Oxidizer = app.PD.phi_t/app.PD.phi.value(i)
-app.PD.S_Oxidizer = {'O2'};
-% DEFINE DILUENTS/INERTS | default: app.PD.N_Inert = app.PD.phi_t/app.PD.phi.value(i) * 79/21;
-app.PD.S_Inert = {'N2'};
-% SOLVE SELECTED PROBLEM
-app = SolveProblem(app, i);
-end
+self = SolveProblem(self);
 toc
 %% DISPLAY RESULTS (PLOTS)
-% Uncomment to specify a custom set of species to be displayed
-% app.Misc.display_species = {'CO','CO2','H','HO2','H2','H2O','NO','NO2','N2','O','OH','O2','Cbgrb'};
-closing(app);
+% Uncomment to specify a custom set of species to be displayed in the molar fraction figure
+% self.Misc.display_species = {'CO','CO2','H','HO2','H2','H2O','NO','NO2','N2','O','OH','O2','Cbgrb'};
+closing(self);

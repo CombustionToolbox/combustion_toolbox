4e4724802ed8287aa57e6e8081db0c56559d21be
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-21 14:27:16 +0200

    Update: include default case

--- Settings/functions/Define_FOI.m
+++ Settings/functions/Define_FOI.m
@@ -1,4 +1,14 @@
 function self = Define_FOI(self, i)
+% DEFINE FUEL
+self.PD.N_Fuel = 1;
+self = Define_F(self);
+% DEFINE OXIDIZER
+self.PD.N_Oxidizer = self.PD.phi_t/self.PD.phi.value(i);
+self = Define_O(self);
+% DEFINE DILUENTS/INERTS
+self.PD.proportion_N2_O2 = 79/21;
+self.PD.N_Inert = self.PD.phi_t/self.PD.phi.value(i) * self.PD.proportion_N2_O2;
+self = Define_I(self);
 % COMPUTE PROPERTIES OF THE REACTIVES FOR THE GIVEN CONDITIONS
 R = self.PD.R_Fuel + self.PD.R_Oxidizer + self.PD.R_Inert;
 self.PS.strR{i} = ComputeProperties(self, R, self.PD.pR.value, self.PD.TR.value);

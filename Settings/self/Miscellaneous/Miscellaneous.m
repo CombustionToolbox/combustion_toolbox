5607751dfc5bb61eabb14d224f53fbf61fe22b86
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-21 14:27:03 +0200

    Update: include default case
    Former-commit-id: c1c3dab8beebdab42e1b186d9ec7ff03e8e22c56

--- Settings/self/Miscellaneous/Miscellaneous.m
+++ Settings/self/Miscellaneous/Miscellaneous.m
@@ -4,9 +4,11 @@ function self = Miscellaneous()
     self.config.fontsize = 22;
     self.FLAG_FIRST = true;
     self.FLAG_FOI = true;
+    self.FLAG_RESULTS = true; % Show result in the command window
     self.FLAG_GUI = false;
     self.FLAG_LABELS = false;
     self.save_Excel = false;
     self.timer_0 = [];
     self.display_species = [];
+    self.i = [];
 end

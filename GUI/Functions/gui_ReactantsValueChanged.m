f8a190d0428d6ea613a8e783748300bbdfee55c8
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-28 17:25:30 +0200

    Update: include seek species
    Former-commit-id: 0bdde656e49d1e152209c45997f887a75fe9c6e8

--- GUI/Functions/gui_ReactantsValueChanged.m
+++ GUI/Functions/gui_ReactantsValueChanged.m
@@ -1,39 +1,84 @@
-function gui_ReactantsValueChanged(self)
+function gui_ReactantsValueChanged(self, event)
     % Update reactants & GUI
-    self = gui_update_Reactants(self);
-    % Update UITable_R2 (species, numer of moles, mole fractions)
-    self.UITable_R2.Data = self.UITable_R.Data(:, 1:3); 
-    % Get temperature of the mixture
-    self.UITable_R.Data(:, 5) = {sscanf(self.PR1.Value, '%f')};
+    self = gui_update_Reactants(self, event);
     % Update equivalence ratio 
     self.edit_phi2.Value = self.edit_phi.Value;
 end
 
 % SUB-PASS FUNCTIONS
-function self = gui_update_Reactants(self)
+function self = gui_update_Reactants(self, event)
+    FLAG_IDEAL_AIR = true;
     switch self.Reactants.Value
         case '1' % No species selected
-            self.UITable_R.Data  = [];
-            self.UITable_P.Data  = [];
-            self.UITable_R2.Data = [];
+            gui_empty_Reactants(self);
             return
-        case '2' % AIR IDEAL (21% O2 + 79% N2)
-            self.UITable_R.Data = {'N2', 0.79/0.21, 0.79, 'Inert'; 'O2', 1, 0.21, 'Oxidant'};
-            self.UITable_P.Data = self.UITable_R.Data(:, 1);
-        case '3' % AIR (20.9476% O2 + 78.084% N2 + 0.9365% Ar + 0.0319% CO2)
-            self.UITable_R.Data = {'N2', 0.78084/0.209476, 0.78084, 'Inert'; 'O2', 1, 0.209476, 'Oxidant'; 'Ar', 0.009365/0.209476, 0.009365, 'Inert'; 'CO2', 0.000319/0.209476, 0.000319, 'Oxidant'};
-            self.UITable_P.Data = self.UITable_R.Data(:, 1);
-        case '4' % METHANE + AIR IDEAL
-            phi_t = 2;
-            Ni = [phi_t * 0.79/0.21, phi_t, 1];
-            Xi = Ni / sum(Ni);
-            self.UITable_R.Data = {'N2', Ni(1), Xi(1), 'Inert'; 'O2', Ni(2), Xi(2), 'Oxidant'; 'CH4', Ni(3), Xi(3), 'Fuel'};
-            self.UITable_P.Data = self.UITable_R.Data(:, 1);
-        case '5' % METHANE + AIR
-            phi_t = 2;
-            Ni = [phi_t * 0.78084/0.209476, phi_t, phi_t * 0.009365/0.209476, phi_t * 0.009365/0.209476, 1];
-            Xi = Ni / sum(Ni);
-            self.UITable_R.Data = {'N2', Ni(1), Xi(1), 'Inert'; 'O2', Ni(2), Xi(2), 'Oxidant'; 'Ar', Ni(3), Xi(3), 'Inert'; 'CO2', Ni(4), Xi(4), 'Oxidant'; 'CH4', Ni(5), Xi(5), 'Fuel'};
-            self.UITable_P.Data = self.UITable_R.Data(:, 1);
+        case '2' % AIR
+            FLAG_FUEL = false;
+            phi_ratio = 1;
+            [~, ~, Data] = compute_reactants(phi_ratio, FLAG_IDEAL_AIR, FLAG_FUEL);
+            self.UITable_R.Data = Data;
+        case '3' % METHANE + AIR
+            FLAG_FUEL = true;
+            phi_ratio = 2;
+            [Ni, Xi, Data] = compute_reactants(phi_ratio, FLAG_IDEAL_AIR, FLAG_FUEL);
+            self.UITable_R.Data = [Data; {'CH4', Ni(3), Xi(3), 'Fuel'}];
+        otherwise % SET NEW SPECIES
+            try
+                species = seeker_species(self, event);
+            catch
+                message = {'Species not found.'};
+                uialert(self.UIFigure, message, 'Warning', 'Icon', 'warning');
+                
+                gui_empty_Reactants(self);
+                return
+            end
+            self.UITable_R.Data = {species, 0, 0, 'Fuel'};
+    end
+    self.UITable_P.Data = self.UITable_R.Data(:, 1);    % Update UITable_P  (species, numer of moles, mole fractions, temperature)
+    self.UITable_R2.Data = self.UITable_R.Data(:, 1:3); % Update UITable_R2 (species, numer of moles, mole fractions)
+    % Get temperature of the mixture
+    self.UITable_R.Data(:, 5) = {sscanf(self.PR1.Value, '%f')};
+end
+
+function self = gui_empty_Reactants(self)
+    self.UITable_R.Data  = [];
+    self.UITable_P.Data  = [];
+    self.UITable_R2.Data = [];
+end
+
+function species = seeker_species(self, event)
+    seekSpecies = event.Value;
+    listSpecies = self.S.LS_DB;
+    index = 0;
+    seekIndex = false;
+    while ~seekIndex
+        index = index + 1;
+        seekIndex = startsWith(listSpecies{index}, seekSpecies, 'IgnoreCase', false);
+    end
+    if index
+        species = listSpecies{index}; % Species found in the Database
+    else
+        species = []; % Species not found in the Database
+    end
+end
+
+function [Ni, Xi, Data] = compute_reactants(phi_ratio, FLAG_IDEAL_AIR, FLAG_FUEL)
+    Ni = compute_N_air(phi_ratio, FLAG_IDEAL_AIR);
+    if FLAG_FUEL
+        Ni = [Ni, 1];
+    end
+    Xi = Ni / sum(Ni);
+    if FLAG_IDEAL_AIR
+        Data = {'N2', Ni(1), Xi(1), 'Inert'; 'O2', Ni(2), Xi(2), 'Oxidant'};
+    else
+        Data = {'N2', Ni(1), Xi(1), 'Inert'; 'O2', Ni(2), Xi(2), 'Oxidant'; 'Ar', Ni(3), Xi(3), 'Inert'; 'CO2', Ni(4), Xi(4), 'Oxidant'};
+    end
+end
+
+function Ni_air = compute_N_air(phi_ratio, FLAG_IDEAL_AIR)
+    if FLAG_IDEAL_AIR
+        Ni_air = [phi_ratio * 0.79/0.21, phi_ratio];
+    else
+        Ni_air = [phi_ratio * 0.78084/0.209476, phi_ratio, phi_ratio * 0.009365/0.209476, phi_ratio * 0.009365/0.209476];
     end
 end

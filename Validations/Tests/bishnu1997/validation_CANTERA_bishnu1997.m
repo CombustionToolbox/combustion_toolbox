5c0f53117fb1d951bb4b163f43cc580e02218594
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-29 10:08:05 +0200

    Validation: test
    Former-commit-id: e7463a3e9cfc458ecae2cfd973aac42abe06f466

--- Validations/Tests/bishnu1997/validation_CANTERA_bishnu1997.m
+++ Validations/Tests/bishnu1997/validation_CANTERA_bishnu1997.m
@@ -9,11 +9,13 @@ test_SD(T,P)
 %% SUB-PASS FUNCTIONS
 function tEnd = test_CT(T,P)
     %% INITIALIZE
-    self = App({'H2','H2O','H2O2','H','OH','HO2','O2','O'});
+    self = App({'H2','H','O','O2','OH','H2O','HO2','H2O2','C','CH','CH2','CH3','CH4','CO','CO2','HCO','CH2OH','CH3O','CH3OH','C2H','C2H4','C2H5','C2H6','HCCO','N','NH','NH2','NH3','NO','NO2','N2O','HNO','CN','HCN','HNCO','NCO','N2','C3H8'});
     %% INITIAL CONDITIONS
     self = set_prop(self, 'TR', 300, 'pR', 1 * 1.01325, 'phi', 1);
-    self.PD.S_Fuel     = {'H2'};
+    self.PD.S_Fuel     = {'CH4'};
     self.PD.S_Oxidizer = {'O2'};
+    self.PD.S_Inert    = {'N2'};
+    self.PD.proportion_inerts_O2 = 79/21;
     %% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
     self = set_prop(self, 'pP', P * 1.01325, 'TP', T); 
     %% TUNNING PROPERTIES
@@ -43,10 +45,9 @@ function tEnd = test_CANTERA(T,P)
 end
 
 function tEnd = test_SD(T,P)
-    mech = 'h2o2_bishnu1997.yaml';
-    gas = Solution(mech);
-    x = 'H2:2 O2:1';
-    set(gas,'Temperature',T,'Pressure',P * 101325.0,'MoleFractions',x);
+    P = P * 1e5;
+    mech = 'gri30.yaml';
+    x = 'CH4:1 O2:2 N2:7.52';
     tStart = tic;
     cj_speed = CJspeed(P, T, x, mech);
     gas = PostShock_eq(cj_speed,P, T, x, mech)

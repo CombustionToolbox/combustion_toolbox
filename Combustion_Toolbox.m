11a2a86c64c1694b882fc92793bde4f6ad3b57bc
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-10-30 07:06:57 +0200

    Update: version
    Former-commit-id: e70fc8dfc61b84dc1184a2be8193362d3c0327c7
    Former-commit-id: efc41eb566aa4e6b5a03c286719ee979ab6bcd08

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -1,5 +1,5 @@
 % -------------------------------------------------------------------------
-% COMBUSTION TOOLBOX @v0.3.75
+% COMBUSTION TOOLBOX @v0.3.77
 % A MATLAB-GUI based open-source tool for solving combustion problems.
 %
 % Type of problems:

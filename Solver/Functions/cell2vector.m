4ab374fbddeb47a47071c7c25ee03b7852a28b86
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-03 08:34:55 +0100

    Update: generalize routine
    Former-commit-id: 916367bcbbf3cca409d884e41180c5756b259b22

--- Solver/Functions/cell2vector.m
+++ Solver/Functions/cell2vector.m
@@ -1,6 +1,27 @@
-function vector = cell2vector(str,field)
+function vector = cell2vector(varargin)
+    % Convert values of a individual cell into a vector. If the value 
+    % correspond with a struct it can return as a vector the values of a 
+    % given fieldname.
+    if nargin > 0
+        str = varargin{1};
+    end
+    if nargin > 1
+        field = varargin{2};
+    end
+    if nargin > 2
+        error('Error in @cell2vector: Too many input arguments.');
+    end
+    
     Nstruct = length(str);
-    for i=Nstruct:-1:1
-        vector(i) = str{i}.(field);
+    if nargin == 1
+        % Return the values of a cell as a vector
+        for i=Nstruct:-1:1
+            vector(i) = str{i};
+        end
+    else
+        % Return the field of a struct as a vector
+        for i=Nstruct:-1:1
+            vector(i) = str{i}.(field);
+        end
     end
 end

e761e17000118c78ebf916b50b8a56b45c20a511
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-03 17:45:40 +0100

    Add: include preload DB from input values
    Former-commit-id: a80a8c8c257854b0998fef78a3dd34250dd349b7
    Former-commit-id: d3cc0fe07ed445dcca98d9f71b998bb175960144

--- Settings/self/App/set_DB.m
+++ Settings/self/App/set_DB.m
@@ -1,8 +1,10 @@
-function self = set_DB(self, reducedDB)
-    % False: complete DataBase; True: reduced DB
-    self.DB_master = generate_DB_master(reducedDB); 
-    % Reduced Database with tabulated data of selected species
-    self.DB = generate_DB(self.DB_master);
+function self = set_DB(self, reducedDB, FLAG_FAST)
+    if ~FLAG_FAST
+        % False: complete DataBase; True: reduced DB
+        self.DB_master = generate_DB_master(reducedDB); 
+        % Reduced Database with tabulated data of selected species
+        self.DB = generate_DB(self.DB_master);
+    end
     self.S.LS_DB = fieldnames(self.DB); 
     self.S.NS_DB = numel(self.S.LS_DB);
 end

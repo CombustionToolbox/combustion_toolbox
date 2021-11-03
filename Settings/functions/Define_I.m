5df9c343d54023a9600bb0572b501459dfdf6d92
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-03 17:42:48 +0100

    Add: comments
    Former-commit-id: dffd945b809c3a54841b714f0c51e7bdba251fe1

--- Settings/functions/Define_I.m
+++ Settings/functions/Define_I.m
@@ -1,9 +1,11 @@
 function self = Define_I(self)
-if ~isempty(self.PD.S_Inert)
-    self.PD.R_Inert = SetSpecies(self, self.PD.S_Inert, self.PD.N_Inert, self.PD.TR.value);
-else
-    self.PD.R_Inert = 0; % case without inert gases
-end
-if ~isempty(self.PD.S_Oxidizer) || ~isempty(self.PD.S_Inert)
-    self.PS.strR_Oxidizer = ComputeProperties(self, self.PD.R_Oxidizer + self.PD.R_Inert, self.PD.pR.value, self.PD.TR.value);
+    % Set Inert of the mixture
+    if ~isempty(self.PD.S_Inert)
+        self.PD.R_Inert = SetSpecies(self, self.PD.S_Inert, self.PD.N_Inert, self.PD.TR.value);
+    else
+        self.PD.R_Inert = 0; % case without inert gases
+    end
+    if ~isempty(self.PD.S_Oxidizer) || ~isempty(self.PD.S_Inert)
+        self.PS.strR_Oxidizer = ComputeProperties(self, self.PD.R_Oxidizer + self.PD.R_Inert, self.PD.pR.value, self.PD.TR.value);
+    end
 end

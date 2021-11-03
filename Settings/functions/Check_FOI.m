6ebd68e8db95692648d277ff2e1e6bb3086686e9
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-03 22:04:34 +0100

    Add: update S.ind_swt, ... with list of products
    Former-commit-id: 528f9eb60005e757cfaac59a09a6e3bbf3e2fc83
    Former-commit-id: 16bff5e1c284d667c1460019a2265d06be514c8b

--- Settings/functions/Check_FOI.m
+++ Settings/functions/Check_FOI.m
@@ -1,18 +1,22 @@
 function self = Check_FOI(self, FOI_species)
+    % Check that fuel species are contained in the list of products (only for initial computations)
     if self.Misc.FLAG_FOI
         self.Misc.FLAG_FOI = false;
-        FLAG = false;
-        for i=1:numel(FOI_species)
+        self.Misc.index_LS_original = 1:1:self.S.NS;
+        for i=1:length(FOI_species)
             if ~strcmp(self.S.LS, FOI_species(i))                 
                 self.S.LS = [self.S.LS, FOI_species(i)];
                 self.S.NS = length(self.S.LS);
-                FLAG = true;
+                self.Misc.FLAG_ADDED_SPECIES = true;
             end
         end
-        if FLAG
+        if self.Misc.FLAG_ADDED_SPECIES
             self.S.ind_nswt = [];
             self.S.ind_swt = [];
             self = Initialize(self);
+            self.S.ind_nswt = intersect(self.S.ind_nswt, self.Misc.index_LS_original);
+            self.S.ind_swt = intersect(self.S.ind_swt, self.Misc.index_LS_original);
+            self.S.ind_cryogenic = intersect(self.S.ind_cryogenic, self.Misc.index_LS_original);
         end
         self.Misc.FLAG_FIRST = false;
     end

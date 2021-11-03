01cd71028fe285b586749162954b4dfeacfe6f02
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-03 17:44:17 +0100

    Update: generalized function and included comments
    Former-commit-id: 0c24fa3661dfd38fea26c9ffe2d11f62322d7dcc

--- GUI/Functions/gui_get_reactants.m
+++ GUI/Functions/gui_get_reactants.m
@@ -1,23 +1,24 @@
-function gui_get_reactants(app)
-    % FUEL
-    if sum(app.ind_Fuel)
-        app.PD.S_Fuel = regexprep(app.UITable_R.Data(app.ind_Fuel,1),'\s+','')';
-        app.PD.N_Fuel = app.UITable_R.Data{app.ind_Fuel, 2};
+function self = gui_get_reactants(self)
+    % Function that obtains the number of moles and the name of each
+    % species in the mixture and catalog them in Fuel, Oxidizer or Inert.
+    
+    % Get indexes of Fuel, Oxidizer and Inert species
+    self = gui_get_typeSpecies(self);
+    % Fuel
+    [self.PD.S_Fuel, self.PD.N_Fuel] = gui_get_reactant(self, self.ind_Fuel);
+    % Oxidizer
+    [self.PD.S_Oxidizer, self.PD.N_Oxidizer] = gui_get_reactant(self, self.ind_Oxidizer);
+    % Inert
+    [self.PD.S_Inert, self.PD.N_Inert] = gui_get_reactant(self, self.ind_Inert);
+end
+
+% SUB-PASS FUNCTIONS
+function [S, N] = gui_get_reactant(self, ind)
+    if sum(self.(ind))
+        S = regexprep(self.UITable_R.Data(self.(ind), 1),'\s+','')';
+        N = self.UITable_R.Data{self.(ind), 2};
     else
-        app.PD.S_Fuel = [];
-    end
-    % OXIDIZER
-    if sum(app.ind_Oxidizer)
-        app.PD.S_Oxidizer = regexprep(app.UITable_R.Data(app.ind_Oxidizer,1),'\s+','')';
-        app.PD.N_Oxidizer = app.UITable_R.Data{app.ind_Oxidizer, 2};
-    else
-        app.PD.S_Oxidizer = [];
-    end
-    % INERT
-    if sum(app.ind_Inert)
-        app.PD.S_Inert = regexprep(app.UITable_R.Data(app.ind_Inert,1),'\s+','')';
-        app.PD.N_Inert = app.UITable_R.Data{app.ind_Inert, 2};
-    else
-        app.PD.S_Inert = [];
+        S = [];
+        N = [];
     end
 end

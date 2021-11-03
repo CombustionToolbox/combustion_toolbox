1e011944f81bdc5f804ba98cb6c5cb9a533870b4
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-03 18:33:16 +0100

    Update: join functions

--- GUI/Functions/gui_get_typeSpecies.m
+++
@@ -1,8 +0,0 @@
-function obj = gui_get_typeSpecies(obj)
-    % Function that obtains the indexes of the different type of species in
-    % the mixture
-    typeSpecies = obj.UITable_R.Data(:, 4);
-    obj.ind_Fuel     = contains(typeSpecies, 'Fuel');
-    obj.ind_Oxidizer = contains(typeSpecies, 'Oxidizer');
-    obj.ind_Inert    = contains(typeSpecies, 'Inert');
-end

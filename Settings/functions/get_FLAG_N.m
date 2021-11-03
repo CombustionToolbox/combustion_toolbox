5b044df1b521b2d32b9f64d8b14af62865585fb5
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-03 17:42:06 +0100

    Update: generalized function and included comments

--- Settings/functions/get_FLAG_N.m
+++ Settings/functions/get_FLAG_N.m
@@ -2,13 +2,16 @@ function self = get_FLAG_N(self)
     % Flag if the number of moles of fuel, oxidant and inert species
     % is specified. If not, consider 1 mole for the fuel and calculate
     % the remaining moles from the equivalence relation.
-    if isempty(self.PD.N_Fuel)
-        self.Misc.FLAG_N_Fuel = false;
-    end
-    if isempty(self.PD.N_Oxidizer)
-        self.Misc.FLAG_N_Oxidizer = false;
-    end
-    if isempty(self.PD.N_Inert)
-        self.Misc.FLAG_N_Inert = false;
+    self = get_FLAG(self, 'FLAG_N_Fuel', 'N_Fuel');
+    self = get_FLAG(self, 'FLAG_N_Oxidizer', 'N_Oxidizer');
+    self = get_FLAG(self, 'FLAG_N_Inert', 'N_Inert');
+end
+
+% SUB-PASS FUNCTIONS
+function self = get_FLAG(self, FLAG_name, cond_name)
+    if isempty(self.PD.(cond_name))
+        self.Misc.(FLAG_name) = false;
+    else
+        self.Misc.(FLAG_name) = true;
     end
 end

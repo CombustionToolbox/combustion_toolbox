1d92b589aabb4663221fd5a8856c0942509b81e5
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-04 07:37:58 +0100

    Solve: compute exactly list of products
    Former-commit-id: bf86f5d3d453d97044994967d84d8867034f48e4

--- Settings/functions/Define_FOI.m
+++ Settings/functions/Define_FOI.m
@@ -1,5 +1,8 @@
 function self = Define_FOI(self, i)
     % Set up mixture: fuel, oxidizer and diluent/inert species
+
+    % Check reactant species are contained in the list of products (initial computations)
+    self = Check_FOI(self, [self.PD.S_Fuel, self.PD.S_Oxidizer, self.PD.S_Inert]);
     % Define Fuel
     if ~self.Misc.FLAG_N_Fuel
         self.PD.N_Fuel = 1;

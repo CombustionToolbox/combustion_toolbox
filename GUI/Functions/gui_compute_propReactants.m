ce47bcd6ba4cb2cca86b46ca6dfbc20adf0716ae
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-05 19:52:18 +0100

    Update: comments

--- GUI/Functions/gui_compute_propReactants.m
+++ GUI/Functions/gui_compute_propReactants.m
@@ -12,7 +12,7 @@ function app = gui_compute_propReactants(obj, app)
     % Compute stoichiometric matrix and properties of the mixture
     app = Define_FOI(app, 1);
     if ~isempty(app.PD.S_Fuel) && ~isempty(app.PD.S_Oxidizer)
-        % Compute percentage Fuel, Oxidant/Fuel ratio and equivalence ratio
+        % Compute percentage Fuel, Oxidizer/Fuel ratio and equivalence ratio
         R = app.PD.R_Fuel + app.PD.R_Oxidizer + app.PD.R_Inert;
         app.PS.strR{1}.percentage_Fuel = sum(app.PD.R_Fuel(:, 1)) / sum(R(:, 1)) * 100;
         app.PS.strR{1}.FO = sum(app.PD.R_Fuel(:, 1)) / sum(app.PD.R_Oxidizer(:, 1));

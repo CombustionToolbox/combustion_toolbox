d0d10e1b83acc1d96a2c69faa6aa90364bab32c5
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-05 16:17:33 +0100

    Add: gui_edit_phiValueChanged (draft)
    Former-commit-id: dfe4c0f3b3061b8e9e6ac1b581f7ad9396f27be2
    Former-commit-id: cb590a12d5d05b7026ddd1354786f1fc46d0a215

---
+++ gui_edit_phiValueChanged.m
@@ -0,0 +1,23 @@
+function gui_edit_phiValueChanged(obj)
+    % Update moles and mole fractions of the reactant UITables for the
+    % given equivalence ratio
+    try
+        % Initialize app (fast: transfer DB)
+        app = App('fast', obj.DB_master, obj.DB);
+        % Get reactant species
+        app = gui_get_reactants(obj, event, app);
+        % Compute properties of the mixture
+        app = gui_compute_propReactants(obj, app);
+        % Update UITable classes
+        gui_update_UITable_R(obj, app);
+        obj.UITable_P.Data = obj.UITable_R.Data(:, 1);    % (species, numer of moles, mole fractions, temperature)
+        obj.UITable_R2.Data = obj.UITable_R.Data(:, 1:3); % (species, numer of moles, mole fractions)
+        % Update GUI: equivalence ratio, O/F and percentage Fuel
+        gui_update_phi(obj, app);
+    catch ME
+      errorMessage = sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
+      ME.stack(1).name, ME.stack(1).line, ME.message);
+      fprintf('%s\n', errorMessage);
+      uiwait(warndlg(errorMessage));
+    end
+end

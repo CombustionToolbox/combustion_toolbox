477dfcf99c47050bbf8f4cc613899dfb3610c8aa
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-05 10:57:47 +0100

    Update: more versatile
    Former-commit-id: cd143d86c035f17ecc9fc3d9cf3a0e07df6cb976

--- GUI/Functions/gui_get_reactants.m
+++ GUI/Functions/gui_get_reactants.m
@@ -1,24 +1,49 @@
-function self = gui_get_reactants(self)
-    % Function that obtains the number of moles and the name of each
-    % species in the mixture and catalog them in Fuel, Oxidizer or Inert.
-    
-    % Get indexes of Fuel, Oxidizer and Inert species
-    self = gui_get_typeSpecies(self);
-    % Fuel
-    [self.PD.S_Fuel, self.PD.N_Fuel] = gui_get_reactant(self, self.ind_Fuel);
-    % Oxidizer
-    [self.PD.S_Oxidizer, self.PD.N_Oxidizer] = gui_get_reactant(self, self.ind_Oxidizer);
-    % Inert
-    [self.PD.S_Inert, self.PD.N_Inert] = gui_get_reactant(self, self.ind_Inert);
+function app = gui_get_reactants(obj, event, app)
+    % Get the species and the number of moles of the current data in
+    % UITable_R
+
+    % Get indexes of the different type of species in the mixture
+    obj = gui_get_typeSpecies(obj);
+    % Get species in the mixture
+    species = obj.UITable_R.Data(:, 1);
+    % Get number of moles of each species in the mixture
+    moles = gui_get_moles(obj, event);
+    % Set mixture into variable app
+    app = gui_set_species(obj, app, species, moles, 'S_Fuel', 'N_Fuel', 'ind_Fuel');
+    app = gui_set_species(obj, app, species, moles, 'S_Oxidizer', 'N_Oxidizer', 'ind_Oxidizer');
+    app = gui_set_species(obj, app, species, moles, 'S_Inert', 'N_Inert', 'ind_Inert');
+    % Compute proportion inerts/O2
+    app = compute_proportion_inerts_O2(app);
 end
 
 % SUB-PASS FUNCTIONS
-function [S, N] = gui_get_reactant(self, ind)
-    if sum(self.(ind))
-        S = regexprep(self.UITable_R.Data(self.(ind), 1),'\s+','')';
-        N = self.UITable_R.Data{self.(ind), 2};
-    else
-        S = [];
-        N = [];
+function obj = gui_get_typeSpecies(obj)
+    % Function that obtains the indexes of the different type of species in
+    % the mixture
+    typeSpecies = obj.UITable_R.Data(:, 4);
+    obj.ind_Fuel     = contains(typeSpecies, 'Fuel');
+    obj.ind_Oxidizer = contains(typeSpecies, 'Oxidizer');
+    obj.ind_Inert    = contains(typeSpecies, 'Inert');
+end
+
+function app = gui_set_species(obj, app, species, moles, species_name, moles_name, ind_name)
+    % Get the species and the number of moles of the current data in
+    % UITable_R for a given category (Fuel, Oxidizer, Inert)
+    app.PD.(species_name) = species(obj.(ind_name))';
+    try
+        app.PD.(moles_name) = cell2vector(moles(obj.(ind_name)));
+    catch 
+        app.PD.(moles_name) = [];
+    end
+end
+
+function moles = gui_get_moles(obj, event)
+    % Get number of moles of each species in the mixture
+    if event.Indices(1, 2) ~= 3 
+        moles = obj.UITable_R.Data(:, 2);
+    else % compute moles from mole fractions
+        total_moles = sum(cell2vector(obj.UITable_R.Data(:, 2)));
+        mole_fractions = cell2vector(obj.UITable_R.Data(:, 3));
+        moles = mole_fractions * total_moles;
     end
 end

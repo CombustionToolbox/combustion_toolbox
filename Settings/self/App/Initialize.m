3aff652b1927cbacb007a20b3522a7603710829d
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-13 10:07:44 +0100

    Update: set matrix properties size from variable
    Former-commit-id: 90227d1e3c76c740ab6cb3368c3f7cea90799681
    Former-commit-id: 9318800540534df57eeeb3ad6a32f5a43981c685

--- Settings/self/App/Initialize.m
+++ Settings/self/App/Initialize.m
@@ -47,7 +47,7 @@ end
 function self = Stoich_Matrix(self)
     % Create stoichiometric matrix
     self.C.A0.value = zeros(self.S.NS, self.E.NE);
-    self.C.M0.value = zeros(self.S.NS, 12);
+    self.C.M0.value = zeros(self.S.NS, self.C.N_prop.value);
     for i=1:self.S.NS
         txFormula = self.DB.(self.S.LS{i}).txFormula;
         self.DB.(self.S.LS{i}).Element_matrix = set_element_matrix(txFormula,self.E.elements);

6356145a549da64fb1f7388300dcdd89016719f0
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-13 20:58:08 +0100

    Solve: element of reactant not contained in list

--- Settings/functions/Check_FOI.m
+++ Settings/functions/Check_FOI.m
@@ -7,12 +7,14 @@ function self = Check_FOI(self, FOI_species)
             if ~strcmp(self.S.LS, FOI_species(i))                 
                 self.S.LS = [self.S.LS, FOI_species(i)];
                 self.S.NS = length(self.S.LS);
+                self.S.LS_formula = [self.S.LS_formula, self.DB.(FOI_species{i}).txFormula];
                 self.Misc.FLAG_ADDED_SPECIES = true;
             end
         end
         if self.Misc.FLAG_ADDED_SPECIES
             self.S.ind_nswt = [];
             self.S.ind_swt = [];
+            self = ContainedElements(self);
             self = Initialize(self);
             self.S.ind_nswt = intersect(self.S.ind_nswt, self.Misc.index_LS_original);
             self.S.ind_swt = intersect(self.S.ind_swt, self.Misc.index_LS_original);

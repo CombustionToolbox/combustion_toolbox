48e5f436fd51901714331d3fee4ed7ff871d4ed2
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-17 10:35:47 +0100

    Solve: error wrong value
    Former-commit-id: 1e9e5c6cbd6478be5aba99b6917c67ba485916ab
    Former-commit-id: 3747f23dbfa887fbac614f1ce52fec1093915929

--- Settings/functions/compute_proportion_inerts_O2.m
+++ Settings/functions/compute_proportion_inerts_O2.m
@@ -4,5 +4,5 @@ function self = compute_proportion_inerts_O2(self)
     % Find index O2
     ind_O2 = find_ind(self.PD.S_Oxidizer, 'O2');
     % Compute proportion inerts/O2
-    self.PD.proportion_inerts_O2 = self.PD.N_Inert./(sum(self.PD.N_Inert) + self.PD.N_Oxidizer(ind_O2));
+    self.PD.proportion_inerts_O2 = self.PD.N_Inert./self.PD.N_Oxidizer(ind_O2);
 end

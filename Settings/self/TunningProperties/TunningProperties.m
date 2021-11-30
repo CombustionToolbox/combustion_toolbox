e16b00d068fb2e4970d5dca829ade888525c5e6e
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-30 01:26:38 +0100

    Update: remove deprecated
    Former-commit-id: 5703de9bec72720b6a931716279746edc647f2a1

--- Settings/self/TunningProperties/TunningProperties.m
+++ Settings/self/TunningProperties/TunningProperties.m
@@ -9,12 +9,6 @@ function self = TunningProperties()
     self.root_T0_r = 1500;      % First guess T[K] right branch - root finding method
     self.root_T0   = 2000;      % Guess T[K] if it's of previous range - root finding method
     self.tol_shocks = 5e-5;     % Tolerance of shocks routines
-    self.it_shocks = 50;        % Max number of iterations - shocks and detonations
+    self.it_shocks = 30;        % Max number of iterations - shocks and detonations
     self.volumeBoundRation = 5; % Initial guess ratio shocks
-    % Deprecated
-    self.factor_c = 1.0;              % Tunning factor to adjust the theoretical soot equivalence ratio
-    self.guess = [2000,2000,0,1.5,2]; % Initial guess shocks (deprecated - SD routines)
-    self.ERRFT = 1e-6;                % Tolerances [shocks - deprecated]
-    self.ERRFV = 1e-6;                % Tolerances [shocks - deprecated]
-    self.ERRFU = 1e-4;                % Tolerances [detonations - deprecated]
 end

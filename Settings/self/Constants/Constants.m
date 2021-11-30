34d181c4554fda036c898f9e9a207d91396b0a96
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-11-30 01:26:13 +0100

    Update: add more digits to Universal gas constant
    Former-commit-id: cd760a02f59e8a6272724ed39c00cc5107d2c75f
    Former-commit-id: f3d80994d9ef0a00d32f31107c88f0bc4b177ca9

--- Settings/self/Constants/Constants.m
+++ Settings/self/Constants/Constants.m
@@ -1,6 +1,6 @@
 function self = Constants()
     self.description = "Constants and tolerances";
-    self.R0 = 8.3144598;  % [J/(K mol)]. Universal gas constant
+    self.R0 = 8.31446261815324;  % [J/(K mol)]. Universal gas constant
     self.A0.description = "Stoichiometric Matrix: number of atoms of each element contained in each species";
     self.A0.value = [];
     self.M0.description = "Matrix with properties of each species";

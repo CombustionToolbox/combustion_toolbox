c346c497cd2a83d600771831228f431da9e36677
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-12-01 23:52:24 +0100

    Add: plot molar fraction of 2 different sources
    Former-commit-id: 8392e7620cc4eb9ba24f27a5a1b903ae786d9e4a

---
+++ Validations/Functions/plot_molar_fractions_validation.m
@@ -0,0 +1,56 @@
+function plot_molar_fractions_validation(results1, results2, varname_x, varname_y, varargin)
+    % Default values
+    nfrec = 1;
+    mintol_display = 1e-14;
+    config.fontsize = 18;
+    config.linewidth = 1.8;
+    
+    dataname_x = get_dataname(varname_x);
+    dataname_y = get_dataname(varname_y);
+    results1.(varname_x) = cell2vector(select_data(results1, dataname_x), varname_x);
+    results1.(varname_y) = cell2vector(select_data(results1, dataname_y), varname_y);
+    LS = results1.Misc.LS_original;
+
+    f = figure;
+    set(f,'units','normalized','innerposition',[0.05 0.05 0.9 0.9],...
+        'outerposition',[0.05 0.05 0.9 0.9]);
+    axes = gca;
+    set(axes,'LineWidth',config.linewidth,'FontSize',config.fontsize-2,'BoxStyle','full')
+    grid(axes, 'off'); box(axes, 'off'); hold(axes, 'on'); axes.Layer = 'Top';
+    xlabel(axes, 'Equivalence ratio, $\phi$','FontSize',config.fontsize,'interpreter','latex');
+    ylabel(axes, 'Molar fraction, $X_i$','FontSize',config.fontsize,'interpreter','latex');
+    set(axes,'yscale','log')
+    xlim(axes, [min(results1.(varname_x)), max(results1.(varname_x))])
+    ylim(axes, [mintol_display, 1])
+    colorbw = brewermap(length(LS), 'Spectral');
+    for i=1:length(LS)
+        plot(results1.(varname_x), results1.(varname_y)(i, :), 'LineWidth', config.linewidth, 'color', colorbw(i,:));
+    end
+    for i=1:length(LS)
+        plot(results2.(varname_x)(1:nfrec:end), results2.(varname_y)(i, 1:nfrec:end), 'd', 'LineWidth', config.linewidth, 'color', colorbw(i,:));
+    end
+    legendname = LS;
+    legend(legendname, 'FontSize', config.fontsize-6, 'Location', 'northeastoutside', 'interpreter', 'latex');
+end
+
+function dataname = get_dataname(var)
+    if strcmpi(var, 'phi')
+        dataname = 'PD.phi.value';
+    else
+        dataname = 'PS.strP';
+    end
+end
+
+function dataselected = select_data(self, dataname)
+    index = strfind(dataname, '.');
+    index = [index, length(dataname) + 1];
+    N = length(index);
+    dataselected = self;
+    pos1 = 1;
+    for i = 1:N
+        pos2 = index(i) - 1;
+        varname = dataname(pos1:pos2);
+        dataselected = dataselected.(varname);
+        pos1 = index(i) + 1;
+    end
+end

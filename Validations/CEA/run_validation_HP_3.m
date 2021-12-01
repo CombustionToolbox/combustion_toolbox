548c6d58ef8f96a1f1a69f1cfef76df3876acd9e
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-12-01 23:53:14 +0100

    Add: validation routines

---
+++ Validations/CEA/run_validation_HP_3.m
@@ -0,0 +1,29 @@
+function run_validation_HP_3
+    % Run test validation_HP_3:
+    % Contrasted with: NASA's Chemical Equilibrium with Applications software
+    % Problem type: Adiabatic T and composition at constant p
+    % Temperature [K]   = 300;
+    % Pressure    [bar] = 1.01325;
+    % Equivalence ratio [-] = 0.5:0.01:4
+    % Initial mixture: C2H2_acetylene + AIR_IDEAL (79% N2 + 21% O2)
+    % List of species considered:
+    %  {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
+    %   'HCN','H','OH','O','CN','NH3','CH4','C2H4','CH3',...
+    %   'NO','HCO','NH2','NH','N','CH','Cbgrb'}
+    
+    % Inputs
+    Fuel = 'CH4';
+    prefixDataName = Fuel;
+    filename = {strcat(prefixDataName, '_air1_HP.out'), strcat(prefixDataName, '_air1_HP2.out'), strcat(prefixDataName, '_air1_HP3.out')};
+    LS =  {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
+           'HCN','H','OH','O','CN','NH3','CH4','C2H4','CH3',...
+           'NO','HCO','NH2','NH','N','CH','Cbgrb'};
+    % Combustion Toolbox
+    results_CT = run_CT('ListSpecies', LS, 'S_Fuel', Fuel,...
+                        'S_Oxidizer', 'O2', 'S_Inert', 'N2',...
+                        'EquivalenceRatio', 0.5:0.01:4);
+    % Load results CEA 
+    results_CEA = data_CEA(filename, LS);
+    % Display validation (plot)
+    plot_molar_fractions_validation(results_CT, results_CEA, 'phi', 'Xi');
+end

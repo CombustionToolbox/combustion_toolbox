fb164edfb869681e4275bbddaba2608a7692c66a
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-12-02 09:20:06 +0100

    Update: include ListSpecies and DisplaySpecies
    Former-commit-id: c21a0cff2e3c0b4c8931d3b5df24d306acb32927
    Former-commit-id: d4fa6ecba9a30daac94f856dfbe775d6f3794c8b

--- Validations/CEA/run_validation_HP_1.m
+++ Validations/CEA/run_validation_HP_1.m
@@ -3,27 +3,25 @@ function run_validation_HP_1
     % Contrasted with: NASA's Chemical Equilibrium with Applications software
     % Problem type: Adiabatic T and composition at constant p
     % Temperature [K]   = 300;
-    % Pressure    [bar] = 1.01325;
+    % Pressure    [bar] = 1;
     % Equivalence ratio [-] = 0.5:0.01:4
     % Initial mixture: C2H2_acetylene + AIR_IDEAL (79% N2 + 21% O2)
-    % List of species considered:
-    %  {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
-    %   'HCN','H','OH','O','CN','NH3','CH4','C2H4','CH3',...
-    %   'NO','HCO','NH2','NH','N','CH','Cbgrb'}
+    % List of species considered: ListSpecies('Soot Formation Extended')
     
     % Inputs
     Fuel = 'C2H2_acetylene';
     prefixDataName = 'C2H2';
     filename = {strcat(prefixDataName, '_air1_HP.out'), strcat(prefixDataName, '_air1_HP2.out'), strcat(prefixDataName, '_air1_HP3.out')};
-    LS =  {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
-           'HCN','H','OH','O','CN','NH3','CH4','C2H4','CH3',...
-           'NO','HCO','NH2','NH','N','CH','Cbgrb'};
+    LS =  'Soot Formation Extended';
+    DisplaySpecies = {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
+                      'HCN','H','OH','O','CN','NH3','CH4','C2H4','CH3',...
+                      'NO','HCO','NH2','NH','N','CH','Cbgrb'};
     % Combustion Toolbox
     results_CT = run_CT('ListSpecies', LS, 'S_Fuel', Fuel,...
                         'S_Oxidizer', 'O2', 'S_Inert', 'N2',...
                         'EquivalenceRatio', 0.5:0.01:4);
     % Load results CEA 
-    results_CEA = data_CEA(filename, LS);
+    results_CEA = data_CEA(filename, DisplaySpecies);
     % Display validation (plot)
-    plot_molar_fractions_validation(results_CT, results_CEA, 'phi', 'Xi');
+    plot_molar_fractions_validation(results_CT, results_CEA, 'phi', 'Xi', DisplaySpecies);
 end

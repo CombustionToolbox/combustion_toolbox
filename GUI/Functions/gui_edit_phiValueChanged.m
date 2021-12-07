22a99e994594ed2f3efcd0d9a1586b3e4e33cf17
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-12-07 20:16:26 +0100

    Add: compute equilibrium with complete reaction
    Former-commit-id: 37ebe00f1afc2a47dbd59ff3901b9a1bd59bf290
    Former-commit-id: 4122c33eeb160ea9efc1238d0459a89c14ee55a5

--- GUI/Functions/gui_edit_phiValueChanged.m
+++ GUI/Functions/gui_edit_phiValueChanged.m
@@ -1,4 +1,4 @@
-function gui_edit_phiValueChanged(obj, event)
+function [obj, temp_app] = gui_edit_phiValueChanged(obj, event)
     % Update moles and mole fractions of the reactant UITables for the
     % given equivalence ratio
     try
@@ -7,24 +7,43 @@ function gui_edit_phiValueChanged(obj, event)
             return
         end
         % Initialize app (fast: transfer DB)
-        app = App('fast', obj.DB_master, obj.DB);
+        temp_app = App('fast', obj.DB_master, obj.DB);
         % Set FLAG compute moles from equivalence ratio
         FLAG_COMPUTE_FROM_PHI = true;
         % Get reactant species
-        app = gui_get_reactants(obj, event, app, FLAG_COMPUTE_FROM_PHI);
+        temp_app = gui_get_reactants(obj, event, temp_app, FLAG_COMPUTE_FROM_PHI);
         % Compute properties of the mixture
-        app = gui_compute_propReactants(obj, app);
+        temp_app = gui_compute_propReactants(obj, temp_app);
         % Update UITable classes
-        gui_update_UITable_R(obj, app);
+        gui_update_UITable_R(obj, temp_app);
         obj.UITable_P.Data = obj.UITable_R.Data(:, 1);    % (species, numer of moles, mole fractions, temperature)
         obj.UITable_R2.Data = obj.UITable_R.Data(:, 1:3); % (species, numer of moles, mole fractions)
         % Update GUI: equivalence ratio, O/F and percentage Fuel
         obj.edit_phi2.Value = obj.edit_phi.Value;
-        obj.edit_OF.Value = 1/app.PS.strR{1}.FO;
-        obj.edit_F.Value = app.PS.strR{1}.percentage_Fuel;
+        obj.edit_OF.Value = 1/temp_app.PS.strR{1}.FO;
+        obj.edit_F.Value = temp_app.PS.strR{1}.percentage_Fuel;
+        % Check List of products (only in case of ListProducts == Complete reaction)
+        temp_app = check_ListProducts(obj, temp_app);
     catch ME
         message = {sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
         ME.stack(1).name, ME.stack(1).line, ME.message)};
         uialert(obj.UIFigure, message, 'Warning', 'Icon', 'warning');
+    end
+end
+
+function app = check_ListProducts(obj, app)
+    if strcmpi(obj.Products.Value, 'Complete Reaction')
+        if isempty(obj.UITable_R.Data)
+            % Set lamp to Error color
+            obj.Lamp.Color = obj.color_lamp_warning;
+            % Print error
+            message = {'First, define initial mixture'};
+            uialert(obj.UIFigure, message, 'Warning', 'Icon', 'warning');
+        end
+        % Update List of Products depending of the value of the equivalence ratio
+        phi = app.PS.strR{1}.phi;
+        phi_c = app.PS.strR{1}.phi_c;
+        app = ListSpecies(obj, obj.Products.Value, phi, phi_c);
+        obj.listbox_Products.Items = app.S.LS;
     end
 end

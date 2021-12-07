ad9fa6d07e10556c15e5afb9091a0dc2e4399749
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-12-07 20:20:09 +0100

    Update: comments
    Former-commit-id: 422c2de8279832e71f74592049d6c5645b57329d

--- Settings/functions/Display/results.m
+++ Settings/functions/Display/results.m
@@ -1,9 +1,11 @@
 function results(self, i)
+    % Display results in the command window
     if self.Misc.FLAG_RESULTS
         if ~strcmp(self.PD.ProblemType,'SHOCK_R') 
-            displayresults(self.PS.strR{i},self.PS.strP{i},self.PD.ProblemType,self.C.mintol_display,self.S.LS);
+            displayresults(self.PS.strR{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);
         else
-            displayresults(self.PS.strR{i},self.PS.str2{i},self.PS.strP{i},self.PD.ProblemType,self.C.mintol_display,self.S.LS); % Display all results SHOCK_R
+            % Display all results SHOCK_R
+            displayresults(self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);
         end
     end
 end

bec665dbcfb0f625454d65d2bb4d427ee45863aa
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2021-12-08 11:56:23 +0100

    Add: compute range of Mach or velocity (GUI)

--- GUI/Functions/gui_compute_mach_or_velocity.m
+++ GUI/Functions/gui_compute_mach_or_velocity.m
@@ -2,17 +2,28 @@ function gui_compute_mach_or_velocity(obj, inputname)
     % Function that computes the pre-shock Mach number of the mixture from 
     % a given pre-shock velocity or viceversa.
     if any(contains(obj.ProblemType.Value, 'SHOCK', 'IgnoreCase', true))
-        temp_app = gui_create_temp_app(obj, [], false);
-        mix1 = temp_app.PS.strR{1};
-
-        if strcmpi(inputname, 'Mach')
-            M1 = gui_get_prop(obj, 'M1', obj.PR4.Value);
-            u1 = M1 * soundspeed(mix1);
-            obj.PR3.Value = sprintf('%.6g', u1);
-        else
-            u1 = gui_get_prop(obj, 'u1', obj.PR3.Value);
-            M1 = u1 / soundspeed(mix1);
-            obj.PR4.Value = sprintf('%.6g', M1);
+        if ~isempty(obj.UITable_R.Data)
+            temp_app = gui_create_temp_app(obj, [], false);
+            mix1 = temp_app.PS.strR{1};
+    
+            if strcmpi(inputname, 'Mach')
+                [M1, FLAG] = gui_get_prop(obj, 'M1', obj.PR4.Value);
+                u1 = M1 * soundspeed(mix1);
+                obj.PR3.Value = compute_vector_or_scalar(u1, FLAG);
+            else
+                [u1, FLAG] = gui_get_prop(obj, 'u1', obj.PR3.Value);
+                M1 = u1 / soundspeed(mix1);
+                obj.PR4.Value = compute_vector_or_scalar(M1, FLAG);
+            end
         end
+    end
+end
+
+% SUB-PASS FUNCTIONS
+function value_char = compute_vector_or_scalar(value, FLAG)
+    if FLAG
+        value_char = sprintf('[%.5g:%.5g:%.5g]', value(1), value(2) - value(1), value(end));
+    else
+        value_char = sprintf('%.5g', value);
     end
 end

825ef5c6bb420395e40afcab6a9828912017aa36
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-02-08 14:36:57 +0100

    Add: include messages in the command window

--- GUI/Functions/gui_CalculateButtonPushed.m
+++ GUI/Functions/gui_CalculateButtonPushed.m
@@ -10,6 +10,8 @@ function obj = gui_CalculateButtonPushed(obj, event)
         app = App('fast', obj.DB_master, obj.DB, obj.LS);
         % Set FLAG GUI
         app.Misc.FLAG_GUI = true;
+        % Update GUI terminal
+        gui_update_terminal(obj, app, 'start');
         % Get tolerances and tunning values
         app = get_tunning_values(obj, app);
         % Get initial conditions
@@ -21,6 +23,8 @@ function obj = gui_CalculateButtonPushed(obj, event)
         results = save_results(obj, app);
         % Update GUI with the last results of the set
         obj = gui_update_results(obj, results);
+        % Update GUI terminal
+        gui_update_terminal(obj, app, 'finish')
         % Display results (plots)
         postResults(app);
         % Set lamp to Done color
@@ -87,37 +91,29 @@ function results = save_results(obj, app)
 end
 
 function app = get_input_constrains(obj, app)
-    [app.PD.TR.value, app.FLAG_PR1] = gui_get_prop(obj, 'TR', obj.PR1.Value);
-    [app.PD.pR.value, app.FLAG_PR2] = gui_get_prop(obj, 'pR', obj.PR2.Value);
-    [app.PD.phi.value, app.FLAG_phi] = gui_get_prop(obj, 'phi', obj.edit_phi.Value);
+    % Get input constrains
     app.PD.ProblemType = obj.ProblemType.Value;
+    app = set_prop(app, 'TR', obj.PR1.Value, 'pR', obj.PR2.Value, 'phi', obj.edit_phi.Value);
     switch app.PD.ProblemType
         case 'TP' % * TP: Equilibrium composition at defined T and p
-            [app.PD.TP.value, app.FLAG_PP1] = gui_get_prop(obj, 'TP', obj.PP1.Value);
-            [app.PD.pP.value, app.FLAG_PP2] = gui_get_prop(obj, 'pP', obj.PP2.Value);
+            app = set_prop(app, 'TP', obj.PP1.Value, 'pP', obj.PP2.Value);
         case 'HP' % * HP: Adiabatic T and composition at constant p
-            [app.PD.pP.value, app.FLAG_PP2] = gui_get_prop(obj, 'pP', obj.PP2.Value);
+            app = set_prop(app, 'pP', obj.PP2.Value);
         case 'SP' % * SP: Isentropic (i.e., adiabatic) compression/expansion to a specified p
-            [app.PD.pP.value, app.FLAG_PP2] = gui_get_prop(obj, 'pP', obj.PP2.Value);
-            app.PD.phi.value = 1*ones(1, length(app.PD.pP.value));
+            app = set_prop(app, 'pP', obj.PP2.Value);
         case 'TV' % * TV: Equilibrium composition at defined T and constant v
-            [app.PD.TP.value, app.FLAG_PP1] = gui_get_prop(obj, 'TP', obj.PP1.Value);
-            [app.PD.pP.value, app.FLAG_PP2] = gui_get_prop(obj, 'pP', obj.PP2.Value);
+            app = set_prop(app, 'TP', obj.PP1.Value, 'pP', obj.PP2.Value);
         case 'EV' % * EV: Equilibrium composition at Adiabatic T and constant v
-            [app.PD.pP.value, app.FLAG_PP2] = gui_get_prop(obj, 'pP', obj.PP2.Value);
+            app = set_prop(app, 'pP', obj.PP2.Value);
         case 'SV' % * SV: Isentropic (i.e., fast adiabatic) compression/expansion to a specified v
-            app.PD.vP_vR.value = gui_get_prop(obj, 'vP_vR', obj.PP2.Value);
-            app.PD.phi.value = 1*ones(1, length(app.PD.vP_vR.value));
+            app = set_prop(app, 'vP_vR', obj.PP2.Value);
         case 'SHOCK_I' % * SHOCK_I: CALCULATE PLANAR INCIDENT SHOCK WAVE
-            [app.PD.u1.value, app.FLAG_PR3] = gui_get_prop(obj, 'u1', obj.PR3.Value);
-            app.PD.phi.value = 1*ones(1, length(app.PD.u1.value));
+            app = set_prop(app, 'u1', obj.PR3.Value);
         case 'SHOCK_R' % * SHOCK_R: CALCULATE PLANAR POST-REFLECTED SHOCK STATE
-            [app.PD.u1.value, app.FLAG_PR3] = gui_get_prop(obj, 'u1', obj.PR3.Value);
-            app.PD.phi.value = 1*ones(1, length(app.PD.u1.value));
+            app = set_prop(app, 'u1', obj.PR3.Value);
         case 'DET' % * DET: CALCULATE CHAPMAN-JOUGET STATE (CJ UPPER STATE)
             % No additional constrains
         case 'DET_OVERDRIVEN' % * DET_OVERDRIVEN: CALCULATE OVERDRIVEN DETONATION
-            [app.PD.overdriven.value, app.FLAG_PR3] = gui_get_prop(obj, 'overdriven', obj.PR3.Value);
-            app.PD.phi.value = 1*ones(1, length(app.PD.overdriven.value));
+            app = set_prop(app, 'overdriven', obj.PR3.Value);
     end
 end

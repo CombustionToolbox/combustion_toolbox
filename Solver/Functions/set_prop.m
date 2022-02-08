04a40ef58ac915340fbaf04c86b75e311c6b3a41
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-02-08 14:35:17 +0100

    Solve: error 1 computation
    Former-commit-id: 0fd3b0f108825cbd54c7ba97e0bbbf777528a226

--- Solver/Functions/set_prop.m
+++ Solver/Functions/set_prop.m
@@ -1,4 +1,5 @@
 function self = set_prop(varargin)
+    % Assign property values to the respective variables
     self = varargin{1};
     n = round(nargin/2 - 1);
     try
@@ -12,10 +13,21 @@ end
 
 % SUB-PASS FUNCTIONS
 function self = set_prop_common(self, name, value)
+    % Assign property values to the given variable name
+
+    % If the value is a string, convert it to a float - (GUI)
+    if value(1) == '['
+        value = sscanf(value, '[%f:%f:%f]');
+        value = value(1):value(2):value(3);
+    elseif ischar(value)
+        value = sscanf(value,'%f');
+    end
+    % Define flags
     if length(value) > 1
         self.Misc.FLAGS_PROP.(name) = true;
     else
         self.Misc.FLAGS_PROP.(name) = false;
     end
+    % Set value
     self.PD.(name).value = value;
 end

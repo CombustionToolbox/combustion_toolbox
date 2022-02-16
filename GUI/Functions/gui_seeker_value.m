6d1b233c15ef1c16333ae9e8ddab1790021b6632
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-02-16 21:24:10 +0100

    Update:  get list of values

--- GUI/Functions/gui_seeker_value.m
+++ GUI/Functions/gui_seeker_value.m
@@ -2,14 +2,15 @@ function value = gui_seeker_value(obj, event, ListValues)
     % Return the value that match with the value introduced in the finder
     try
         seekValue = event.Value;
-        index = 0;
-        seekIndex = false;
-        while ~seekIndex
-            index = index + 1;
-            seekIndex = startsWith(ListValues{index}, seekValue, 'IgnoreCase', false);
+        if isempty(seekValue)
+            value = [];
+            return
         end
-        if index
-            value = ListValues{index}; % Value found in ListValues
+        for i = length(ListValues):-1:1
+            seekIndex(i) = startsWith(ListValues{i}, seekValue, 'IgnoreCase', false);
+        end
+        if any(seekIndex)
+            value = ListValues(seekIndex); % Value found in ListValues
         else
             value = []; % Value not found in ListValues
         end

4797ec294b806932f2c7c370da6c0773ac87a4c4
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-02-18 21:07:13 +0100

    Solve: error computing SP and SV #38

--- Solver/Functions/root_finding/steffenson/get_gpoint.m
+++ Solver/Functions/root_finding/steffenson/get_gpoint.m
@@ -1,5 +1,9 @@
 function [gpoint, gpoint_relative]= get_gpoint(self, strR, pP, attr_name, x)
     strP = equilibrate_T(self, strR, pP, x);
-    gpoint = strP.(attr_name) - strR.(attr_name);
-    gpoint_relative = gpoint / strP.(attr_name);
+    gpoint = (strP.(attr_name) - strR.(attr_name));
+    gpoint_relative = gpoint / (strP.(attr_name));
+    if strcmpi(attr_name, 's')
+        gpoint = gpoint * 1e3;
+        gpoint_relative = gpoint_relative * 1e-3;
+    end
 end

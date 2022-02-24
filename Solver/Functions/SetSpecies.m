951031a703277d3c0d5eb7a1f752aade36c9573c
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-02-24 15:31:47 +0100

    Update: reorganize equations
    Former-commit-id: bba10a4ef03506bd344e25c6197de652e390ff87
    Former-commit-id: aa1a11ef290bf7dea66c19b48f6d534833bc7858

--- Solver/Functions/SetSpecies.m
+++ Solver/Functions/SetSpecies.m
@@ -14,29 +14,29 @@ M = self.C.M0.value;
 R0 = self.C.R0;
 
 for n = length(N):-1:1
-    mi = N(n) * self.DB.(species{n}).mm;
-    mmi = self.DB.(species{n}).mm;
-    hfi = self.DB.(species{n}).hf/1000;
-    efi = self.DB.(species{n}).ef/1000;
-    swtCondensed = self.DB.(species{n}).swtCondensed;
+    mi = N(n) * self.DB.(species{n}).mm * 1e-3;       % [kg]
+    mmi = self.DB.(species{n}).mm;                    % [g/mol]
+    hfi = self.DB.(species{n}).hf/1000;               % [kJ]
+    efi = self.DB.(species{n}).ef/1000;               % [kJ]
+    swtCondensed = self.DB.(species{n}).swtCondensed; % [bool]
     if length(self.DB.(species{n}).T) > 1
-        DhTi = species_DhT(species{n},T,self.DB);
-        DeTi = species_DeT(species{n},T,self.DB);
-        cPi  = species_cP(species{n},T,self.DB);
-        cVi  = species_cV(species{n},T,self.DB);
-        s0i  = species_s0(species{n},T,self.DB);
+        DhTi = species_DhT(species{n},T,self.DB);     % [kJ]
+        DeTi = species_DeT(species{n},T,self.DB);     % [kJ]
+        cPi  = species_cP(species{n},T,self.DB);      % [J/K]
+        cVi  = species_cV(species{n},T,self.DB);      % [J/K]
+        s0i  = species_s0(species{n},T,self.DB);      % [kJ/K]
         if ~swtCondensed
-            pVi = N(n)*R0*T/100; % For ideal gases
+            pVi = N(n)*R0*T * 1e-5; % For ideal gases [bar m3]
         else
-            pVi = 0; % For condensed species
+            pVi = 0; % For condensed species [bar m3]
         end
     else
-        DhTi = 0;
-        DeTi = 0;
-        cPi  = 0;
-        cVi  = 0;
-        s0i  = 0;
-        pVi  = 0;
+        DhTi = 0; % [kJ]
+        DeTi = 0; % [kJ]
+        cPi  = 0; % [J/K]
+        cVi  = 0; % [J/K]
+        s0i  = 0; % [kJ/K]
+        pVi  = 0; % [bar m3]
     end   
     ind = find_ind(self.S.LS, species(n));
     M(ind, :) = [N(n), N(n) * [hfi, DhTi, efi, DeTi, cPi, cVi, s0i], pVi, swtCondensed, mi, mmi];

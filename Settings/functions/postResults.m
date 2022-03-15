3c749289b923932118ac0f774d9829b5eede3293
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-15 07:55:02 +0100

    Update: miscellaneous
    Former-commit-id: 20fea4d81be9ce8634de2c6954a66bfbf2203def
    Former-commit-id: 253d1b65d9da9003172728e80a4242e72c2b1f49

--- Settings/functions/postResults.m
+++ Settings/functions/postResults.m
@@ -1,120 +1,139 @@
-function postResults(app)
+function postResults(self)
 % Abbreviations ---------------------
-mix1 = app.PS.strR;
-mix2 = app.PS.strP;
-phi = app.PD.phi.value;
-timer_0 = app.Misc.timer_0;
-ProblemType = app.PD.ProblemType;
+mix1 = self.PS.strR;
+mix2 = self.PS.strP;
+phi = self.PD.phi.value;
+timer_0 = self.Misc.timer_0;
+ProblemType = self.PD.ProblemType;
 % -----------------------------------
-app.Misc.timer_loop = toc(timer_0);
+self.Misc.timer_loop = toc(timer_0);
 
-fprintf('Elapsed time is %.6g seconds\n', app.Misc.timer_loop);
+fprintf('Elapsed time is %.6g seconds\n', self.Misc.timer_loop);
 
-app.Misc.config.tit = ProblemType;
+self.Misc.config.tit = ProblemType;
 FLAG_PLOT_PHI = numel(phi)>1 && all(phi(2:end) ~= phi(1));
 
-if ~isfield(app.Misc.FLAGS_PROP, 'TP')
-    app.Misc.FLAGS_PROP.TP = false;
+if ~isfield(self.Misc.FLAGS_PROP, 'TP')
+    self.Misc.FLAGS_PROP.TP = false;
 end
 
 % PLOTS REACTANTS
-if app.Misc.FLAGS_PROP.TR && length(phi) > 1
-    app.Misc.config.labelx = 'Temperature reactants $T$ [K]';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
-    displaysweepresults(app, mix2, app.PD.range);
-elseif app.Misc.FLAGS_PROP.pR && length(phi) > 1
-    app.Misc.config.labelx = 'Pressure reactants $p$ [bar]';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
-    displaysweepresults(app, mix2, app.PD.range);
-elseif isfield(app.Misc.FLAGS_PROP, 'pP')
-    if app.Misc.FLAGS_PROP.pP && length(phi) > 1
-        app.Misc.config.labelx = 'Pressure $p$ [bar]';
-        app.Misc.config.labely = 'Molar fraction $X_i$';
-        displaysweepresults(app, mix2, app.PD.range);
+if self.Misc.FLAGS_PROP.TR && length(phi) > 1
+    self.Misc.config.labelx = 'Temperature reactants $T$ [K]';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
+    displaysweepresults(self, mix2, self.PD.range);
+elseif self.Misc.FLAGS_PROP.pR && length(phi) > 1
+    self.Misc.config.labelx = 'Pressure reactants $p$ [bar]';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
+    displaysweepresults(self, mix2, self.PD.range);
+elseif isfield(self.Misc.FLAGS_PROP, 'pP')
+    if self.Misc.FLAGS_PROP.pP && length(phi) > 1
+        self.Misc.config.labelx = 'Pressure $p$ [bar]';
+        self.Misc.config.labely = 'Molar fraction $X_i$';
+        displaysweepresults(self, mix2, self.PD.range);
     end
 end
 
 % PLOTS PRODUCTS
 if ~strcmp(ProblemType,'DET_OVERDRIVEN') && FLAG_PLOT_PHI
-    app.Misc.config.labelx = 'Equivalence Ratio $\phi$';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
-    displaysweepresults(app, mix2, phi);
+    self.Misc.config.labelx = 'Equivalence Ratio $\phi$';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
+    displaysweepresults(self, mix2, phi);
     if ~any(strcmp(ProblemType,{'TP','TV'}))
-        app.Misc.config.labely = 'Temperature $T$ [K]';
-        plot_figure(phi, mix2,'phi','T',app.Misc.config,app.PD.CompleteOrIncomplete);
+        self.Misc.config.labely = 'Temperature $T$ [K]';
+        plot_figure(phi, mix2,'phi','T',self.Misc.config,self.PD.CompleteOrIncomplete);
     end
 
-elseif app.Misc.FLAGS_PROP.TP && length(phi) > 1
-    app.Misc.config.labelx = 'Temperature $T$ [K]';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
-    displaysweepresults(app, mix2, app.PD.range);
+elseif self.Misc.FLAGS_PROP.TP && length(phi) > 1
+    self.Misc.config.labelx = 'Temperature $T$ [K]';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
+    displaysweepresults(self, mix2, self.PD.range);
 
 elseif (strcmp(ProblemType,{'HP'}) || strcmp(ProblemType,{'EV'})) && length(phi) > 1
-    app.Misc.config.labelx = 'Adiabatic flame temperature $T$ [K]';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
+    self.Misc.config.labelx = 'Adiabatic flame temperature $T$ [K]';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
     T = cell2vector(mix2, 'T');
-    displaysweepresults(app, mix2, T);
+    displaysweepresults(self, mix2, T);
 
 elseif strcmp(ProblemType,{'SP'}) && length(phi) > 1
-    app.Misc.config.labelx = 'Pressure $p$ [bar]';
-    app.Misc.config.labely = 'Temperature $T$ [K]';
-    plot_figure(app.PD.pP.value, mix2,'p','T',app.Misc.config,app.PD.CompleteOrIncomplete);
+    self.Misc.config.labelx = 'Pressure $p$ [bar]';
+    self.Misc.config.labely = 'Temperature $T$ [K]';
+    plot_figure(self.PD.pP.value, mix2,'p','T',self.Misc.config,self.PD.CompleteOrIncomplete);
 
 elseif strcmp(ProblemType,{'SV'}) && length(phi) > 1
-    app.Misc.config.labelx = 'Volume ratio $v_P/v_R$';
-    app.Misc.config.labely = 'Temperature $T$ [K]';
-    plot_figure(app.PD.vP_vR.value, mix2,'v_P/v_R','T',app.Misc.config,app.PD.CompleteOrIncomplete);  
+    self.Misc.config.labelx = 'Volume ratio $v_P/v_R$';
+    self.Misc.config.labely = 'Temperature $T$ [K]';
+    plot_figure(self.PD.vP_vR.value, mix2,'v_P/v_R','T',self.Misc.config,self.PD.CompleteOrIncomplete);  
     T = cell2vector(mix2, 'T');
-    displaysweepresults(app, mix2, T);
+    displaysweepresults(self, mix2, T);
     
 elseif strcmp(ProblemType,{'SHOCK_I'}) && length(phi) > 1
-    app.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
-    app.Misc.config.labely = 'Temperature $T$ [K]';
-    plot_figure(app.PD.u1.value, mix2,'u','T',app.Misc.config,app.PD.CompleteOrIncomplete);
-    plot_hugoniot(app, mix1, mix2);
-    app.Misc.config.labelx = 'Temperature $T$ [K]';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
+    self.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
+    self.Misc.config.labely = 'Temperature $T$ [K]';
+    plot_figure(self.PD.u1.value, mix2,'u','T',self.Misc.config,self.PD.CompleteOrIncomplete);
+    plot_hugoniot(self, mix1, mix2);
+    self.Misc.config.labelx = 'Temperature $T$ [K]';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
     T = cell2vector(mix2, 'T');
-    displaysweepresults(app, mix2, T);
-    app.Misc.config.labelx = 'Pressure $p$ [bar]';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
+    displaysweepresults(self, mix2, T);
+    self.Misc.config.labelx = 'Pressure $p$ [bar]';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
     p = cell2vector(mix2, 'p');
-    displaysweepresults(app, mix2, p);
-    app.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
-    app.Misc.config.labely = 'Molar fraction $X_i$';
+    displaysweepresults(self, mix2, p);
+    self.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
+    self.Misc.config.labely = 'Molar fraction $X_i$';
     u = cell2vector(mix1, 'u');
-    displaysweepresults(app, mix2, u);
+    displaysweepresults(self, mix2, u);
 
 elseif strcmp(ProblemType,{'SHOCK_R'}) && length(phi) > 1
     mix3 = mix2;
-    mix2 = app.PS.str2;
-    app.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
-    app.Misc.config.labely = 'Temperature $T$ [K]';
-    app.Misc.config.tit = 'SHOCK_I';
-    ax = plot_figure(app.PD.u1.value, mix2,'u','T',app.Misc.config,app.PD.CompleteOrIncomplete); 
-    app.Misc.config.tit = 'SHOCK_R';
-    plot_figure(app.PD.u1.value,app.PS.strP,'u','T',app.Misc.config,app.PD.CompleteOrIncomplete, ax, {'$SHOCK_I$', '$SHOCK_R$'}); 
-    ax = plot_hugoniot(app, mix1, mix2);
-    ax = plot_hugoniot(app, mix1, mix3, ax);
-    ax = plot_hugoniot(app, mix2, mix3, ax);
+    mix2 = self.PS.str2;
+    self.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
+    self.Misc.config.labely = 'Temperature $T$ [K]';
+    self.Misc.config.tit = 'SHOCK_I';
+    ax = plot_figure(self.PD.u1.value, mix2,'u','T',self.Misc.config,self.PD.CompleteOrIncomplete); 
+    self.Misc.config.tit = 'SHOCK_R';
+    plot_figure(self.PD.u1.value,self.PS.strP,'u','T',self.Misc.config,self.PD.CompleteOrIncomplete, ax, {'$SHOCK_I$', '$SHOCK_R$'}); 
+    ax = plot_hugoniot(self, mix1, mix2);
+    ax = plot_hugoniot(self, mix1, mix3, ax);
+    ax = plot_hugoniot(self, mix2, mix3, ax);
 
 elseif strcmp(ProblemType,{'DET_OVERDRIVEN'}) && length(phi) > 1
-    app.Misc.config.labelx = 'Overdriven ratio $u_1/u_{cj}$';
-    app.Misc.config.labely = 'Temperature $T$ [K]';
-    plot_figure(mix1, mix2, 'overdriven', 'T', app.Misc.config, app.PD.CompleteOrIncomplete);
-    plot_hugoniot(app, mix1, mix2);
+    self.Misc.config.labelx = 'Overdriven ratio $u_1/u_{cj}$';
+    self.Misc.config.labely = 'Temperature $T$ [K]';
+    plot_figure(mix1, mix2, 'overdriven', 'T', self.Misc.config, self.PD.CompleteOrIncomplete);
+    plot_hugoniot(self, mix1, mix2);
 end
 
 if strcmp(ProblemType,{'DET'}) && length(phi) > 1
-    app.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
-    app.Misc.config.labely = 'Temperature $T$ [K]';
-    plot_figure(mix1, mix2, 'u', 'T', app.Misc.config, app.PD.CompleteOrIncomplete);
+    self.Misc.config.labelx = 'Incident velocity $u_1$ [m/s]';
+    self.Misc.config.labely = 'Temperature $T$ [K]';
+    plot_figure(mix1, mix2, 'u', 'T', self.Misc.config, self.PD.CompleteOrIncomplete);
 end
 
 if strcmp(ProblemType,{'ROCKET'}) && length(phi) > 1
-    app.Misc.config.labelx = 'Equivalence Ratio $\phi$';
-    app.Misc.config.labely = 'Characteristic velocity $c^*$ [m/s]';
-    plot_figure(mix1, mix2, 'phi', 'cstar', app.Misc.config, app.PD.CompleteOrIncomplete);
+    self.Misc.config.labelx = 'Equivalence Ratio $\phi$';
+    self.Misc.config.labely = 'Characteristic velocity $c^*$ [m/s]';
+    ax = plot_figure(self.PS.strR, self.PS.strP, 'phi', 'cstar', self.Misc.config, self.PD.CompleteOrIncomplete);
+    legend_name = sprintf('$p = %.2f$ [bar]', self.PS.strR.p); 
+    for i = 2:length(self)
+        legend_name{i} = sprintf('$p = %.2f$ [bar]', self{i}.PS.strR.p); 
+        ax = plot_figure(self{i}.PS.strR, self{i}.PS.strP, 'phi', 'cstar', self.Misc.config, self.PD.CompleteOrIncomplete, ax, legend_name);
+    end
+
+    self.Misc.config.labelx = 'Equivalence Ratio $\phi$';
+    self.Misc.config.labely = 'Temperature $T$ [K]';
+    ax = plot_figure(self.PS.strR, self.PS.strP, 'phi', 'T', self.Misc.config, self.PD.CompleteOrIncomplete);
+    for i = 2:length(self)
+        ax = plot_figure(self{i}.PS.strR, self{i}.PS.strP, 'phi', 'T', self.Misc.config, self.PD.CompleteOrIncomplete, ax, legend_name);
+    end
+
+    self.Misc.config.labelx = 'Equivalence Ratio $\phi$';
+    self.Misc.config.labely = 'Specific impulse $I_sp$ [m/s]';
+    ax = plot_figure(self.PS.strR, self.PS.strP, 'phi', 'I_sp', self.Misc.config, self.PD.CompleteOrIncomplete);
+    for i = 2:length(self)
+        ax = plot_figure(self{i}.PS.strR, self{i}.PS.strP, 'phi', 'I_sp', self.Misc.config, self.PD.CompleteOrIncomplete, ax, legend_name);
+    end
 end
 % EXPORT RESULTS
-export_results(app);
+export_results(self);

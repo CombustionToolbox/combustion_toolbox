5edfcdb5c4a59cad549f3deb8e7e818fec8246bc
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-21 22:02:10 +0100

    Update: distinguish from 2 states or more
    Former-commit-id: 67611be615b81c055a2c4d6ab7f14331ead40512

--- Settings/functions/Display/results.m
+++ Settings/functions/Display/results.m
@@ -1,10 +1,8 @@
 function results(self, i)
     % Display results in the command window
     if self.Misc.FLAG_RESULTS
-        if ~contains(self.PD.ProblemType, '_R') && ~strcmpi(self.PD.ProblemType, 'ROCKET')
+        if ~isfield(self.PS, 'str2')
             displayresults(self.PS.strR{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);
-        elseif strcmpi(self.PD.ProblemType, 'ROCKET') 
-            displayresults(self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);
         else
             % Display results reflected shocks (SHOCK_R, DET_R, or DET_OVERDRIVEN_R)
             displayresults(self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);

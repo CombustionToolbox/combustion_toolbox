5ca163d0e533b95a764c85ed3938f8acacc146ea
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-22 17:59:07 +0100

    Add: include print results for 4 states
    Former-commit-id: cbdcc82c507e5cf6040cb76348f9448753f8887e

--- Settings/functions/Display/results.m
+++ Settings/functions/Display/results.m
@@ -3,9 +3,10 @@ function results(self, i)
     if self.Misc.FLAG_RESULTS
         if ~isfield(self.PS, 'str2')
             displayresults(self.PS.strR{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);
-        else
-            % Display results reflected shocks (SHOCK_R, DET_R, or DET_OVERDRIVEN_R)
+        elseif ~isfield(self.PS, 'str3_2')
             displayresults(self.PS.strR{i}, self.PS.str2{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);
+        else
+            displayresults(self.PS.strR{i}, self.PS.str2{i}, self.PS.str3{i}, self.PS.str3_2{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);
         end
     end
 end

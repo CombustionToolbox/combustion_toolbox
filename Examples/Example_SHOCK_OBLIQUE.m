49cff121f7b8111ff7baf244b09fa5d91f4a1296
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-22 10:53:24 +0100

    Add: compute shock_oblique for a given deflection angle or for a given wave angle #267
    Former-commit-id: 038e34aa1eb21b20355336b2640fece5b8e95dd2
    Former-commit-id: cde0a1d304f6038af4711936e4b0d1ea68a42248

--- Examples/Example_SHOCK_OBLIQUE.m
+++ Examples/Example_SHOCK_OBLIQUE.m
@@ -2,8 +2,9 @@
 % EXAMPLE: SHOCK_OBLIQUE
 %
 % Compute pre-shock and post-shock state for a oblique incident shock wave
-% at standard conditions, a set of 20 species considered and a initial 
-% shock front velocities u1 = 1500 [m/s]
+% at standard conditions, a set of 20 species considered, a initial 
+% shock front velocities u1 = a1 * 10 [m/s], and a deflection angle 
+% theta = 20 [deg]
 %    
 % Air == {'O2','N2','O','O3','N','NO','NO2','NO3','N2O','N2O3','N2O4',...
 %         'N3','C','CO','CO2','Ar','H2O','H2','H','He'}
@@ -27,7 +28,7 @@ self.PD.S_Inert    = {'N2', 'Ar', 'CO2'};
 self.PD.proportion_inerts_O2 = [78.084, 0.9365, 0.0319] ./ 20.9476;
 %% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
 overdriven = 10;
-self = set_prop(self, 'u1', 3.472107491008314e+02 * overdriven, 'theta', 51.2161);
+self = set_prop(self, 'u1', 3.472107491008314e+02 * overdriven, 'theta', 20);
 %% SOLVE PROBLEM
 self = SolveProblem(self, 'SHOCK_OBLIQUE');
 %% DISPLAY RESULTS (PLOTS)

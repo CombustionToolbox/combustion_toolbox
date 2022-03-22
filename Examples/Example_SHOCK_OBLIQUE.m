5e098167e36492c76846f6458c13a57b6346608b
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-22 10:55:50 +0100

    Update

--- Examples/Example_SHOCK_OBLIQUE.m
+++ Examples/Example_SHOCK_OBLIQUE.m
@@ -6,8 +6,12 @@
 % shock front velocities u1 = a1 * 10 [m/s], and a deflection angle 
 % theta = 20 [deg]
 %    
-% Air == {'O2','N2','O','O3','N','NO','NO2','NO3','N2O','N2O3','N2O4',...
-%         'N3','C','CO','CO2','Ar','H2O','H2','H','He'}
+% Air_ions == {'O2','N2','O','O3','N','NO','NO2','NO3','N2O','N2O3',...
+%              'N2O4','N3','eminus','Nminus','Nplus','NOplus','NO2minus',...
+%              'NO3minus','N2plus','N2minus','N2Oplus','Oplus','Ominus',...
+%              'O2plus', 'O2minus,'CO2','CO','COplus','C','Cplus',...
+%              'Cminus','CN','CNplus','CNminus','CNN','NCO','NCN','Ar',...
+%              'Arplus'}
 %   
 % See wiki or ListSpecies() for more predefined sets of species
 %
@@ -15,7 +19,7 @@
 %          PhD Candidate - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                 
-% Last update March 18 2021
+% Last update March 22 2022
 % -------------------------------------------------------------------------
 
 %% INITIALIZE

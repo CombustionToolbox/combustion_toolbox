fa5887b181a9cf58471864749381ce03cfc00c31
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-22 19:25:29 +0100

    Add: include routine that includes the filenames as nodes in a uitreeobject

---
+++ GUI/Functions/gui_add_nodes_validations.m
@@ -0,0 +1,13 @@
+function gui_add_nodes_validations(obj, code_validation_name)
+    % Add nodes with the name of the validations routines
+    try
+        filenames = dir(strcat('.\Validations\', code_validation_name, '\*.m'));
+        for i = 1:length(filenames)
+            uitreenode(obj.(code_validation_name), 'Text', filenames(i).name);
+        end
+    catch ME
+        % Print error
+        fprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
+        ME.stack(1).name, ME.stack(1).line, ME.message);
+    end
+end

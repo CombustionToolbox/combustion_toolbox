00e8587cd38b06256657153e476ad2797e117000
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-23 17:18:39 +0100

    Update: homogenize variable names and few comments
    Former-commit-id: cd376c1b99566738b6b7e654dd6169374d953821
    Former-commit-id: d53aac8bb6518182c230d5b2f134ab813e267dec

--- Solver/Functions/root_finding/newton/newton.m
+++ Solver/Functions/root_finding/newton/newton.m
@@ -1,26 +1,26 @@
-function [x, ERR] = newton(self, mix1, pP, attr_name, x0)
+function [x, STOP] = newton(self, mix1, pP, attr_name, x0)
     % 1D Newton-Raphson method for finding roots
     if any(strcmpi(self.PD.ProblemType, {'TP', 'TV'}))
         x = get_transformation(self, 'TP');
-        ERR = 0;
+        STOP = 0;
         return
     end
-    it = 0; ERR = 1.0;
-    while ERR > self.TN.tol0 && it < self.TN.itMax
+    it = 0; STOP = 1.0;
+    while STOP > self.TN.tol0 && it < self.TN.itMax
         it = it + 1;
         [f0, fprime0, frel] = get_ratio_newton(self, mix1, pP, attr_name, x0);
         x = abs(x0 - f0 / fprime0);
         
-        ERR = max(abs((x - x0) / x), frel);
+        STOP = max(abs((x - x0) / x), frel);
         x0 = x;
     end
-    if ERR > self.TN.tol0
+    if STOP > self.TN.tol0
         fprintf('\n***********************************************************\n')
         fprintf('Newton method not converged\nCalling Steffensen-Aitken root finding algorithm\n')
         x0 = steff_guess(self, mix1, pP, attr_name);
-        [x, ERR] = steff(self, mix1, pP, attr_name, x0);
+        [x, STOP] = steff(self, mix1, pP, attr_name, x0);
     else
-        print_error_root(it, self.TN.itMax, x, ERR);
+        print_error_root(it, self.TN.itMax, x, STOP);
     end
 end
 

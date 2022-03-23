cd376c1b99566738b6b7e654dd6169374d953821
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-23 17:18:39 +0100

    Update: homogenize variable names and few comments

--- Solver/Shocks and detonations/shock_oblique_beta.m
+++ Solver/Shocks and detonations/shock_oblique_beta.m
@@ -1,38 +1,38 @@
-function [str1, str2] = shock_oblique_beta(varargin)
+function [mix1, mix2] = shock_oblique_beta(varargin)
     % Solve oblique shock for a given shock wave angle
     
     % Unpack input data
-    [self, str1, str2] = unpack(varargin);
+    [self, mix1, mix2] = unpack(varargin);
     % Definitions
-    a1 = soundspeed(str1);     % [m/s]
-    u1 = str1.u;               % [m/s]
+    a1 = soundspeed(mix1);     % [m/s]
+    u1 = mix1.u;               % [m/s]
     M1 = u1 / a1;              % [-]
-    beta = str1.beta * pi/180; % [rad]
+    beta = mix1.beta * pi/180; % [rad]
     beta_min = asin(1/M1);     % [rad]
     beta_max = pi/2;           % [rad]
     u1n = u1 * sin(beta);       % [m/s]
     % Check range beta
     if beta < beta_min || beta > beta_max
         error(['\nERROR! The given wave angle beta = %.2g is not in the '...
-               'range of possible solutions [%.2g, %2.g]'], str1.beta,...
+               'range of possible solutions [%.2g, %2.g]'], mix1.beta,...
                beta_min * 180/pi, beta_max * 180/pi);
     end
     
     % Obtain deflection angle, pre-shock state and post-shock states for
     % an oblique shock
-    [~, str2] = shock_incident(self, str1, u1n, str2);
+    [~, mix2] = shock_incident(self, mix1, u1n, mix2);
     
-    u2n = str2.v_shock;
+    u2n = mix2.v_shock;
     theta = beta - atan(u2n / (u1 .* cos(beta)));
     u2 = u2n * csc(beta - theta);
     % Save results
-    str2.beta = beta * 180/pi;   % [deg]
-    str2.theta = theta * 180/pi; % [deg]
-    str2.beta_min = beta_min * 180/pi; % [deg]
-    str2.beta_max = beta_max * 180/pi; % [deg]
-    str2.u = u2; % [m/s]
-    str2.u2n = u2n; % [m/s]
-    str2.v_shock = u2; % [m/s]
+    mix2.beta = beta * 180/pi;   % [deg]
+    mix2.theta = theta * 180/pi; % [deg]
+    mix2.beta_min = beta_min * 180/pi; % [deg]
+    mix2.beta_max = beta_max * 180/pi; % [deg]
+    mix2.u = u2; % [m/s]
+    mix2.u2n = u2n; % [m/s]
+    mix2.v_shock = u2; % [m/s]
 end
 
 % SUB-PASS FUNCTIONS

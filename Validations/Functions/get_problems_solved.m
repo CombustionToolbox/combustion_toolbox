4353debf378bf3673bd210e92a76c260c62d5949
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-24 10:35:56 +0100

    Solve: error with the previous changes done in shock polar

--- Validations/Functions/get_problems_solved.m
+++ Validations/Functions/get_problems_solved.m
@@ -1,7 +1,17 @@
-function value = get_problems_solved(mix, variable)
+function value = get_problems_solved(varargin)
     % Get problems solved based on the length of the given variable
-    for i=length(mix):-1:1
-        value(i) = length(mix{i}.(variable));
+    
+    mix = varargin{1};
+    variable = varargin{2};
+    if nargin == 3
+        subvariable = varargin{3};
+        for i=length(mix):-1:1
+            value(i) = length(mix{i}.(variable).(subvariable));
+        end
+    else
+        for i=length(mix):-1:1
+            value(i) = length(mix{i}.(variable));
+        end
     end
     value = sum(value);
 end

33fd27b41ac2950dd8d1a09277141bf93b0433f3
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-24 12:55:49 +0100

    Update: improve performance by 65%

--- Solver/Functions/find_ind.m
+++ Solver/Functions/find_ind.m
@@ -1,21 +1,26 @@
 function ind = find_ind(S, species)
     % Find index of the species in the list S
-    NS = length(S);
-    if ischar(species)
-        Nspecies = 1;
-        species = {species};
-    else
-        Nspecies = length(species);
-    end
-    ind = [];
-    for j=1:Nspecies
-        i = 0;
-        while i < NS
-            i = i + 1;
-            if strcmp(species{j}, S{i})
-                ind = [ind, i];
-                break
-            end 
+    if length(species) > 1
+        NS = length(S);
+        if ischar(species)
+            Nspecies = 1;
+            species = {species};
+        else
+            Nspecies = length(species);
         end
+        ind = [];
+        for j=1:Nspecies
+            i = 0;
+            while i < NS
+                i = i + 1;
+                if strcmp(species{j}, S{i})
+                    ind = [ind, i];
+                    break
+                end 
+            end
+        end
+    else
+        ind = ismember(S, species);
+        ind = find(ind);
     end
 end

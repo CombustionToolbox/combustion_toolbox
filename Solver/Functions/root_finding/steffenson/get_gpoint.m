11b6e87547bea24846e7a38083f876f3e90e08aa
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-28 16:40:01 +0200

    Update: comments of all the functions #381
    Former-commit-id: ba130fddb56d29fca5f3f72fc3a04ffe6ca6a9d6

--- Solver/Functions/root_finding/steffenson/get_gpoint.m
+++ Solver/Functions/root_finding/steffenson/get_gpoint.m
@@ -1,8 +1,20 @@
-function [gpoint, gpoint_relative] = get_gpoint(self, strR, pP, attr_name, x)
-    strP = equilibrate_T(self, strR, pP, x);
-    gpoint = (strP.(attr_name) - strR.(attr_name));
-    gpoint_relative = gpoint / (strP.(attr_name));
-    if strcmpi(attr_name, 's')
+function [gpoint, gpoint_relative] = get_gpoint(self, mix1, pP, field, x0)
+    % Get fixed point of a function based on the chemical transformation
+    %
+    % Args:
+    %     self (struct):  Data of the mixture, conditions, and databases
+    %     mix1 (struct):  Properties of the initial mixture
+    %     pP (float):     Pressure [bar]
+    %     field (str):    Fieldname in Problem Description (PD)
+    %     x0 (float):     Guess temperature [K]
+    % Returns:
+    %     gpoint (float): Fixed point of the function [kJ] (HP, EV) or [kJ/K] (SP, SV)
+    %     gpoint_relative (float): Fixed relative point of the function [kJ] (HP, EV) or [kJ/K] (SP, SV)
+
+    mix2 = equilibrate_T(self, mix1, pP, x0);
+    gpoint = (mix2.(field) - mix1.(field));
+    gpoint_relative = gpoint / (mix2.(field));
+    if strcmpi(field, 's')
         gpoint = gpoint * 1e3;
     end
 end

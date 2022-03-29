e63d9a1ab9cd91566c313a33fb2d5b7814f5f6d9
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-29 21:01:31 +0200

    Update: comments of all the functions #381
    Former-commit-id: 450d02fc22341d2acd917c6ada4fea5c33688960
    Former-commit-id: 45b9c92c622c770073281db634f0283ccb61f0a7

--- Settings/self/TunningProperties/TunningProperties.m
+++ Settings/self/TunningProperties/TunningProperties.m
@@ -1,19 +1,30 @@
 function self = TunningProperties()
+    % Initialize struct with tunning properties data
+    % 
+    % Returns:
+    %     self (struct): struct with tunning properties data
+
+    % Description
     self.description = "Tunning properties";
+    % Variables
+    %   * Chemical equilibrium TP, TV
     self.tolN = 1e-15;          % Tolerance of the gibbs minimization method
     self.tol_pi_e = 1e-4;       % Tolerance of the Lagrangian multiplier for ions divided by RT
+    %   * Chemical equilibrium HP, EV, SP, SV
     self.tol0 = 1e-3;           % Tolerance of the root finding algorithm
     self.root_method = @newton; % Method for root finding
     self.itMax = 30;            % Max number of iterations - root finding method
     self.root_T0_l = 300;       % First guess T[K] left branch - root finding method
     self.root_T0_r = 1500;      % First guess T[K] right branch - root finding method
     self.root_T0   = 2000;      % Guess T[K] if it's of previous range - root finding method
+    %   * Shocks and detonations 
     self.tol_oblique = 1e-3;    % Tolerance oblique shocks
     self.it_oblique = 20;       % Max number of iterations - oblique shocks
     self.N_points_polar = 100;  % Number of points to compute shock polar
     self.tol_shocks = 5e-5;     % Tolerance of shocks routines
     self.it_shocks = 50;        % Max number of iterations - shocks and detonations
     self.volumeBoundRation = 5; % Initial guess ratio shocks
+    %   * Rocket propellant performance
     self.tol_rocket = 0.4*1e-4; % Tolerance rocket performance
     self.it_rocket = 2;         % Max number of iterations - rocket performance
 end

450d02fc22341d2acd917c6ada4fea5c33688960
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-29 21:01:31 +0200

    Update: comments of all the functions #381

--- Settings/self/Species/get_index_ions.m
+++ Settings/self/Species/get_index_ions.m
@@ -1,3 +1,11 @@
 function index = get_index_ions(species)
+    % Get index of ions for the given list of species
+    %
+    % Args:
+    %     species (str): List of species
+    % 
+    % Returns:
+    %     index (float): Index of ions
+    
     index = (contains(species, 'minus') | contains(species, 'plus')) & ~contains(species, 'cyclominus');
 end

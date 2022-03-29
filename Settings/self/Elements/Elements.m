03e253c6228f6b1e45730aa34ef92425a95a9a6f
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-29 20:08:49 +0200

    Update: comments of all the functions #381

--- Settings/self/Elements/Elements.m
+++ Settings/self/Elements/Elements.m
@@ -1,10 +1,18 @@
 function self = Elements()
-   self.description = "Data of the chemical elements";
-   self.elements = set_elements();
-   self.NE = [];
-   self.ind_C = [];
-   self.ind_H = [];
-   self.ind_O = [];
-   self.ind_N = [];
-   self.ind_E = []; % Electrons
+    % Initialize struct with elements data
+    % 
+    % Returns:
+    %     self (struct): struct with elements data
+    
+    % Description
+    self.description = "Data of the chemical elements";
+    % Load cell with the elements in the periodic table
+    self.elements = set_elements();
+    % Variables
+    self.NE = [];    % Number of elements
+    self.ind_C = []; % Index element Carbon
+    self.ind_H = []; % Index element Hydrogen
+    self.ind_O = []; % Index element Oxygen
+    self.ind_N = []; % Index element Nytrogen
+    self.ind_E = []; % Index element Electron
 end

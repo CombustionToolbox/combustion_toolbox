039251a9f280b80dbf687fac6f90d6c4f168b876
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-03-29 21:03:03 +0200

    Update: comments of all the functions #381
    Former-commit-id: 204308030eb680c5d3a294b84f01c6a2a75f5d71
    Former-commit-id: 7056012f76ece26d8a31bb3bde28ef27678665b3

--- Settings/self/ProblemSolution/ProblemSolution.m
+++ Settings/self/ProblemSolution/ProblemSolution.m
@@ -7,12 +7,12 @@ function self = ProblemSolution()
     % Description
     self.description = "Problem solution";
     % Variables
-    self.strR_Fuel = [];               % Properties of the initial fuel mixture             (reactants)
-    self.strR_Oxidizer = [];           % Properties of the initial oxidizer + inert mixture (reactants)
-    self.strR = [];                    % Properties of the initial mixture                  (reactants)
-    self.strP = [];                    % Properties of the final mixture                    (products)
-    self.mix1 = [];                    % Properties of the mixture at the state 1           (reactants)
-    self.mix2 = [];                    % Properties of the mixture at the state 2           (products)
-    self.mix3 = [];                    % Properties of the mixture at the state 3           (products)
-    self.mix4 = [];                    % Properties of the mixture at the state 4           (products)
+    self.strR_Fuel = [];     % Properties of the initial fuel mixture             (reactants)
+    self.strR_Oxidizer = []; % Properties of the initial oxidizer + inert mixture (reactants)
+    self.strR = [];          % Properties of the initial mixture                  (reactants)
+    self.strP = [];          % Properties of the final mixture                    (products)
+    self.mix1 = [];          % Properties of the mixture at the state 1           (reactants)
+    self.mix2 = [];          % Properties of the mixture at the state 2           (products)
+    self.mix3 = [];          % Properties of the mixture at the state 3           (products)
+    self.mix4 = [];          % Properties of the mixture at the state 4           (products)
 end

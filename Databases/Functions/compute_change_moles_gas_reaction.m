ef7ee9e427eab0feda43dd0cdb27dbc70371c43a
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-04-02 18:08:51 +0200

    Update: comments
    Former-commit-id: d12f4efa997f48f96df67142788bc3f41026980e
    Former-commit-id: 9a7953d3ffe014b7a537627c1ac58f8f78fc93f7

--- Databases/Functions/compute_change_moles_gas_reaction.m
+++ Databases/Functions/compute_change_moles_gas_reaction.m
@@ -2,11 +2,13 @@ function Delta_n = compute_change_moles_gas_reaction(element_matrix, swtCondense
     % In order to compute the internal energy of formation from the enthalpy of
     % formation of a given species, we must determine the change in moles of
     % gases during the formation reaction of a mole of that species starting
-    % from the elements in their reference state. The only elements that are
-    % stable as diatomic gases are elements 1 (H), 8 (N), 9 (O), 10 (F), and 18
-    % (Cl). The remaining elements that are stable as (monoatomic) gases are
-    % the noble gases He (3), Ne (11), Ar (19), Kr (37), Xe (55), and Rn (87),
-    % which do not form any compound.
+    % from the elements in their reference state. 
+    % 
+    % Notes:
+    % The only elements that are stable as diatomic gases are elements
+    % 1 (H), 8 (N), 9 (O), 10 (F), and 18 (Cl). The remaining elements that
+    % are stable as (monoatomic) gases are the noble gases He (3), Ne (11),
+    % Ar (19), Kr (37), Xe (55), and Rn (87), which do not form any compound.
     %
     % Args: 
     %   element_matrix (float): Element matrix of the species

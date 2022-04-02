1f6f210063a1ca3caba77f61007cb3f6f3db3378
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-04-02 11:27:47 +0200

    Update: comments of all the functions #381
    Former-commit-id: e2071bbab9e42a06ff7b8a28b68d97515075484d

--- Databases/Functions/species_DeT.m
+++ Databases/Functions/species_DeT.m
@@ -1,9 +1,18 @@
-function DeTi = species_DeT(species, T, DB)
-% global strThProp
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% Calculates the thermal internal energy (in kJ/mol) for the specified 
-% chemical species (Species) at the specified temperature (T)
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-DeTi = DB.(species).DeTcurve(T)/1000;
+function DeT = species_DeT(species, T, DB)
+    % Compute thermal internal energy [kJ/mol] of the species at the given temperature [K]
+    % using piecewise cubic Hermite interpolating polynomials and linear extrapolation
+    %
+    % Args:
+    %     species (str): Chemical species
+    %     T (float): Temperature [K]
+    %     DB (struct): Database with custom thermodynamic polynomials functions generated from NASAs 9 polynomials fits
+    %
+    % Returns:
+    %     DeT (float): Thermal internal energy [kJ/mol]
+    
+    try
+        DeT = DB.(species).DeTcurve(T)/1000;
+    catch
+        DeT = 0; % cryogenic species
+    end
+end

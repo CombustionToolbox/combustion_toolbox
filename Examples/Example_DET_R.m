712cf6160ee8bb552ec8e0da206df2e7361656c5
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-04-04 11:20:06 +0200

    Update: default list species, comments, and validations
    Former-commit-id: ce55a5505fc9bb040a662101c7b42a2559ade466
    Former-commit-id: 5697806cb5ac896dc4b31f425c656ea623bfda41

--- Examples/Example_DET_R.m
+++ Examples/Example_DET_R.m
@@ -3,13 +3,13 @@
 %
 % Compute pre-shock and post-shock state for a reflected planar detonation
 % considering Chapman-Jouguet (CJ) theory for lean to rich CH4-air mixtures
-% at standard conditions, a set of 24 species considered and a set of
+% at standard conditions, a set of 26 species considered and a set of
 % equivalence ratios (phi) contained in (0.5, 5) [-]
 %   
 %   
-% Soot formation == {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
-%                    'HCN','H','OH','O','CN','NH3','CH4','C2H4','CH3',...
-%                    'NO','HCO','NH2','NH','N','CH','Cbgrb'}
+% Soot formation == {'CO2','CO','H2O','H2','O2','N2','He','Ar','Cbgrb',...
+%                    'C2','C2H4','CH','CH','CH3','CH4','CN','H',...
+%                    'HCN','HCO','N','NH','NH2','NH3','NO','O','OH'}
 %   
 % See wiki or ListSpecies() for more predefined sets of species
 %

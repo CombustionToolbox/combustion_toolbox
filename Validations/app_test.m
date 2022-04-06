5dedb56fffe09723bdc5250ad7e2707a4cf07bf6
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-04-06 15:49:01 +0200

    Add: new tests
    Former-commit-id: 1cad90a3d15eed41474dd9564152649f43a2837b

--- Validations/app_test.m
+++ Validations/app_test.m
@@ -13,6 +13,7 @@ classdef app_test < matlab.unittest.TestCase
     properties(TestParameter)
         equivalence_ratio = {0.5, 1, 2, 4};
         velocity_preshock = {505, 3140, 7830}; % Error for 11862 [m/s]
+        velocity_preshock_R = {505, 3140, 5169}; % Error for 5617, 6103, 6632, 7206, 7830 [m/s]
     end
 
     methods (Test)
@@ -21,13 +22,18 @@ classdef app_test < matlab.unittest.TestCase
              verifyLessThanOrEqual(testCase, act_max_rel_error_moles, testCase.max_rel_error);
              verifyLessThanOrEqual(testCase, act_max_rel_error_prop, testCase.max_rel_error);
          end
-%         function test_HP_CEA_2(testCase, equivalence_ratio) % Error with soot (CHECK)
-%             [act_max_rel_error_moles, act_max_rel_error_prop] = run_test_HP_CEA_2(equivalence_ratio);
-%             verifyLessThanOrEqual(testCase, act_max_rel_error_moles, testCase.max_rel_error);
-%             verifyLessThanOrEqual(testCase, act_max_rel_error_prop, testCase.max_rel_error);
-%         end
+        function test_HP_CEA_2(testCase, equivalence_ratio)
+            [act_max_rel_error_moles, act_max_rel_error_prop] = run_test_HP_CEA_2(equivalence_ratio);
+            verifyLessThanOrEqual(testCase, act_max_rel_error_moles, testCase.max_rel_error);
+            verifyLessThanOrEqual(testCase, act_max_rel_error_prop, testCase.max_rel_error);
+        end
         function test_SHOCK_IONIZATION_CEA_1(testCase, velocity_preshock)
             [act_max_rel_error_prop_mix1, act_max_rel_error_prop_mix2] = run_test_SHOCK_IONIZATION_CEA_1(velocity_preshock);
+            verifyLessThanOrEqual(testCase, act_max_rel_error_prop_mix1, testCase.max_rel_error);
+            verifyLessThanOrEqual(testCase, act_max_rel_error_prop_mix2, testCase.max_rel_error);
+        end
+        function test_SHOCK_R_IONIZATION_CEA_1(testCase, velocity_preshock_R)
+            [act_max_rel_error_prop_mix1, act_max_rel_error_prop_mix2] = run_test_SHOCK_R_IONIZATION_CEA_1(velocity_preshock_R);
             verifyLessThanOrEqual(testCase, act_max_rel_error_prop_mix1, testCase.max_rel_error);
             verifyLessThanOrEqual(testCase, act_max_rel_error_prop_mix2, testCase.max_rel_error);
         end

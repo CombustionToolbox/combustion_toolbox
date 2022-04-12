3d0ba6a3768383b975f67e5555cee6ff66251e49
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-04-12 12:08:17 +0200

    Update: miscellaneous

--- Settings/display/results.m
+++ Settings/display/results.m
@@ -1,5 +1,6 @@
 function results(self, i)
     % Display results in the command window
+    
     if self.Misc.FLAG_RESULTS
         if ~isfield(self.PS, 'str2')
             displayresults(self.PS.strR{i}, self.PS.strP{i}, self.PD.ProblemType, self.C.mintol_display, self.S.LS);

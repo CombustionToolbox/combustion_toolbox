17da2e86225fe4caa6e5eadb59f9907fd740f0ce
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-04-12 11:56:50 +0200

    Update:  include guess moles as a possible argument
    Former-commit-id: 0f7bf619030cad808d89a19447eff0f54a0ff782

--- Solver/chemical_equilibrium/equilibrate.m
+++ Solver/chemical_equilibrium/equilibrate.m
@@ -13,29 +13,27 @@ function mix2 = equilibrate(self, mix1, pP, varargin)
     %     mix2 (struct): Properties of the final mixture
 
     mix1 = get_struct(mix1);
-    [mix2, guess_moles] = unpack(varargin);
+    mix2 = unpack(varargin);
     % get attribute xx of the specified transformations
     attr_name = get_attr_name(self);
     % compute initial guess
-    guess = get_guess(self, mix1, pP, attr_name, mix2);
+    [guess, guess_moles] = get_guess(self, mix1, pP, attr_name, mix2);
     % root finding: find the value x that satisfies f(x) = mix2.xx(x) - mix1.xx = 0
-    [T, ERR] = root_finding(self, mix1, pP, attr_name, guess);
+    [T, STOP] = root_finding(self, mix1, pP, attr_name, guess, guess_moles);
     % compute properties
     mix2 = equilibrate_T(self, mix1, pP, T, guess_moles);
     % check convergence in case the problemType is TP (defined Temperature and Pressure)
     print_convergence(mix2.error_moles, self.TN.tolN, mix2.error_moles_ions, self.TN.tol_pi_e, self.PD.ProblemType)
     % save error - root finding
-    mix2.error_problem = ERR;
+    mix2.error_problem = STOP;
 end
 
 %%% SUB-PASS FUNCTIONS
 function [mix2, guess_moles] = unpack(value)
     if ~isempty(value)
         mix2 = get_struct(value{1});
-        guess_moles = mix2.Xi * mix2.N;
     else
         mix2 = [];
-        guess_moles = [];
     end
 end
 
@@ -59,18 +57,21 @@ function attr_name = get_attr_name(self)
     end
 end
 
-function guess = get_guess(self, mix1, pP, attr_name, mix2)
+function [guess, guess_moles] = get_guess(self, mix1, pP, attr_name, mix2)
     if any(strcmpi(self.PD.ProblemType, {'TP', 'TV'}))
         guess = get_transformation(self, 'TP');
+        guess_moles = [];
     elseif ~isempty(mix2)
         guess = mix2.T;
+        guess_moles = mix2.Xi * mix2.N;
     else
         guess = steff_guess(self, mix1, pP, attr_name);
+        guess_moles = [];
     end
 end
 
-function [x, ERR] = root_finding(self, mix1, pP, attr_name, x0)
-    [x, ERR] = self.TN.root_method(self, mix1, pP, attr_name, x0);
+function [x, ERR] = root_finding(self, mix1, pP, attr_name, x0, guess_moles)
+    [x, ERR] = self.TN.root_method(self, mix1, pP, attr_name, x0, guess_moles);
 end
 
 function print_convergence(STOP, TOL, STOP_ions, TOL_ions, ProblemType)

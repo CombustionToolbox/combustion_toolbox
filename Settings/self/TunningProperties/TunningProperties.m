3bbefdbe043463d117361e0b2cc250142e8ce376
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-04-12 11:46:48 +0200

    Update: change default values temperature guess
    Former-commit-id: 354ae747fbd619702e0b4cf2f106ace96572465c
    Former-commit-id: eabca1b95ba97589e7e6ef59049617f6a67a5647

--- Settings/self/TunningProperties/TunningProperties.m
+++ Settings/self/TunningProperties/TunningProperties.m
@@ -38,9 +38,9 @@ function self = TunningProperties()
     self.tol0 = 1e-3;           % Tolerance of the root finding algorithm
     self.root_method = @newton; % Method for root finding
     self.itMax = 30;            % Max number of iterations - root finding method
-    self.root_T0_l = 300;       % First guess T[K] left branch - root finding method
-    self.root_T0_r = 1500;      % First guess T[K] right branch - root finding method
-    self.root_T0   = 2000;      % Guess T[K] if it's of previous range - root finding method
+    self.root_T0_l = 1000;      % First guess T[K] left branch - root finding method
+    self.root_T0_r = 2000;      % First guess T[K] right branch - root finding method
+    self.root_T0   = 3000;      % Guess T[K] if it's of previous range - root finding method
     % * Shocks and detonations 
     self.tol_shocks = 5e-5;     % Tolerance of shocks routines
     self.it_shocks = 50;        % Max number of iterations - shocks and detonations

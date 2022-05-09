a48ad9727101ecd5cf3840a5e3eba54eb02a5e36
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-05-09 15:43:12 +0200

    Add: include new thermo functions
    Former-commit-id: b031ea5c1af627a2944a60e1299886a36e88328f

---
+++ Solver/Functions/Thermo/volume.m
@@ -0,0 +1,11 @@
+function value = volume(mix)
+    % Get the volume [m3] of the mixture
+    %
+    % Args:
+    %     mix (struct):  Properties of the mixture
+    %
+    % Returns:
+    %     value (float): volume [m3] of the mixture
+
+    value = mix.v;
+end

8bef12a6adf0befcfe2ca74e3f9a400769c9de23
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-05-25 15:47:27 +0200

    Solve: error comments

--- Solver/Functions/SetSpecies.m
+++ Solver/Functions/SetSpecies.m
@@ -16,26 +16,26 @@ function M = SetSpecies(self, species, moles, T)
     for n = length(moles):-1:1
         mmi = self.DB.(species{n}).mm;                    % [g/mol]
         mi  = moles(n) * mmi * 1e-3;                      % [kg]
-        hfi = self.DB.(species{n}).hf/1000;               % [kJ]
-        efi = self.DB.(species{n}).ef/1000;               % [kJ]
+        hfi = self.DB.(species{n}).hf/1000;               % [kJ/mol]
+        efi = self.DB.(species{n}).ef/1000;               % [kJ/mol]
         swtCondensed = self.DB.(species{n}).swtCondensed; % [bool]
         if length(self.DB.(species{n}).T) > 1
-            DhTi = species_DhT(species{n},T,self.DB);     % [kJ]
-            DeTi = species_DeT(species{n},T,self.DB);     % [kJ]
-            cPi  = species_cP(species{n},T,self.DB);      % [J/K]
-            cVi  = species_cV(species{n},T,self.DB);      % [J/K]
-            s0i  = species_s0(species{n},T,self.DB);      % [kJ/K]
+            DhTi = species_DhT(species{n},T,self.DB);     % [kJ/mol]
+            DeTi = species_DeT(species{n},T,self.DB);     % [kJ/mol]
+            cPi  = species_cP(species{n},T,self.DB);      % [J/mol-K]
+            cVi  = species_cV(species{n},T,self.DB);      % [J/mol-K]
+            s0i  = species_s0(species{n},T,self.DB);      % [kJ/mol-K]
             if ~swtCondensed
                 pVi = moles(n) * R0 * T * 1e-5; % For ideal gases [bar m3]
             else
                 pVi = 0; % For condensed species [bar m3]
             end
         else
-            DhTi = 0; % [kJ]
-            DeTi = 0; % [kJ]
-            cPi  = 0; % [J/K]
-            cVi  = 0; % [J/K]
-            s0i  = 0; % [kJ/K]
+            DhTi = 0; % [kJ/mol]
+            DeTi = 0; % [kJ/mol]
+            cPi  = 0; % [J/mol-K]
+            cVi  = 0; % [J/mol-K]
+            s0i  = 0; % [kJ/mol-K]
             pVi  = 0; % [bar m3]
         end   
         ind = find_ind(self.S.LS, species(n));

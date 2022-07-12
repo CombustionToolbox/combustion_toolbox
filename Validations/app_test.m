cd638d14c7e43a7848fa33cad59a7565b9bde575
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-07-12 08:42:06 +0200

    Add: validation EV in CI and CD tests
    Former-commit-id: 9ca61d23e726402018eadb5c2f89351a824f6bf1

--- Validations/app_test.m
+++ Validations/app_test.m
@@ -12,8 +12,8 @@ classdef app_test < matlab.unittest.TestCase
 
     properties(TestParameter)
         equivalence_ratio = {0.5, 1, 2, 4};
-        velocity_preshock = {505, 3140, 7830}; % Error for 11862 [m/s]
-        velocity_preshock_R = {505, 3140, 5169}; % Error for 5617, 6103, 6632, 7206, 7830 [m/s]
+        velocity_preshock = {505, 3140, 7830, 11862};
+        velocity_preshock_R = {505, 3140, 5169, 5617, 6103, 6632, 7206, 7830};
     end
 
     methods (Test)
@@ -26,6 +26,11 @@ classdef app_test < matlab.unittest.TestCase
             [act_max_rel_error_moles, act_max_rel_error_prop] = run_test_HP_CEA_2(equivalence_ratio);
             verifyLessThanOrEqual(testCase, act_max_rel_error_moles, testCase.max_rel_error);
             verifyLessThanOrEqual(testCase, act_max_rel_error_prop, testCase.max_rel_error);
+        end
+        function test_EV_CEA_1(testCase, equivalence_ratio)
+             [act_max_rel_error_moles, act_max_rel_error_prop] = run_test_EV_CEA_1(equivalence_ratio);
+             verifyLessThanOrEqual(testCase, act_max_rel_error_moles, testCase.max_rel_error);
+             verifyLessThanOrEqual(testCase, act_max_rel_error_prop, testCase.max_rel_error);
         end
         function test_SHOCK_IONIZATION_CEA_1(testCase, velocity_preshock)
             [act_max_rel_error_prop_mix1, act_max_rel_error_prop_mix2] = run_test_SHOCK_IONIZATION_CEA_1(velocity_preshock);

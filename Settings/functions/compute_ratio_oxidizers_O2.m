171b125af2379ed19cd45b44894df7e56ebe9ad9
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-07-22 19:22:36 +0200

    Update: change proportion_inerts_O2 to proportion_oxidizers #103
    Former-commit-id: 15e5a8727c69f17e0a452a705d4c0f3e59442085
    Former-commit-id: e277e5ff12ddc876fa8c07de23b75b5b52b6ce2d

---
+++ Settings/functions/compute_ratio_oxidizers_O2.m
@@ -0,0 +1,11 @@
+function self = compute_ratio_oxidizers_O2(self)
+    % Compute ratio oxidizers/O2
+    
+    % Find index O2
+    ind_O2 = find_ind(self.PD.S_Oxidizer, 'O2');
+    if isempty(ind_O2)
+        ind_O2 = find_ind(self.PD.S_Oxidizer, 'O2bLb');
+    end
+    % Compute proportion inerts/O2
+    self.PD.ratio_oxidizers_O2 = self.PD.N_Oxidizer / self.PD.N_Oxidizer(ind_O2);
+end

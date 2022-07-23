5697311f4d5044523d454c2297db652930416ad1
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-07-23 07:31:22 +0200

    Add: examples rocket
    Former-commit-id: 3721c1b25d47f50c83b8cc7441e467ead1a512c9

--- Examples/Example_ROCKET_CONTOUR.m
+++ Examples/Example_ROCKET_CONTOUR.m
@@ -15,29 +15,56 @@
 %          PhD Candidate - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                 
-% Last update April 12 2022
+% Last update July 22 2022
 % -------------------------------------------------------------------------
 
-
+FLAG_SAVE = true;
 %% SOLVE PROBLEM
-phi = 1:0.1:3;
-Aratio = 1.1:0.1:2.62;
-for i = length(phi):-1:1
+phi = 1:0.01:4;
+Aratio = 1.1:0.1:2.6;
+DisplaySpecies = {'CO2','CO','H2O','H2','O2','C2H2_acetylene',...
+          'C2H4','C2H6','CH2CO_ketene','CH3','CH3CHO_ethanal','CH3OH',...
+          'CH4','COOH','H','H2O2','HCHO_formaldehy','HCO','HCOOH','HO2',...
+          'O','OH','Cbgrb'};
+
+for i = length(Aratio):-1:1
     %% INITIALIZE
-    self = App('SOOT FORMATION');
+    self = App('HC/O2/N2 PROPELLANTS');
+    self.TN.tolN = 1e-18;
+    self.Misc.display_species = DisplaySpecies;
     %% INITIAL CONDITIONS
-    self = set_prop(self, 'TR', 298.15, 'pR', 22, 'phi', 1.44922);
     self.PD.S_Fuel     = {'RP_1'};
     self.PD.S_Oxidizer = {'O2bLb'};
     self.PD.FLAG_IAC = false;
     %% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
-    self = set_prop(self, 'Aratio_c', 2, 'Aratio', Aratio);
-    self = set_prop(self, 'TR', 298.15, 'pR', 22, 'phi', phi(i));
+    self = set_prop(self, 'Aratio_c', 2, 'Aratio', Aratio(i));
+    self = set_prop(self, 'TR', 298.15, 'pR', 22, 'phi', phi);
     self = SolveProblem(self, 'ROCKET');
     Z(:, i) = cell2vector(self.PS.strP, 'I_sp');
+    OF(:, i) = cell2vector(self.PS.strR, 'OF');
+    %% DISPLAY RESULTS (PLOTS)
+    if FLAG_SAVE
+        % Plot molar fraction
+        self.C.mintol_display = 1e-16;
+        self.Misc.config.labelx = 'Mixture ratio $O/F$';
+        self.Misc.config.labely = 'Molar fraction $X_i$';
+        self.Misc.config.title = strcat('Area ratio $A_{\rm exit}/A_{\rm throat} = ', sprintf('%.2f', Aratio(i)), '$');
+        ax = displaysweepresults(self, self.PS.strP, OF(:, i));
+        set_title(ax, self.Misc.config);
+        % Save plots
+        folderpath = strcat(pwd,'\Validations\Figures\');
+        stack_trace = dbstack;
+        filename = strcat('RP1_LOX_', sprintf('%d', i));
+        saveas(gcf, strcat(folderpath, filename, '_molar'), 'svg');
+    end
 end
-%% DISPLAY RESULTS (PLOTS)
-% postResults(self);
-
-[X, Y] = meshgrid(phi, Aratio);
-contourf(X, Y, Z);
+% Contour plot
+[X, Y] = meshgrid(OF(:, i), Aratio);
+ax = set_figure;
+contourf(ax, X, Y, Z', 10);
+c = colorbar;
+xlabel(ax, 'Mixture ratio $O/F$');
+ylabel(ax, 'Area ratio $A_{\rm exit}/A_{\rm throat}$');
+c.Label.String = 'Specific impulse at sea level $I_{sp}$';
+c.Label.Interpreter = 'latex';
+set(ax, 'Layer','top')

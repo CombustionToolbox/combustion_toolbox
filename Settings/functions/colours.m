6416eb77eb0bb21ae496a8e8b63714b1bf3ac74d
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-07-23 19:27:05 +0200

    Update: remove deprecated
    Former-commit-id: b69b9e0002af45c43c25f295ccf96b028e78b3ce
    Former-commit-id: d2c993e83d4c9341140edd0daaedf05948a07c30

--- Settings/functions/colours.m
+++
@@ -1,4 +0,0 @@
-function c = colours()
-red = [0.85,0.33,0.1];blue=[0,0.45,0.74];green=[0.47,0.67,0.19];
-purple = [0.49,0.18,0.56];yellow = [0.93,0.69,0.13];orange=[0.702,0.349,0];
-c=[red;blue;green;purple;yellow;orange];

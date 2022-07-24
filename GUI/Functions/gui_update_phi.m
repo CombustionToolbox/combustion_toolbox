c860121b85ca05dea071ba5fd5389c0209e1b7af
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-07-24 13:39:01 +0200

    Solve: error ratio Oxidizer/Fuel (app)
    Former-commit-id: b9f5cb0f8684fec055b8ebb126f534212a6b090e

--- GUI/Functions/gui_update_phi.m
+++ GUI/Functions/gui_update_phi.m
@@ -6,6 +6,6 @@ function gui_update_phi(obj, app)
         obj.edit_phi.Value = app.PS.strR{1}.phi; 
     end
     obj.edit_phi2.Value = obj.edit_phi.Value;
-    obj.edit_OF.Value = 1/app.PS.strR{1}.FO;
+    obj.edit_OF.Value = app.PS.strR{1}.OF;
     obj.edit_F.Value = app.PS.strR{1}.percentage_Fuel;
 end

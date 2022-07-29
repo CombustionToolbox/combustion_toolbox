d511ff9a16f30694ca36575e197bbfdc7fbe6da7
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-07-29 17:23:39 +0200

    Update: include possibility of copy a previous setup #567
    Former-commit-id: d42dc37f4d0e88bba91b79aed0103ed748b04a76
    Former-commit-id: 43631928fb17176988806766ed348ed9027c13ae

--- Settings/self/Species/Species.m
+++ Settings/self/Species/Species.m
@@ -7,7 +7,7 @@ function self = Species()
     % Description
     self.description = "Data of the chemical species";
     % Variables
-    %   * Species and lengths
+    % * Species and lengths
     self.LS_DB = [];      % List species Database
     self.NS_DB = [];      % Number species Database
     self.NG = [];         % Number gaseous species in the mixture
@@ -16,7 +16,7 @@ function self = Species()
     self.LS_formula = []; % Formula of each species contained in LS
     self.LS_fixed = {'CO2','CO','H2O','H2','O2','N2','He','Ar'}; 
     self.NS_fixed = length(self.LS_fixed); % Number fixed species
-    %   * Index values
+    % * Index values
     self.ind_nswt = [];      % Index gaseous species
     self.ind_swt = [];       % Index condensed species
     self.ind_cryogenic = []; % Index cryogenic liquified species
@@ -24,10 +24,10 @@ function self = Species()
     self.ind_fixed = [];     % Index fixed species in LS
     self.ind_all = [];       % Index all species in LS
     self.ind_ions = [];      % Index all ion species in LS
-    %   * Default list of species for complete combustion
+    % * Default list of species for complete combustion
     self.LS_lean = {'CO2', 'H2O', 'N2', 'Ar', 'O2'};       % List of species for a lean complete combustion (equivalence ratio < 1)
     self.LS_rich = {'CO2', 'H2O', 'N2', 'Ar', 'CO', 'H2'}; % List of species for a lean complete combustion (equivalence ratio > 1)
-    self.LS_soot = {'N2', 'Ar', 'CO', 'H2', 'Cbgrb'};      % List of species for a lean complete combustion (equivalence ratio > equivalence ratio soot)
-    %   * Flags
+    self.LS_soot = {'N2', 'Ar', 'CO', 'H2', 'Cbgrb', 'CO2', 'H2O'}; % List of species for a lean complete combustion (equivalence ratio > equivalence ratio soot)
+    % * Flags
     self.FLAG_COMPLETE = false;
 end

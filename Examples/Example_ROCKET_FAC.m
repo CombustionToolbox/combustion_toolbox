b6be4e1243f7e8ff11c8a8e8021995254f722e57
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-07-30 11:27:02 +0200

    Update: examples rockets
    Former-commit-id: 1f497da12d5f98199110921aa0397f04242357e8

--- Examples/Example_ROCKET_FAC.m
+++ Examples/Example_ROCKET_FAC.m
@@ -1,10 +1,11 @@
 % -------------------------------------------------------------------------
-% EXAMPLE: ROCKET Propellants considering a Finite-Area-Chamber (FAC)
+% EXAMPLE: ROCKET Propellants considering an Finite-Area-Chamber (FAC)
 %
-% Compute adiabatic temperature and equilibrium composition at constant
-% pressure (e.g., 1.01325 bar) for lean to rich LH2-LOX mixtures at
-% standard conditions, a set of 24 species considered and a set of
-% equivalence ratios phi contained in (1, 5) [-]
+% Compute rocket propellant performance considering an Finite-Area-Chamber
+% with an area ratio of the combustion chamber A_chamber/A_throat = 2
+% for lean to rich LH2-LOX mixtures at 101.325 bar, a set of 24 species
+% considered, a set of equivalence ratios phi contained in (2, 5) [-], and
+% an exit area ratio A_exit/A_throat = 3 
 %   
 % HYDROGEN_L == {'H','H2O','OH','H2','O','O3','O2','HO2','H2O2',...
 %                'H2bLb','O2bLb'}
@@ -15,7 +16,7 @@
 %          PhD Candidate - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                 
-% Last update April 12 2022
+% Last update Jul 30 2022
 % -------------------------------------------------------------------------
 
 %% INITIALIZE
@@ -26,7 +27,7 @@ self.PD.S_Fuel     = {'H2bLb'};
 self.PD.S_Oxidizer = {'O2bLb'};
 self.PD.FLAG_IAC = false;
 %% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
-self = set_prop(self, 'Aratio_c', 2);
+self = set_prop(self, 'Aratio_c', 2, 'Aratio', 3);
 %% SOLVE PROBLEM
 self = SolveProblem(self, 'ROCKET');
 %% DISPLAY RESULTS (PLOTS)

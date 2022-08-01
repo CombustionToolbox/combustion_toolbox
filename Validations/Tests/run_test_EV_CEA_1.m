ff285a8ad4557190bb43644d7b6f0992b4741b00
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-08-01 16:48:29 +0200

    Update: include validation for more properties

--- Validations/Tests/run_test_EV_CEA_1.m
+++ Validations/Tests/run_test_EV_CEA_1.m
@@ -1,11 +1,11 @@
 function [max_rel_error_moles, max_rel_error_prop] = run_test_EV_CEA_1(value)
-    % Run test_HP_CEA_1:
+    % Run test_EV_CEA_1:
     % Contrasted with: NASA's Chemical Equilibrium with Applications software
     % Problem type: Adiabatic T and composition at constant volume
     % Temperature [K]   = 300;
     % Pressure    [bar] = 1.01325;
     % Equivalence ratio [-] = value
-    % Initial mixture: Fuel + AIR_IDEAL (78.084% N2, 20.9476% O2, 0.9365% Ar, 0.0319% CO2)
+    % Initial mixture: Fuel + AIR (78.084% N2, 20.9476% O2, 0.9365% Ar, 0.0319% CO2)
     % List of species considered: ListSpecies('Soot Formation Extended')
     
     % Inputs
@@ -34,5 +34,7 @@ function [max_rel_error_moles, max_rel_error_prop] = run_test_EV_CEA_1(value)
     % * Molar fractions
     max_rel_error_moles = compute_error_moles_CEA(results_CT, results_CEA, 'phi', value, 'Xi', DisplaySpecies);
     % * Properties mixture 2
-    max_rel_error_prop = compute_error_prop_CEA(results_CT, results_CEA, {'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi'}, value, {'T', 'p', 'h', 'e', 'g', 'S', 'cP', 'cV', 'gamma_s'}, 'mix2');
+    properties_y = {'T', 'p', 'h', 'e', 'g', 'S', 'cP', 'cV', 'gamma_s', 'dVdT_p', 'dVdp_T', 'sound', 'W'};
+    properties_x = {'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi'};
+    max_rel_error_prop = compute_error_prop_CEA(results_CT, results_CEA, properties_x, value, properties_y, 'mix2');
 end

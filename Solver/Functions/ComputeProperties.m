bee18ee78e661b7675ef92467ed9e0ed6a3c1773
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-08-01 11:40:28 +0200

    Solve: error computation molecular weight of mixture with condensed species #593
    Former-commit-id: 81ca7de06b947b55722b8f9740b058d8b3d10f1d

--- Solver/Functions/ComputeProperties.m
+++ Solver/Functions/ComputeProperties.m
@@ -4,7 +4,7 @@ function mix =  ComputeProperties(self, SpeciesMatrix, p, T)
     %
     % Args:
     %     self (struct): Data of the mixture, conditions, and databases
-    %     SpeciesMatrix (float): Matrix with the properties values of the mixture
+    %     SpeciesMatrix (float): Matrix with the properties of the mixture
     %     p (float): Pressure [bar]
     %     T (float): Temperature [K]
     %
@@ -17,22 +17,22 @@ function mix =  ComputeProperties(self, SpeciesMatrix, p, T)
     mix.p = p; % [bar]
     mix.T = T; % [K]
     % Unpack SpeciesMatrix
-    Ni          = SpeciesMatrix(:,1);                       % [mol]
-    mix.N       = sum(SpeciesMatrix(:, self.C.M0.ind_ni));  % [mol] 
-    mix.hf      = sum(SpeciesMatrix(:, self.C.M0.ind_hfi)); % [kJ]
-    mix.h       = sum(SpeciesMatrix(:, self.C.M0.ind_hi));  % [kJ]
-    mix.ef      = sum(SpeciesMatrix(:, self.C.M0.ind_efi)); % [kJ]
-    mix.cP      = sum(SpeciesMatrix(:, self.C.M0.ind_cPi)); % [J/K]
-    mix.S0      = sum(SpeciesMatrix(:, self.C.M0.ind_si));  % [kJ/K]
-    mix.pv      = sum(SpeciesMatrix(:, self.C.M0.ind_pVi)); % [bar m3]
-    mix.phase   = SpeciesMatrix(:, self.C.M0.ind_phase);    % [bool]
-    mix.mi      = sum(SpeciesMatrix(:, self.C.M0.ind_mi));  % [kg]
+    Ni        = SpeciesMatrix(:,1);                       % [mol]
+    mix.N     = sum(SpeciesMatrix(:, self.C.M0.ind_ni));  % [mol] 
+    mix.hf    = sum(SpeciesMatrix(:, self.C.M0.ind_hfi)); % [kJ]
+    mix.h     = sum(SpeciesMatrix(:, self.C.M0.ind_hi));  % [kJ]
+    mix.ef    = sum(SpeciesMatrix(:, self.C.M0.ind_efi)); % [kJ]
+    mix.cP    = sum(SpeciesMatrix(:, self.C.M0.ind_cPi)); % [J/K]
+    mix.S0    = sum(SpeciesMatrix(:, self.C.M0.ind_si));  % [kJ/K]
+    mix.pv    = sum(SpeciesMatrix(:, self.C.M0.ind_pVi)); % [bar m3]
+    mix.phase = SpeciesMatrix(:, self.C.M0.ind_phase);    % [bool]
+    mix.mi    = sum(SpeciesMatrix(:, self.C.M0.ind_mi));  % [kg]
     % Compute mass fractions [-]
-    mix.Yi = SpeciesMatrix(:, self.C.M0.ind_mi)./mix.mi;
-    % Compute mean molecular weight [g/mol]
-    mix.W = 1/sum(mix.Yi./SpeciesMatrix(:, self.C.M0.ind_mmi), 'OmitNan');
+    mix.Yi = SpeciesMatrix(:, self.C.M0.ind_mi) ./ mix.mi;
     % Compute molar fractions [-]
-    mix.Xi = Ni/mix.N;
+    mix.Xi = Ni / mix.N;
+    % Compute mean molecular weight [g/mol]
+    mix.W = sum(Ni .* SpeciesMatrix(:, self.C.M0.ind_W)) / molesGas(mix);
     % Get non zero species
     FLAG_NONZERO = mix.Xi > 0;
     % Compute vector atoms of each element
@@ -58,9 +58,9 @@ function mix =  ComputeProperties(self, SpeciesMatrix, p, T)
     % Compute specific heat at constant volume [J/K]
     mix.cV = mix.cP - R0 * mix.N;
     % Compute Adibatic index [-]
-    mix.gamma = mix.cP/mix.cV;
+    mix.gamma = mix.cP / mix.cV;
     % Compute sound velocity [m/s]
-    mix.sound = sqrt(mix.gamma * p * 1e5 / mix.rho);
+    mix.sound = sqrt(mix.gamma * convert_bar_to_Pa(p) / mix.rho);
     
     % Correction of: cP, cV, gamma, and speed of sound as consequence of the
     % chemical reaction
@@ -70,13 +70,13 @@ function mix =  ComputeProperties(self, SpeciesMatrix, p, T)
         if ~any(isnan(self.dNi_T)) && ~any(isinf(self.dNi_T))
             delta = ~mix.phase;
             h0_j = (SpeciesMatrix(:, self.C.M0.ind_hi)) ./ Ni * 1e3; % [J/mol]
-            mix.cP_r = sum(h0_j/T .* (1 +  delta .* (Ni - 1)) .* self.dNi_T, 'omitnan'); % [J/K]
+            mix.cP_r = sum(h0_j / T .* (1 +  delta .* (Ni - 1)) .* self.dNi_T, 'omitnan'); % [J/K]
             mix.cP_f = mix.cP;
             mix.cP = mix.cP_f + mix.cP_r; % [J/K]
             mix.cV = mix.cP + (molesGas(mix) * R0 * mix.dVdT_p^2) / mix.dVdp_T; % [J/K]
-            mix.gamma = mix.cP/mix.cV; % [-]
+            mix.gamma = mix.cP / mix.cV; % [-]
             mix.gamma_s = - mix.gamma / mix.dVdp_T; % [-]
-            mix.sound = sqrt(mix.gamma_s * p*1e5 / mix.rho); % [m/s]
+            mix.sound = sqrt(mix.gamma_s * convert_bar_to_Pa(p) / mix.rho); % [m/s]
         else
             mix.gamma_s = - 1 / mix.dVdp_T; % [-]
         end
@@ -89,7 +89,6 @@ end
 function DS = compute_entropy_mixing(mix, Ni, R0, FLAG_NONZERO)
     % Compute entropy of mixing [kJ/K]. 
     % Note: only nonzero for gaseous species
-
     DSi = Ni(FLAG_NONZERO) .* log(Ni(FLAG_NONZERO) / molesGas(mix) * mix.p) .* (1 - mix.phase(FLAG_NONZERO));
     DS  = -R0 * sum(DSi) * 1e-3;
 end

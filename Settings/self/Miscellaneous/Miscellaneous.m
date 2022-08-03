655a78efbd8f5cd8f5071a9a911f6b8780f0a24e
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-08-03 17:54:32 +0200

    Update: add new settings for plots
    Former-commit-id: c15ab287d1e11ee72522c0c539299ff3875944bb
    Former-commit-id: 60792d40a293e308434860b42cb8cd2a6465278a

--- Settings/self/Miscellaneous/Miscellaneous.m
+++ Settings/self/Miscellaneous/Miscellaneous.m
@@ -11,24 +11,29 @@ function self = Miscellaneous()
     self.timer_0 = [];
     self.timer_loop = [];
     % * Plot
-    self.config.linewidth = 1.8;              % Default linewidth for plots
-    self.config.fontsize = 22;                % Default fontsize
-    self.config.colorpalette = 'Spectral';    % Default color palette (see brewermap function for more options)
-    self.config.colorpaletteLenght = 12;      % Default number of colors to use in the color palette
-    self.config.labelx = 'x';                 % Default xlabel
-    self.config.labely = 'y';                 % Default ylabel
+    self.config.linewidth = 1.8;              % Set linewidth for plots
+    self.config.fontsize = 20;                % Set fontsize
+    self.config.colorpalette = 'Spectral';    % Set Color palette (see brewermap function for more options)
+    self.config.colorpaletteLenght = 12;      % Set Maximum number of colors to use in the color palette
+    self.config.box = 'off';                  % Display axes outline
+    self.config.grid = 'off';                 % Display or hide axes grid lines
+    self.config.hold = 'on';                  % Retain current plot when adding new plots
+    self.config.axis = 'tight';               % Set axis limits
+    self.config.title = [];                   % Set title
+    self.config.label_type = 'medium';        % Set label with variable (short), name (medium), or name and variable (long)
+    self.config.labelx = [];                  % Set x label
+    self.config.labely = [];                  % Set y label
+    self.config.legend_name = [];             % Set the legend labels
     self.config.colorlines = [135, 205, 222;...
                                95, 188, 211;...
                                44, 137, 160;...
                                22,  68,  80]/255;
-    self.config.colorline = [0.3725, 0.7373, 0.8275];
-    self.config.colorline2 = [0.50, 0.50, 0.50];
-    self.config.colorsymbol = [0.50, 0.50, 0.50];
+    self.config.blue = [0.3725, 0.7373, 0.8275];
+    self.config.gray = [0.50, 0.50, 0.50];
     self.config.red = [0.64,0.08,0.18];
     self.config.orange = [212, 85, 0]/255;
     self.config.brown = [200, 190, 183]/255;
     self.config.brown2 = [72, 55, 55]/255;
-
     % * Flags
     self.FLAG_FIRST = true;                   % Flag indicating first calculation
     self.FLAG_FOI = true;                     % Flag indicating that the computations of the reactant mixture ....

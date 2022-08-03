afd2c5d2647d69568b4d6527be82037bdfcbe94d
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-08-03 18:19:39 +0200

    Add: include tab to do custom plots from last set of results #621

--- GUI/Functions/gui_plot_custom_figures.m
+++ GUI/Functions/gui_plot_custom_figures.m
@@ -5,28 +5,28 @@ function gui_plot_custom_figures(obj)
     % Get mixtures
     mixtures_nodes = obj.Tree_mixtures.CheckedNodes;
     % Get variables x
-    x_name = get(obj.Tree_variable_x.CheckedNodes, 'Text');
+    x_field = get(obj.Tree_variable_x.CheckedNodes, 'Text');
     % Get variables y
-    y_name = get(obj.Tree_variable_y.CheckedNodes, 'Text');
+    y_field = get(obj.Tree_variable_y.CheckedNodes, 'Text');
     % Plot settings
-    obj.Misc.config.title = 'Figure';
-    obj.Misc.config.labelx = interpret_label(x_name);
-    obj.Misc.config.labely = interpret_label(y_name);
-    obj.Misc.config.fontsize = 16;
+    Misc = Miscellaneous();
+    config = Misc.config;
+    config.labelx = interpreter_label(x_field, config.label_type); % Set x label
+    config.labely = interpreter_label(y_field, config.label_type); % Set y label
     % Clear axes
     cla(obj.UIAxes);
     % Initialize axes
-    set_figure(obj.UIAxes, obj.Misc.config);
+    set_figure(obj.UIAxes, config);
     % Plot
-    if ~iscell(y_name)
-        y_name = {y_name};
+    if ~iscell(y_field)
+        y_field = {y_field};
     end
         
     for i = length(mixtures_nodes):-1:1
         mixtures = obj.temp_results.(mixtures_nodes(i).Text);
-        x_values = cell2vector(mixtures, x_name);
-        for j = length(y_name):-1:1
-            plot_figure(x_values, mixtures, x_name, y_name{j}, obj.Misc.config, obj.PD.CompleteOrIncomplete, obj.UIAxes);
+        x_values = cell2vector(mixtures, x_field);
+        for j = length(y_field):-1:1
+            plot_figure(x_field, x_values, y_field{j}, mixtures, 'config', config, 'axes', obj.UIAxes);
         end
     end
 end

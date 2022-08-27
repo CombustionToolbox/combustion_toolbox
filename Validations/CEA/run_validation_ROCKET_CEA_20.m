213f77dbcd9ab85a4f3ea03b259e4251cacb5c55
Alberto <acuadra@ing.uc3m.es>
2022-08-27 10:46:47 +0200

    Add: validations ROCKET
    Former-commit-id: 5838d5668eb945ee84fc1b7f385bd68ab372f02d
    Former-commit-id: d6f80de058de8cfccc3e0c5c5cf5f049943ab6ae

---
+++ Validations/CEA/run_validation_ROCKET_CEA_20.m
@@ -0,0 +1,55 @@
+function problems_solved = run_validation_ROCKET_CEA_20
+    % Run test validation_ROCKET_CEA_19:
+    % Contrasted with: NASA's Chemical Equilibrium with Applications software
+    % Problem type: Equilibrium composition at exit of the rocket nozzle
+    % Pressure chamber [bar] = 101.325;
+    % Model: Finite Area Chamber (FAC)
+    % Area ratio A_c/A_t = 2;
+    % Area ratio A_e/A_t = 3;
+    % Equivalence ratio [-] = 0.5:0.01:4
+    % Initial mixture: LH2 + LOX === H2bLb + O2bLb
+    % List of species considered: ListSpecies('HYDROGEN_L')
+
+    % Inputs
+    Fuel = 'RP_1';
+    Aratio_c = 2;
+    Aratio = 3;
+    prefixDataName = 'RP1';
+    filename = {[prefixDataName, '_LOX_A20_ROCKET1.out'], [prefixDataName, '_LOX_A20_ROCKET2.out']};
+    LS =  'HC/O2/N2 PROPELLANTS';
+    DisplaySpecies = {'CO2','CO','H2O','H2','O2','C2H2_acetylene',...
+          'C2H4','C2H6','CH2CO_ketene','CH3','CH3CHO_ethanal','CH3OH',...
+          'CH4','COOH','H','H2O2','HCHO_formaldehy','HCO','HCOOH','HO2',...
+          'O','OH','Cbgrb'};
+    tolN = 1e-18;
+
+    % Load results CEA 
+    results_CEA = data_CEA(filename, DisplaySpecies);
+    % Combustion Toolbox
+    results_CT = run_CT('ProblemType', 'ROCKET',...
+                        'TR', 298.15,...
+                        'pR', 101.325,...
+                        'Species', LS,...
+                        'S_Fuel', Fuel,...
+                        'S_Oxidizer', 'O2bLb',...
+                        'ratio_oxidizers_O2', 1,...
+                        'EquivalenceRatio', 0.5:0.01:4,...
+                        'tolN', tolN,...
+                        'FLAG_IAC', false,...
+                        'Aratio_c', Aratio_c,...
+                        'Aratio', Aratio);
+    problems_solved = length(results_CT.PD.range);
+    
+    % Display validation (plot)
+    results_CT.Misc.config.axis = 'tight';
+    % * Molar fractions
+    fig1 = plot_molar_fractions_validation(results_CT, results_CEA, 'phi', 'Xi', DisplaySpecies);
+    % * Properties mixture Exit - 1
+    fig2 = plot_properties_validation(results_CT, results_CEA, {'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi'}, {'T', 'p', 'h', 'cP', 'cV', 'gamma_s', 'u', 'I_sp', 'I_vac'}, 'mix2');
+    % Save plots
+    folderpath = strcat(pwd,'\Validations\Figures\');
+    stack_trace = dbstack;
+    filename = stack_trace.name;
+    saveas(fig1, strcat(folderpath, filename, '_molar'), 'svg');
+    saveas(fig2, strcat(folderpath, filename, '_properties_1'), 'svg');
+end

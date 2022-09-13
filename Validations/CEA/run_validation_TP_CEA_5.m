fa577a9596e7366993386d353d30c24009b62312
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-09-13 19:47:08 +0200

    Update: rename variables
    Former-commit-id: db0bdbfd21efbdd816daa9402630c9f004d1e67e
    Former-commit-id: 37cc7ddc11c7763bebc890136f8f4bb88c9590cf

--- Validations/CEA/run_validation_TP_CEA_5.m
+++ Validations/CEA/run_validation_TP_CEA_5.m
@@ -13,7 +13,7 @@ function problems_solved = run_validation_TP_CEA_5
     prefixDataName = Fuel;
     filename = {strcat(prefixDataName, '_air_T_TP1.out'), strcat(prefixDataName, '_air_T_TP2.out')};
     LS =  'Soot Formation Extended';
-    DisplaySpecies = {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
+    display_species = {'CO2', 'CO', 'H2O', 'H2', 'O2', 'N2', 'He', 'Ar',...
                       'HCN','H','OH','O','CN','NH3','CH4','C2H4','CH3',...
                       'NO','HCO','NH2','NH','N','CH','Cbgrb'};
     tolN = 1e-18;
@@ -28,10 +28,10 @@ function problems_solved = run_validation_TP_CEA_5
                         'tolN', tolN);
     problems_solved = length(results_CT.PD.range);
     % Load results CEA 
-    results_CEA = data_CEA(filename, DisplaySpecies);
+    results_CEA = data_CEA(filename, display_species);
     % Display validation (plot)
     % * Molar fractions
-    fig1 = plot_molar_fractions_validation(results_CT, results_CEA, 'phi', 'Xi', DisplaySpecies);
+    fig1 = plot_molar_fractions_validation(results_CT, results_CEA, 'phi', 'Xi', display_species);
     % * Properties mixture 2
     fig2 = plot_properties_validation(results_CT, results_CEA, {'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi', 'phi'}, {'rho', 'h', 'e', 'g', 'S', 'cP', 'cV', 'gamma_s'}, 'mix2');
     % Save plots

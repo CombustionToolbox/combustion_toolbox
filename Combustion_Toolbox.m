53889c8b3770443cda60c80244315b566fe4ac17
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-02 10:35:45 +0100

    Solve: typo
    Former-commit-id: 0dda69508ee02085d6e5fbd7e2bcf13241ce24af

--- Combustion_Toolbox.m
+++ Combustion_Toolbox.m
@@ -57,7 +57,7 @@
 %   * Example_DET_OVERDRIVEN_R
 %   * Example_DET_UNDERDRIVEN
 %   * Example_DET_UNDERDRIVEN_R
-%   * Example_DET_OVERDRIVEN_AND_OVERDRIVEN
+%   * Example_DET_OVERDRIVEN_AND_UNDERDRIVEN
 %   * Example_DET_OBLIQUE_BETA
 %   * Example_DET_OBLIQUE_THETA
 %   * Example_DET_POLAR
@@ -72,7 +72,7 @@
 %          PhD Candidate - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                  
-% Last update Nov 01 2022
+% Last update Nov 02 2022
 % -------------------------------------------------------------------------
 help Combustion_Toolbox.m
 

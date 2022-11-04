7b0912fd5ee6fbbf047244f5aaf5507b915236a4
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-04 12:23:21 +0100

    Solve: error when changing equivalence-ratio/composition after clicked a node in the UITree #694
    Former-commit-id: aa1845b6844c346ed6da467f1c3283b8f0f48808

--- gui/utils/gui_write_results.m
+++ gui/utils/gui_write_results.m
@@ -1,4 +1,4 @@
-function gui_write_results(obj, results, i, varargin)
+function gui_write_results(app, results, i, varargin)
     % Update GUI with the results of the ith case solved
     
     % Default values
@@ -11,53 +11,31 @@ function gui_write_results(obj, results, i, varargin)
         end
     end
     % Update properties
-    update_properties(obj, results, i);
+    update_properties(app, results, i);
     % Update mixture (tables)
-    update_mixtures(obj, results, i, FLAG_REACTANTS);
+    update_mixtures(app, results, i, FLAG_REACTANTS);
     % Update equivalence ratio
-    update_equivalence_ratio(obj, results, i);
+    update_equivalence_ratio(app, results, i);
 end
 
 % SUB-PASS FUNCTIONS
-function update_properties(obj, results, i)
+function update_properties(app, results, i)
     mix1 = results(i).mix1;
     mix2 = results(i).mix2;
-    obj.text_TR.Value = temperature(mix1);
-    obj.text_TP.Value = temperature(mix2);
-    obj.text_pR.Value = pressure(mix1);
-    obj.text_pP.Value = pressure(mix2);
-    obj.text_rR.Value = density(mix1);
-    obj.text_rP.Value = density(mix2);
-    obj.text_hR.Value = enthalpy_mass(mix1);
-    obj.text_hP.Value = enthalpy_mass(mix2);
-    obj.text_eR.Value = intEnergy_mass(mix1);
-    obj.text_eP.Value = intEnergy_mass(mix2);
-    obj.text_cpR.Value = cp_mass(mix1);
-    obj.text_cpP.Value = cp_mass(mix2);
-    try
-        obj.text_sR.Value = entropy_mass(mix1);
-    catch
-        obj.text_sR.Value = Inf;
-    end
-    obj.text_sP.Value = entropy_mass(mix2);
-    obj.text_gammaR.Value = adiabaticIndex(mix1);
-    obj.text_gammaP.Value = adiabaticIndex(mix2);
-    obj.text_WR.Value = MolecularWeight(mix1);
-    obj.text_WP.Value = MolecularWeight(mix2);
-    obj.text_soundR.Value = soundspeed(mix1);
-    obj.text_soundP.Value = soundspeed(mix2);
+    
+    update_properties_common(app, mix1, 'R');
+    update_properties_common(app, mix2, 'P');
+
     if strcmpi(results(i).ProblemType, 'TP')
-        obj.text_error_problem.Value = mix2.error_moles;
+        app.text_error_problem.Value = mix2.error_moles;
     else
-        obj.text_error_problem.Value = mix2.error_problem;
+        app.text_error_problem.Value = mix2.error_problem;
     end
     if contains(results(i).ProblemType, 'SHOCK', 'IgnoreCase', true) || contains(results(i).ProblemType, 'DET', 'IgnoreCase', true) || contains(results(i).ProblemType, 'ROCKET', 'IgnoreCase', true)
-        obj.text_uR.Value = velocity_relative(mix1);
-        obj.text_uP.Value = mix2.v_shock;
-        obj.text_MR.Value = velocity_relative(mix1)/soundspeed(mix1);
-        obj.text_MP.Value = mix2.v_shock/soundspeed(mix2);
+        update_properties_velocities(app, mix1, 'R')
+        update_properties_velocities(app, mix2, 'P')
         if contains(results(i).ProblemType, 'ROCKET', 'IgnoreCase', true)
-            obj.text_Aratio_2.Value = mix2.Aratio;
+            app.text_Aratio_2.Value = mix2.Aratio;
             
             if isfield(results(i), 'mix2_c')
                 mix3 = results(i).mix2_c;
@@ -66,22 +44,10 @@ function update_properties(obj, results, i)
                 mix3 = results(i).mix3;
                 FLAG_IAC = true;
             end
-            obj.text_TP_3.Value = temperature(mix3);
-            obj.text_pP_3.Value = pressure(mix3);
-            obj.text_rP_3.Value = density(mix3);
-            obj.text_hP_3.Value = enthalpy_mass(mix3);
-            obj.text_eP_3.Value = intEnergy_mass(mix3);
-            obj.text_cpP_3.Value = cp_mass(mix3);
-            obj.text_sP_3.Value = entropy_mass(mix3);
-            obj.text_gammaP_3.Value = adiabaticIndex(mix3);
-            obj.text_WP_3.Value = MolecularWeight(mix3);
-            obj.text_soundP_3.Value = soundspeed(mix3);
-            obj.text_uP_3.Value = mix3.v_shock;
-            obj.text_MP_3.Value = mix3.v_shock/soundspeed(mix3);
-            obj.text_Aratio_3.Value = mix3.Aratio;
-            obj.text_Cstar_3.Value = mix3.cstar;
-            obj.text_Ivac_3.Value = mix3.I_vac;
-            obj.text_Isp_3.Value = mix3.I_sp;
+            
+            update_properties_common(app, mix3, '_3');
+            update_properties_velocities(app, mix2, '_3')
+            update_properties_rocket(app, mix3, '_3')
             
             if ~isempty(results(i).strP) || ~FLAG_IAC
 
@@ -91,51 +57,83 @@ function update_properties(obj, results, i)
                     mix4 = results(i).strP;
                 end
                 
-                obj.text_TP_4.Value = temperature(mix4);
-                obj.text_pP_4.Value = pressure(mix4);
-                obj.text_rP_4.Value = density(mix4);
-                obj.text_hP_4.Value = enthalpy_mass(mix4);
-                obj.text_eP_4.Value = intEnergy_mass(mix4);
-                obj.text_cpP_4.Value = cp_mass(mix4);
-                obj.text_sP_4.Value = entropy_mass(mix4);
-                obj.text_gammaP_4.Value = adiabaticIndex(mix4);
-                obj.text_WP_4.Value = MolecularWeight(mix4);
-                obj.text_soundP_4.Value = soundspeed(mix4);
-                obj.text_uP_4.Value = mix4.v_shock;
-                obj.text_MP_4.Value = mix4.v_shock/soundspeed(mix4);
-                obj.text_Aratio_4.Value = mix4.Aratio;
-                obj.text_Cstar_4.Value = mix4.cstar;
-                obj.text_Ivac_4.Value = mix4.I_vac;
-                obj.text_Isp_4.Value = mix4.I_sp;
+                update_properties_common(app, mix4, '_4');
+                update_properties_velocities(app, mix4, '_4')
+                update_properties_rocket(app, mix4, '_4')
 
                 % Get max error method
-                obj.text_error_problem.Value = max([mix2.error_problem, mix3.error_problem, mix4.error_problem]);
+                app.text_error_problem.Value = max([mix2.error_problem, mix3.error_problem, mix4.error_problem]);
             else
-                obj.text_TP_4.Value = 0;
-                obj.text_pP_4.Value = 0;
-                obj.text_rP_4.Value = 0;
-                obj.text_hP_4.Value = 0;
-                obj.text_eP_4.Value = 0;
-                obj.text_cpP_4.Value = 0;
-                obj.text_sP_4.Value = 0;
-                obj.text_gammaP_4.Value = 0;
-                obj.text_WP_4.Value = 0;
-                obj.text_soundP_4.Value = 0;
-                obj.text_uP_4.Value = 0;
-                obj.text_MP_4.Value = 0;
-                obj.text_Aratio_4.Value = 0;
-                obj.text_Cstar_4.Value = 0;
-                obj.text_Ivac_4.Value = 0;
-                obj.text_Isp_4.Value = 0;
+                app.text_TP_4.Value = 0;
+                app.text_pP_4.Value = 0;
+                app.text_rP_4.Value = 0;
+                app.text_hP_4.Value = 0;
+                app.text_eP_4.Value = 0;
+                app.text_cpP_4.Value = 0;
+                app.text_sP_4.Value = 0;
+                app.text_gammaP_4.Value = 0;
+                app.text_WP_4.Value = 0;
+                app.text_soundP_4.Value = 0;
+                app.text_uP_4.Value = 0;
+                app.text_MP_4.Value = 0;
+                app.text_Aratio_4.Value = 0;
+                app.text_Cstar_4.Value = 0;
+                app.text_Ivac_4.Value = 0;
+                app.text_Isp_4.Value = 0;
 
                 % Get max error method
-                obj.text_error_problem.Value = max(mix2.error_problem, mix3.error_problem);
+                app.text_error_problem.Value = max(mix2.error_problem, mix3.error_problem);
             end
         end
     end
 end
 
-function update_mixtures(obj, results, i, FLAG_REACTANTS)
+function update_properties_common(app, mix, suffix)
+    % Update common mixture properties
+    app.(['text_T', suffix]).Value = temperature(mix);
+    app.(['text_p', suffix]).Value = pressure(mix);
+    app.(['text_r', suffix]).Value = density(mix);
+    app.(['text_h', suffix]).Value = enthalpy_mass(mix);
+    app.(['text_e', suffix]).Value = intEnergy_mass(mix);
+    app.(['text_cp', suffix]).Value = cp_mass(mix);
+    try
+        app.(['text_s', suffix]).Value = entropy_mass(mix);
+    catch
+        app.(['text_s', suffix]).Value = Inf;
+    end
+    app.(['text_gamma', suffix]).Value = adiabaticIndex(mix);
+    app.(['text_W', suffix]).Value = MolecularWeight(mix);
+    app.(['text_sound', suffix]).Value = soundspeed(mix);
+end
+
+function update_properties_velocities(app, mix, suffix)
+    % Update velocities and Mach numbers of the mixture
+    
+    % Shock velocity (pre-shock or post-shock)
+    switch lower(suffix)
+        case {'r', '1', '_1'}
+            app.(['text_u', suffix]).Value = velocity_relative(mix);
+        otherwise
+            app.(['text_u', suffix]).Value = mix.v_shock;
+    end
+    % Mach number
+    app.(['text_M', suffix]).Value = app.(['text_u', suffix]).Value / soundspeed(mix);
+end
+
+function update_properties_rocket(app, mix, suffix)
+    % Update rocket propellant performance parameters
+    app.(['text_Aratio', suffix]).Value = mix.Aratio;
+    app.(['text_Cstar', suffix]).Value = mix.cstar;
+    app.(['text_Ivac', suffix]).Value = mix.I_vac;
+    app.(['text_Isp', suffix]).Value = mix.I_sp;
+end
+
+function update_error_method()
+    % Update error of the method employed
+
+end
+
+function update_mixtures(app, results, i, FLAG_REACTANTS)
     % Update mixture tables with the results of the ith case solved
     if FLAG_REACTANTS
         species = results(i).UITable_R_Data(:, 1);
@@ -143,18 +141,18 @@ function update_mixtures(obj, results, i, FLAG_REACTANTS)
         temperature = results(i).UITable_R_Data(:, 5);
         ind_species = find_ind(results(i).LS, species);
         [N, Xi, ind_sort] = sort_mixture(results, 'mix1', i, ind_species);
-        data = table2cell(table(species(ind_sort), Xi .* N, Xi, type, temperature));
+        data = table2cell(table(species(ind_sort), Xi .* N, Xi, type(ind_sort), temperature(ind_sort)));
 
-        obj.UITable_R.Data = data;
-        obj.UITable_R2.Data = data(:, 1:3);
+        app.UITable_R.Data = data;
+        app.UITable_R2.Data = data(:, 1:3);
         % Update GUI: ListProducts
-        obj.listbox_Products.Items = results(i).LS;
+        app.listbox_Products.Items = results(i).LS;
     end
     species = results(i).LS';
     ind_species = find_ind(results(i).LS, species);
     [N, Xi, ind_sort] = sort_mixture(results, 'mix2', i, ind_species);
     data = table2cell(table(species(ind_sort), Xi .* N, Xi));
-    obj.UITable_P.Data = data;
+    app.UITable_P.Data = data;
 end
 
 function [N, Xi, ind_sort] = sort_mixture(results, mix, i, ind_species)
@@ -163,15 +161,15 @@ function [N, Xi, ind_sort] = sort_mixture(results, mix, i, ind_species)
     [Xi, ind_sort] = sort(results(i).(mix).Xi(ind_species), 'descend');
 end
 
-function update_equivalence_ratio(obj, results, i)
+function update_equivalence_ratio(app, results, i)
     % Update GUI: equivalence ratio, O/F, and percentage Fuel
     if strcmp(results(i).mix1.phi, '-')
-        obj.edit_phi.Value = '-';
+        app.edit_phi.Value = '-';
     else
-        obj.edit_phi.Value = sprintf('%.5g', results(i).mix1.phi); 
+        app.edit_phi.Value = sprintf('%.5g', results(i).mix1.phi); 
     end
-    obj.edit_phi2.Value = obj.edit_phi.Value;
-    obj.edit_phi3.Value = obj.edit_phi.Value;
-    obj.edit_OF.Value = 1/results(i).mix1.FO;
-    obj.edit_F.Value = results(i).mix1.percentage_Fuel;
+    app.edit_phi2.Value = app.edit_phi.Value;
+    app.edit_phi3.Value = app.edit_phi.Value;
+    app.edit_OF.Value = 1/results(i).mix1.FO;
+    app.edit_F.Value = results(i).mix1.percentage_Fuel;
 end

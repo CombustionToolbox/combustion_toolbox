71b5dd5e370ed4c037a11bcb2269b9bfde4ca35f
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-11 10:50:11 +0100

    Update: solve minor bugs and update installers
    Former-commit-id: 57b401acc7879695f52a930fef7f37f42c0df50b

--- utils/get_combustion_toolbox_version.m
+++ utils/get_combustion_toolbox_version.m
@@ -1,5 +1,5 @@
 function [release, date] = get_combustion_toolbox_version()
     % Get Combustion Toolbox version
-    release = 'v0.9.99';
-    date = '01 Nov 2022';
+    release = 'v0.9.99b';
+    date = '11 Nov 2022';
 end

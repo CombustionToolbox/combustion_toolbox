b3e79c8abafe467323edb4c9cf3484f4b3207c93
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-12 16:21:07 +0100

    Update: generalization - include FLAG_SPECIES
    Former-commit-id: 58bb46140cf9973d17e10c042fb69a12a3c9ec42

--- utils/display/set_legends.m
+++ utils/display/set_legends.m
@@ -1,11 +1,22 @@
 function set_legends(ax, legend_name, varargin)
     % Set legend to the given axes
-    if nargin < 3
-        config.fontsize = 18;
-    else
-        config = varargin{1};
+
+    % Default values
+    config.fontsize = 18;
+    FLAG_SPECIES = true;
+    % Unpack inputs
+    for i = 1:2:nargin-2
+        switch lower(varargin{i})
+            case {'config'}
+                config = varargin{i + 1};
+            case {'flag', 'flag_species'}
+                FLAG_SPECIES = varargin{i + 1};
+        end
     end
 
-    legend_name = strrep(legend_name, '_', '\_');
+    if FLAG_SPECIES
+        legend_name = strrep(legend_name, '_', '\_');
+    end
+
     legend(ax, legend_name, 'FontSize', config.fontsize - 4, 'Location', 'best', 'interpreter', 'latex');
 end

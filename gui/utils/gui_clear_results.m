25f88e1ab679586cbd2cf5851856ee770ca49540
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-12 16:27:24 +0100

    Update: generalize routine

--- gui/utils/gui_clear_results.m
+++ gui/utils/gui_clear_results.m
@@ -1,22 +1,40 @@
 function gui_clear_results(app)
-    % Function that clears the result tab panel, setting them to 0, or
-    % empty.
+    % Function that clears the result tab panel, setting them to 0
     
     % Clear Tab results: parameters
-    app.text_rR.Value = 0;
-    app.text_hR.Value = 0;
-    app.text_sR.Value = 0;
-    app.text_cpR.Value = 0;
-    app.text_WR.Value = 0;
-    app.text_soundR.Value = 0;
-    app.text_gammaR.Value = 0;
-    app.text_eR.Value = 0;
-    app.text_eP.Value = 0;
-    app.text_rP.Value = 0;
-    app.text_hP.Value = 0;
-    app.text_sP.Value = 0;
-    app.text_cpP.Value = 0;
-    app.text_WP.Value = 0;
-    app.text_soundP.Value = 0;
-    app.text_gammaP.Value = 0;
+    update_properties_common(app, 0, '_1');
+    update_properties_common(app, 0, '_2');
+    update_properties_common(app, 0, '_3');
+    update_properties_common(app, 0, '_4');
+    update_properties_common(app, 0, '_5');
+    update_properties_rocket(app, 0, '_2');
+    update_properties_rocket(app, 0, '_3');
+    update_properties_rocket(app, 0, '_4');
+    update_properties_rocket(app, 0, '_5');
+    % Clear error
+    app.text_error_problem.Value = 0;
+end
+
+function update_properties_common(app, value, suffix)
+    % Update properties to the given value
+    app.(['text_T', suffix]).Value = value;
+    app.(['text_p', suffix]).Value = value;
+    app.(['text_r', suffix]).Value = value;
+    app.(['text_h', suffix]).Value = value;
+    app.(['text_e', suffix]).Value = value;
+    app.(['text_cp', suffix]).Value = value;
+    app.(['text_s', suffix]).Value = value;
+    app.(['text_gamma', suffix]).Value = value;
+    app.(['text_W', suffix]).Value = value;
+    app.(['text_sound', suffix]).Value = value;
+    app.(['text_u', suffix]).Value = value;
+    app.(['text_M', suffix]).Value = value;
+end
+
+function update_properties_rocket(app, value, suffix)
+    % Update rocket propellant performance parameters
+    app.(['text_Aratio', suffix]).Value = value;
+    app.(['text_Cstar', suffix]).Value = value;
+    app.(['text_Ivac', suffix]).Value = value;
+    app.(['text_Isp', suffix]).Value = value;
 end

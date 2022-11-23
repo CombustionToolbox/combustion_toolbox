a5b62c35c62a2418b68416f9f0ab8249f9042e76
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-23 10:05:28 +0100

    Update: include Rankine-Hugoniot slope parameter
    Former-commit-id: 80f9f89a15747e7ad3c84a2ac818ccb0dd466a5e
    Former-commit-id: 9f9cba70286399ac1b6c93fe78e8d1f60a41ec44

---
+++ utils/compute_Gammas.m
@@ -0,0 +1,13 @@
+function Gammas = compute_Gammas(M1, R, P)
+    % Compute slope of the Hugoniot curve for thermochemically frozen air
+    %
+    % Args:
+    %     M1 (float):        Pre-shock Mach number [-]
+    %     Density ratio [-]: Density ratio [-]
+    %     P (float):         Pressure ratio [-]
+    %
+    % Returns:
+    %     - Gammas (float): Slope of the Hugoniot curve [-]
+
+    Gammas =  7/5 * (M1(1:end-1).^2 ./ R(1:end-1).^2) .* compute_first_derivative(P, R).^(-1);
+end

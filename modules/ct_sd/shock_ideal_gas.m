e9b774ce2695c4901c5520dda9df9373403235fc
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-23 10:05:28 +0100

    Update: include Rankine-Hugoniot slope parameter
    Former-commit-id: 80f9f89a15747e7ad3c84a2ac818ccb0dd466a5e

--- modules/ct_sd/shock_ideal_gas.m
+++ modules/ct_sd/shock_ideal_gas.m
@@ -1,4 +1,4 @@
-function [R, P, T] = shock_ideal_gas(gamma, M1)
+function [R, P, T, Gammas, M1] = shock_ideal_gas(gamma, M1)
     % Compute jump conditions assuming a thermochemically frozen gas
     %
     % Args:
@@ -8,11 +8,24 @@ function [R, P, T] = shock_ideal_gas(gamma, M1)
     % Returns:
     %     Tuple containing
     %
-    %     - R (float):     Density ratio [-]
-    %     - P (float):     Pressure ratio [-]
-    %     - T (float):     Temperature ratio [-]
+    %     - R (float):      Density ratio [-]
+    %     - P (float):      Pressure ratio [-]
+    %     - T (float):      Temperature ratio [-]
+    %     - Gammas (float): Rankine-Hugoniot slope parameter [-]
 
     R = (gamma + 1) .* M1.^2 ./ ((gamma - 1) .* M1.^2 + 2);
     P = (2 * gamma * M1.^2 - (gamma - 1)) ./ (gamma + 1);
     T = (((gamma - 1) .* M1.^2 + 2) .* (2 * gamma * M1.^2 - (gamma - 1))) ./ ((gamma + 1).^2 .* M1.^2);
+
+    if length(M1) > 1
+        % Compute Gamma
+        Gammas = compute_Gammas(M1, R, P);
+        % Remove last point of the dataset to match dimensions with Gammas
+        R = R(1:end-1);
+        P = P(1:end-1);
+        T = T(1:end-1);
+        M1 = M1(1:end-1);
+    else
+        Gammas = [];
+    end
 end

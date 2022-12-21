e0b07c3bd6218843de38b489f02c7c395455322f
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-12-21 18:05:19 +0100

    Update: compute only cj speed for first case

--- modules/ct_sd/det_underdriven.m
+++ modules/ct_sd/det_underdriven.m
@@ -12,22 +12,27 @@ function [mix1, mix2] = det_underdriven(self, mix1, drive_factor, varargin)
     % Returns:
     %     Tuple containing
     %
-    %     - mix1 (struct): Properties of the mixture in the pre-shock state
-    %     - mix2 (struct): Properties of the mixture in the post-shock state
-
+    %     * mix1 (struct): Properties of the mixture in the pre-shock state
+    %     * mix2 (struct): Properties of the mixture in the post-shock state
+    
+    % Definitions
+    zeta = 0.1;
     % Unpack input data
     [self, mix1, mix2] = unpack(self, mix1, drive_factor, varargin);
-    % Compute CJ state
-    [str1_cj, str2_cj] = det_cj(self, mix1);
-    mix1.cj_speed = str1_cj.u;
-    % Adjust guess for underdriven detonation
-    u1 = mix1.cj_speed * drive_factor; % [m/s]
-    p1 = convert_bar_to_Pa(mix1.p); % [Pa]
-    mix2.rho = 1 / (0.1 / str2_cj.rho + 0.9 / mix1.rho);
-    mix2.p = convert_Pa_to_bar(p1 - mix1.rho * u1^2 * (mix1.rho / mix2.rho - 1));
-    mix2.Xi = str2_cj.Xi;
-    mix2.N = str2_cj.N;
-    mix2.T = mix2.p / (mix2.rho * self.C.R0 / (str2_cj.W)) * 1e2; % assuming ideal EoS
+    if isempty(mix1.cj_speed)
+        % Compute CJ state
+        [str1_cj, str2_cj] = det_cj(self, mix1);
+        mix1.cj_speed = str1_cj.u;
+        % Adjust guess for underdriven detonation
+        u1 = mix1.cj_speed * drive_factor; % [m/s]
+        p1 = convert_bar_to_Pa(mix1.p); % [Pa]
+        mix2.rho = 1 / (zeta / str2_cj.rho + (1 - zeta) / mix1.rho);
+        mix2.p = convert_Pa_to_bar(p1 - mix1.rho * u1^2 * (mix1.rho / mix2.rho - 1));
+        mix2.Xi = str2_cj.Xi;
+        mix2.N = str2_cj.N;
+        mix2.T = mix2.p / (mix2.rho * self.C.R0 / (str2_cj.W)) * 1e2; % assuming ideal EoS
+    end
+    
     % Solve detonation
     [mix1, mix2] = shock_incident(self, mix1, mix1.cj_speed * mix1.drive_factor, mix2);
     % Assign CJ speed

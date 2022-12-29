b7e600d1d304fc5497ff28613148e675258c0708
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-12-29 16:40:05 +0100

    Update: version

--- utils/get_combustion_toolbox_version.m
+++ utils/get_combustion_toolbox_version.m
@@ -1,5 +1,5 @@
 function [release, date] = get_combustion_toolbox_version()
     % Get Combustion Toolbox version
-    release = 'v0.9.99b';
-    date = '12 Nov 2022';
+    release = 'v0.9.99c';
+    date = '29 Dec 2022';
 end

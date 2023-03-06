73ac5e108d0b4ca30ba0b76328d354e0ea227d6f
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-03-06 16:50:27 +0100

    Add: validation with multiple condensed species
    Former-commit-id: a93fd2e845b525601bbee0e96a6c16b17facfd21

--- examples/Example_TP_scoggins2015.m
+++ examples/Example_TP_scoggins2015.m
@@ -1,24 +1,24 @@
 % -------------------------------------------------------------------------
-% EXAMPLE: TP
+% EXAMPLE: TP_scoggins2015
 %
-% Compute equilibrium composition at defined temperature (e.g., 3000 K) and
-% pressure (e.g., 1.01325 bar) for lean to rich CH4-air mixtures at
-% standard conditions, a set of 26 species considered and a set of
-% equivalence ratios phi contained in (0.5, 5) [-]
-%   
-% Soot formation == {'CO2','CO','H2O','H2','O2','N2','He','Ar','Cbgrb',...
-%                    'C2','C2H4','CH','CH','CH3','CH4','CN','H',...
-%                    'HCN','HCO','N','NH','NH2','NH3','NO','O','OH'}
-%   
-% See wiki or list_species() for more predefined sets of species
+% Compute equilibrium composition for a defined set of temperatures
+% (200, 5000) [K] at atmospheric pressure (1.01325 bar) for a Si-C6H5OH_phenol
+% mixture at standard conditions, and considering a set of 111 species 
+% (7 in condensed phase)
+% 
+% This example is obtained from [1]
+% 
+% [1] Scoggins, J. B., & Magin, T. E. (2015). Gibbs function continuation
+%     for linearly constrained multiphase equilibria. Combustion and Flame,
+%     162(12), 4514-4522.
 %
 % @author: Alberto Cuadra Lara
 %          PhD Candidate - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                  
-% Last update July 22 2022
+% Last update Jan 17 2023
 % -------------------------------------------------------------------------
-LS = {'Ar','C','C10H8_naphthale','C2','C2H','C2H2_acetylene',...
+LS = {'C','C2','C2H','C10H8_naphthale','C2H2_acetylene',...
     'C2H2_vinylidene','C2H3_vinyl','C2H4','C2H4O_ethylen_o','C2H5',...
     'C2H5OH','C2H6','C2N2','C2O','C3','C3H3_1_propynl',...
     'C3H3_2_propynl','C3H4_allene','C3H4_propyne','C3H5_allyl',...
@@ -31,21 +31,23 @@ LS = {'Ar','C','C10H8_naphthale','C2','C2H','C2H2_acetylene',...
     'C8H8_styrene','CH','CH2','CH2CO_ketene','CH2OH','CH3',...
     'CH3CHO_ethanal','CH3CN','CH3COOH','CH3CO_acetyl','CH3O',...
     'CH3OCH3','CH3OH','CH4','CN','CO','CO2','COOH','Cbgrb','H','H2',...
-    'H2O','H2O2','H2ObLb','H2Obcrb','HCCO','HCHO_formaldehy','HCN',...
-    'HCO','HCOOH','HNC','HNCO','HNO','HO2','He','N','N2','N2O','NCO',...
+    'H2O','H2ObLb','H2Obcrb','H2O2','HCCO','HCHO_formaldehy','HCN',...
+    'HCO','HCOOH','HNC','HNCO','HNO','HO2','N','N2','N2O','NCO',...
     'NH','NH2','NH2OH','NH3','NO','NO2','O','O2','OCCN','OH','Si',...
-    'Si2','Si2C','Si3','SiC','SiC2','SiCbbb','SiH','SiH2','SiH3',...
-    'SiH4','SiO','SiO2','SiO2ba_qzb','SiO2bb_crtb','SiO2bb_qzb'};
+    'Si2','Si2C','Si3','SiC','SiC2','SiH','SiH2','SiH3',...
+    'SiH4','SiO','SiO2','SiCbbb','SiO2ba_qzb','SiO2bb_qzb','SiO2bb_crtb'};
 %% INITIALIZE
 self = App(LS);
 %% INITIAL CONDITIONS
 self = set_prop(self, 'TR', 300, 'pR', 1.01325);
-self.PD.S_Fuel = {'C6H5OH_phenol', 'Si'};
-self.PD.N_Fuel = [9, 1];
+self.PD.S_Fuel = {'Si', 'C6H5OH_phenol'};
+self.PD.N_Fuel = [1, 9];
 %% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
-self = set_prop(self, 'pP', self.PD.pR.value, 'TP', 2000:50:5000);
-self.Misc.display_species = {'H2', 'H', 'CH4', 'C2H2_acetylene', 'SiC2', 'Si', 'C', 'C2H', 'C3', 'CO', 'CO2', 'Cbgrb', 'SiCbbb'};
-self.C.mintol_display = 1e-4;
+self = set_prop(self, 'pP', self.PD.pR.value, 'TP', 200:10:5000);
+self.Misc.display_species = {'H2', 'H', 'CH4', 'C2H2_acetylene', 'SiC2',...
+    'Si', 'C', 'C2H', 'C3', 'CO', 'CO2', 'Cbgrb', 'SiCbbb', 'H2O',...
+    'SiO2ba_qzb','SiO2bb_qzb','SiO2bb_crtb','H2ObLb','H2Obcrb'};
+self.C.mintol_display = 1e-3;
 self.TN.tolN = 1e-16;
 %% SOLVE PROBLEM
 self = solve_problem(self, 'TP');

ba3a116f65b2874b7697fd1c9444039c32a71612
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-03-06 17:12:05 +0100

    Update: reorganize CT-ROCKET module
    Former-commit-id: a4a9f46ccdb7517ba0d2735f46fc4d85a859cbca

--- modules/ct_rocket/rocket_performance.m
+++ modules/ct_rocket/rocket_performance.m
@@ -1,9 +1,9 @@
-function [mix1, mix2_inj, mix2_c, mix3, mix4] = rocket_performance(self, mix1, Aratio, varargin)
+function [mix1, mix2_inj, mix2_c, mix3, mix4] = rocket_performance(self, mix1, varargin)
     % Routine that computes the propellant rocket performance
     %
     % Methods implemented:
     %   * Infinite-Area-Chamber (IAC)
-    %   * Finite-Area-Chamber (FAC) - NOT YET
+    %   * Finite-Area-Chamber (FAC)
     %
     % This method is based on Gordon, S., & McBride, B. J. (1994). NASA reference publication,
     % 1311.
@@ -11,25 +11,26 @@ function [mix1, mix2_inj, mix2_c, mix3, mix4] = rocket_performance(self, mix1, A
     % Args:
     %     self (struct): Data of the mixture, conditions, and databases
     %     mix1 (struct): Properties of the initial mixture
-    %     Aratio (struct): Ratio area_exit / area_throat
     %
     % Optional Args:
-    %     - mix2 (struct): Properties of the mixture at the outlet of the chamber (previous calculation)
-    %     - mix3 (struct): Properties of the mixture at the throat (previous calculation)
+    %     * Aratio (struct): Ratio area_exit / area_throat
+    %     * mix2 (struct): Properties of the mixture at the outlet of the chamber (previous calculation)
+    %     * mix3 (struct): Properties of the mixture at the throat (previous calculation)
     %
     % Returns:
     %     Tuple containing
     %
-    %     - mix1 (struct): Properties of the initial mixture
-    %     - mix2 (struct): Properties of the mixture at the outlet of the chamber
-    %     - mix3 (struct): Properties of the mixture at the throat
-    %     - mix4 (struct): Properties of the mixture at the given exit points
-
-    % Assign values
-    if nargin > 3, mix2_inj = get_struct(varargin{1}); else, mix2_inj = []; end
-    if nargin > 4, mix2_c = get_struct(varargin{2}); else, mix2_c = []; end
-    if nargin > 5, mix3 = get_struct(varargin{3}); else, mix3 = []; end
-    if nargin > 6, mix4 = get_struct(varargin{4}); else, mix4 = []; end
+    %     * mix1 (struct): Properties of the initial mixture
+    %     * mix2 (struct): Properties of the mixture at the outlet of the chamber
+    %     * mix3 (struct): Properties of the mixture at the throat
+    %     * mix4 (struct): Properties of the mixture at the given exit points
+    
+    % Unpack additional input parameters
+    if nargin > 2, Aratio = varargin{1}; else, Aratio = []; end
+    if nargin > 3, mix2_inj = get_struct(varargin{2}); else, mix2_inj = []; end
+    if nargin > 4, mix2_c = get_struct(varargin{3}); else, mix2_c = []; end
+    if nargin > 5, mix3 = get_struct(varargin{4}); else, mix3 = []; end
+    if nargin > 6, mix4 = get_struct(varargin{5}); else, mix4 = []; end
     % Compute chemical equilibria at different points of the rocket
     % depending of the model selected
     [mix2_inj, mix2_c, mix3, mix4] = solve_model_rocket(self, mix1, mix2_inj, mix2_c, mix3, mix4, Aratio);
@@ -39,18 +40,18 @@ function [mix1, mix2_inj, mix2_c, mix3, mix4] = rocket_performance(self, mix1, A
     if self.PD.FLAG_IAC
         % Velocity at the outlet of the chamber
         mix2_c.u = 0; mix2_c.v_shock = 0;
-        [mix3, mix4] = compute_rocket_parameters(mix2_c, mix3, self.C.gravity, mix4);
+        [mix3, mix4] = rocket_parameters(mix2_c, mix3, self.C.gravity, mix4);
     else
         % Velocity at the injector
         mix2_inj.u = 0; mix2_inj.v_shock = 0;
-        [mix3, mix2_c, mix4] = compute_rocket_parameters(mix2_inj, mix3, self.C.gravity, mix2_c, mix4);
+        [mix3, mix2_c, mix4] = rocket_parameters(mix2_inj, mix3, self.C.gravity, mix2_c, mix4);
     end
 
 end
 
 % SUB-PASS FUNCTIONS
 function str = get_struct(var)
-
+    % Get struct
     try
         str = var{1, 1};
     catch

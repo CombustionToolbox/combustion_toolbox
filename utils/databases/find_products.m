a324563e8873359ce52a0c9e629121a8ecd3bdcf
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-03-18 13:11:32 +0100

    Update: comments

--- utils/databases/find_products.m
+++ utils/databases/find_products.m
@@ -14,20 +14,25 @@ function [LS, ind_elements_DB] = find_products(self, species, varargin)
     %
     %     * LS (cell): List of products
     %     * ind_elements_DB (float): Matrix NS_DB x MAX_ELEMENTS with element indeces of the species contained in the database
+    %     * FLAG_BURCAT (bool): Flag indicating to look for species also in Burcat's database
+    %     * FLAG_ION (bool): Flag indicating to include ionized species
     %
     % Examples:
     %     [LS, ind_elements_DB] = find_products(self, {'O2', 'N', 'eminus'})
+    %
     %     [LS, ind_elements_DB] = find_products(self, {'O2', 'CO', 'N'}, DB, 'Flag', true)
+    %
     %     [LS, ind_elements_DB] = find_products(self, {'O2', 'CO', 'N'}, DB, 'Flag', true, 'ind', ind_elements_DB)
     
     % Definitions
     MAX_ELEMENTS = 5;
     FLAG_BURCAT = self.S.FLAG_BURCAT; % Flag indicating to look for species also in Burcat's database
+    FLAG_ION = self.S.FLAG_ION; % Flag indicating to consider ionized species
     [elements, ~] = set_elements(); % Elements list
     
     % Initialization
-    FLAG_IND = false;    % Flag indicating that ind_elements_DB is an input
-    LS = [];             % Initialize list of products
+    FLAG_IND = false; % Flag indicating that ind_elements_DB is an input
+    LS = [];          % Initialize list of products
     
     % Unpack
     for i = 1:2:nargin-2
@@ -37,10 +42,17 @@ function [LS, ind_elements_DB] = find_products(self, species, varargin)
                 FLAG_IND = true;
             case {'flag', 'flag_burcat'}
                 FLAG_BURCAT = varargin{i + 1};
+            case {'flag_ion', 'flag_ions'}
+                FLAG_ION = varargin{i + 1};
         end
         
     end
-
+    
+    % If FLAG_ION == true, then find ionized species
+    if FLAG_ION
+        species{end + 1} = 'eminus';
+    end
+    
     % If FLAG_BURCAT == true, then remove the species from Third millenium database
     if ~FLAG_BURCAT
         self.S.LS_DB = find_species_LS(self.S.LS_DB, {}, 'any', {'_M'}, 'all');

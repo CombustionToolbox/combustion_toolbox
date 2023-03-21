fa1a2c5f902a7ecaa63adebc64b1f301919e117b
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-03-21 13:51:44 +0100

    Update: comments
    Former-commit-id: da0426d5f46e390b30369c7b3eab737bb0b0785c
    Former-commit-id: 09330279bd7f065958b96b86767f272e848e8014

--- Combustion_Toolbox_setPath.m
+++ Combustion_Toolbox_setPath.m
@@ -2,29 +2,30 @@ function Combustion_Toolbox_setPath()
     % Set directories to MATLAB search path
     
     % Get current path
-    executableFolder = GetExecutableFolder();
+    executable_folder = get_executable_folder();
     % Generate a path that includes myfolder and all folders below it
-    p = genpath(executableFolder);
+    p = genpath(executable_folder);
     % Add the folder and its subfolders to the search path
     addpath(p);
 end
 
 % SUB-PASS FUNCTIONS
-function [executableFolder] = GetExecutableFolder()
+function [executable_folder] = get_executable_folder()
     % Returns the folder where the compiled executable actually resides
     try
         if isdeployed
             % User is running an executable in standalone mode.
             [status, result] = system('set PATH');
-            executableFolder = char(regexpi(result, 'Path=(.*?);', 'tokens', 'once'));
-            % fprintf(1, '\nIn function GetExecutableFolder(), currentWorkingDirectory = %s\n', executableFolder);
+            executable_folder = char(regexpi(result, 'Path=(.*?);', 'tokens', 'once'));
+            % fprintf(1, '\nIn function get_executable_folder(), currentWorkingDirectory = %s\n', executable_folder);
         else
             % User is running an m-file from the MATLAB integrated development environment (regular MATLAB).
-            executableFolder = pwd;
+            executable_folder = pwd;
         end
+
     catch ME
         errorMessage = sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
             ME.stack(1).name, ME.stack(1).line, ME.message);
-        uiwait(warndlg(errorMessage));
     end
+    
 end

52b8559bb047cce9d19282d22b6940a5907c8e50
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-03-26 17:31:04 +0200

    Update: reorganize routine

--- utils/export/export_results.m
+++ utils/export/export_results.m
@@ -1,19 +1,29 @@
 function export_results(self)
     % Export results of reactants (mix1) and products (mix2) into a .xls
     % file
-    if self.Misc.export_results.value
-        data_mix1 = FormattedOutput_test([], self.PD.phi.value, self.PS.strR, self.S.LS);
-        data_mix2 = FormattedOutput_test([], self.PD.phi.value, self.PS.strP, self.S.LS);
-        
-        switch lower(self.Misc.export_results.format)
-            case {'.xls', 'xls', 'excel'}
-                sheet_name = self.PD.ProblemType;
-                filename = strcat(self.Misc.export_results.filename, '.xls');
-                writecell(data_mix1, filename, 'Sheet', strcat('R-', sheet_name));
-                writecell(data_mix2, filename, 'Sheet', strcat('P-', sheet_name));
-            case {'.mat', 'mat', 'matlab'}
-                filename = strcat(self.Misc.export_results.filename, '.mat');
-                save(filename, 'data_mix1', 'data_mix2');
-        end
+    %
+    % Args:
+    %     self (struct): Data of the mixture, conditions, and databases
+    
+    % Check export_results value
+    if ~self.Misc.export_results.value
+        return
     end
+    
+    % Format data
+    data_mix1 = FormattedOutput_test([], self.PD.phi.value, self.PS.strR, self.S.LS);
+    data_mix2 = FormattedOutput_test([], self.PD.phi.value, self.PS.strP, self.S.LS);
+    
+    % Export data
+    switch lower(self.Misc.export_results.format)
+        case {'.xls', 'xls', 'excel', 'spreadsheet'}
+            sheet_name = self.PD.ProblemType;
+            filename = strcat(self.Misc.export_results.filename, '.xls');
+            writecell(data_mix1, filename, 'Sheet', strcat('R-', sheet_name));
+            writecell(data_mix2, filename, 'Sheet', strcat('P-', sheet_name));
+        case {'.mat', 'mat', 'matlab'}
+            filename = strcat(self.Misc.export_results.filename, '.mat');
+            save(filename, 'data_mix1', 'data_mix2');
+    end
+    
 end

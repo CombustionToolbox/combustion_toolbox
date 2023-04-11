069d87aa5f3b547c4c1c5fb3712a455ca3584efd
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-04-11 16:27:47 +0200

    Update: miscellaneous

--- gui/utils/gui_seeker_value.m
+++ gui/utils/gui_seeker_value.m
@@ -6,18 +6,22 @@ function value = gui_seeker_value(app, event, ListValues)
             value = [];
             return
         end
+
         for i = length(ListValues):-1:1
             seekIndex(i) = startsWith(ListValues{i}, seekValue, 'IgnoreCase', false);
         end
+
         if any(seekIndex)
             value = ListValues(seekIndex); % Value found in ListValues
         else
             value = []; % Value not found in ListValues
         end
+
     catch ME
-      errorMessage = sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
-      ME.stack(1).name, ME.stack(1).line, ME.message);
-      fprintf('%s\n', errorMessage);
-      uiwait(warndlg(errorMessage));
+        errorMessage = sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
+        ME.stack(1).name, ME.stack(1).line, ME.message);
+        fprintf('%s\n', errorMessage);
+        uiwait(warndlg(errorMessage));
     end
+    
 end

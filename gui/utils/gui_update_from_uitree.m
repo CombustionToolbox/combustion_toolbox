2ea4761ed700d590f08ff6571820a388ada069f7
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-04-11 16:35:41 +0200

    Major update: initialization, find_products when LS is not given, GUI, several bugs
    Former-commit-id: e5113b64b1a2ede543b0df2652e27dbd06291e1a
    Former-commit-id: d27324d2ac2dd38ccdd41da9426bfec8137d01c9

--- gui/utils/gui_update_from_uitree.m
+++ gui/utils/gui_update_from_uitree.m
@@ -1,8 +1,16 @@
 function gui_update_from_uitree(app, selectedNodes)
     % Update GUI with the data that correspond with the selected node from
     % the UITree
-    if isempty(selectedNodes.Children)
-        results = selectedNodes.NodeData;
-        gui_write_results(app, results, 1, 'FLAG_REACTANTS', true);
+
+    % Check if the selected node has children (i.e., does not contain results
+    % from a previous calculation)
+    if ~isempty(selectedNodes.Children)
+        return
     end
+
+    % Get the results from the selected node
+    results = selectedNodes.NodeData;
+
+    % Update the GUI with the results
+    gui_write_results(app, results, 1, 'FLAG_REACTANTS', true);
 end

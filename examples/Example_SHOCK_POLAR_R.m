564a84bb2bfa9aff7b77a0263ff26c6702bc8e35
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2023-04-13 18:47:02 +0200

    Update: example SHOCK_POLAR_R
    Former-commit-id: 6dc97a290b623d242f1927cf2519711c363bafc0

--- examples/Example_SHOCK_POLAR_R.m
+++ examples/Example_SHOCK_POLAR_R.m
@@ -1,9 +1,10 @@
 % -------------------------------------------------------------------------
-% EXAMPLE: SHOCK_POLAR_REFLECTED
+% EXAMPLE: SHOCK_POLAR_REFLECTED_THERMO
 %
 % Compute shock polar plots at T1 = 226.65 K and p1 = 0.0117 bar
-% (altitude 30 km), a set of 39 species considered, an initial
-% shock front Mach numbers = 20, and a deflection angle theta = 35;
+% (altitude 30 km), a set of 28 species considered, an initial
+% shock front Mach number = 20, deflection angle theta = 35, and different
+% thermochemical models (chemical equilibrium and frozen chemistry).
 %    
 % LS== {'eminus', 'Ar', 'Arplus', 'N', ...
 %       'Nplus', 'Nminus', 'NO', 'NOplus', 'NO2', ...
@@ -18,7 +19,7 @@
 %          PhD Candidate - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                  
-% Last update Jan 10 2023
+% Last update Apr 13 2023
 % -------------------------------------------------------------------------
     
 LS = {'eminus', 'Ar', 'Arplus', 'N', ...
@@ -28,22 +29,23 @@ LS = {'eminus', 'Ar', 'Arplus', 'N', ...
       'N2O5', 'N3', 'O', 'Oplus', 'Ominus', 'O2', 'O2plus', ...
       'O2minus', 'O3'};
 
-% LS = {'N2', 'O2', 'Ar'};
-
-%% INITIALIZE
+% Initialize
 self = App(LS);
-
+% Miscellaneous
 self.TN.FLAG_FAST = true;
-self.TN.FLAG_TCHEM_FROZEN = false;
-%% INITIAL CONDITIONS
+self.Misc.FLAG_RESULTS = false;
+if FLAG_FROZEN
+    self.Misc.config.linestyle = '--';
+end
+% Thermochemical model
+self.PD.FLAG_FROZEN = false;
+% Initial conditions
 self = set_prop(self, 'TR', 226.65, 'pR', 0.0117);
 self.PD.S_Oxidizer = {'N2', 'O2', 'Ar'};
 self.PD.N_Oxidizer = [78, 21, 1] ./ 21;
-sound_velocity = compute_sound(self.PD.TR.value, self.PD.pR.value, self.PD.S_Oxidizer, self.PD.N_Oxidizer, 'self', self);
-%% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
-Mach_number = 20;
-self = set_prop(self, 'u1', sound_velocity * Mach_number, 'theta', 35);
-%% SOLVE PROBLEM
+% Additional inputs (depends of the problem selected)
+self = set_prop(self, 'M1', 20, 'theta', 35);
+% Solve problem
 self = solve_problem(self, 'SHOCK_POLAR_R');
-%% DISPLAY RESULTS (PLOTS)
+% Display results (plots)
 post_results(self);

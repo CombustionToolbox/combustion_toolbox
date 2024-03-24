bc6b2a4f1e564e3e6c795c1a08305d11850f9023
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-03-24 12:57:42 +0100

    Update: rename properties and methods
    Former-commit-id: a911c6921333fa609ce7ae28f4d7d0afba515699
    Former-commit-id: 3ea42686108340fdde5545e0c6da79c8554af5d4

--- +combustiontoolbox/+databases/@NasaDatabase/NasaDatabase.m
+++ +combustiontoolbox/+databases/@NasaDatabase/NasaDatabase.m
@@ -23,7 +23,37 @@ classdef NasaDatabase < combustiontoolbox.databases.Database
 
     methods (Access = public, Static)
         
-        function [cp, cv, h0, DhT, e0, DeT, s0, g0] = species_thermo_NASA(species, temperature, DB)
+        function [a, b, tRange, tExponents, ctTInt, txFormula, phase] = getCoefficients(species, DB)
+            % Unpack NASA's polynomials coefficients from database
+            %
+            % Args:
+            %     species (char): Chemical species
+            %     DB (struct): Database with custom thermodynamic polynomials functions generated from NASAs 9 polynomials fits
+            %
+            % Returns:
+            %     Tuple containing
+            %
+            %     * a (cell): Temperature coefficients
+            %     * b (cell): Integration constants
+            %     * tRange (cell): Ranges of temperatures [K]
+            %     * tExponents (cell): Exponent coefficients
+            %     * ctTInt (float): Number of intervals of temperatures
+            %     * txFormula (char): Chemical formula
+            %     * phase (float): 0 or 1 indicating gas or condensed phase, respectively
+            %
+            % Example:
+            %     [a, b, tRange, tExponents, ctTInt, txFormula, phase] = getCoefficients('H2O', DB)
+        
+            a = DB.(species).a;
+            b = DB.(species).b;
+            tRange = DB.(species).tRange;
+            tExponents = DB.(species).tExponents;
+            ctTInt = DB.(species).ctTInt;
+            txFormula = DB.(species).txFormula;
+            phase = DB.(species).phase;
+        end
+
+        function [cp, cv, h0, DhT, e0, DeT, s0, g0] = speciesThermo(species, temperature, DB)
             % Compute thermodynamic function using NASA's 9 polynomials
             %
             % Args:
@@ -44,25 +74,25 @@ classdef NasaDatabase < combustiontoolbox.databases.Database
             %     * g0  (float): Gibbs energy in molar basis [kJ/mol]
             %
             % Example:
-            %     [cP, cV, h0, DhT, e0, DeT, s0, g0] = species_thermo_NASA('H2O', 300:100:6000, DB)
+            %     [cp, cv, h0, DhT, e0, DeT, s0, g0] = speciesThermo('H2O', 300:100:6000, DB)
         
             % Definitions
             R0 = 8.31446261815324; % Universal Gas Constant [J/(mol-K)];
-            hf0 = DB.(species).hf; % [J/mol];
+            hf = DB.(species).hf; % [J/mol];
             Tref = 298.15; % [K]
 
             % Unpack NASA's polynomials coefficients
-            [a, b, tRange, tExponents, ctTInt, txFormula, swtCondensed] = unpack_NASA_coefficients(species, DB);
+            [a, b, Trange, Texponents, Tintervals, formula, phase] = getCoefficients(species, DB);
 
             % Get elements
             elements = set_elements();
 
             % Get element matrix of the species
-            element_matrix = set_element_matrix(txFormula, elements);
+            elementMatrix = set_element_matrix(formula, elements);
 
             % Compute change in moles of gases during the formation reaction of a
             % mole of that species starting from the elements in their reference state
-            Delta_n = compute_change_moles_gas_reaction(element_matrix, swtCondensed);
+            Delta_n = compute_change_moles_gas_reaction(elementMatrix, phase);
 
             % Compute specific enthalpy [kJ/mol]
             for i = length(temperature):-1:1
@@ -70,27 +100,27 @@ classdef NasaDatabase < combustiontoolbox.databases.Database
         
                 if DB.(species).ctTInt > 0
                     % Compute interval temperature
-                    tInterval = compute_interval_NASA(species, T, DB, tRange, ctTInt);
+                    tInterval = getIndexTempereratureInterval(species, T, DB, Trange, Tintervals);
 
                     % Compute thermodynamic function
-                    cp(i) = R0 * (sum(a{tInterval} .* T.^tExponents{tInterval}));
-                    h0(i) = R0 * T * (sum(a{tInterval} .* T.^tExponents{tInterval} .* [-1 log(T) 1 1/2 1/3 1/4 1/5 0]) + b{tInterval}(1) / T);
-                    s0(i) = R0 * (sum(a{tInterval} .* T.^tExponents{tInterval} .* [-1/2 -1 log(T) 1 1/2 1/3 1/4 0]) + b{tInterval}(2));
-                    ef0 = hf0 - Delta_n * R0 * Tref;
-                    e0(i) = (ef0 + (h0(i) - hf0) - (1 - swtCondensed) * R0 * (T - Tref));
+                    cp(i) = R0 * (sum(a{tInterval} .* T.^Texponents{tInterval}));
+                    h0(i) = R0 * T * (sum(a{tInterval} .* T.^Texponents{tInterval} .* [-1 log(T) 1 1/2 1/3 1/4 1/5 0]) + b{tInterval}(1) / T);
+                    s0(i) = R0 * (sum(a{tInterval} .* T.^Texponents{tInterval} .* [-1/2 -1 log(T) 1 1/2 1/3 1/4 0]) + b{tInterval}(2));
+                    ef = hf - Delta_n * R0 * Tref;
+                    e0(i) = (ef + (h0(i) - hf) - (1 - phase) * R0 * (T - Tref));
                     cv(i) = cp(i) - R0;
-                    DhT(i) = h0(i) - hf0;
-                    DeT(i) = e0(i) - ef0;
+                    DhT(i) = h0(i) - hf;
+                    DeT(i) = e0(i) - ef;
                     % If the species is only a reactant determine it's reference temperature
                     % Tref. For noncryogenic reactants, assigned enthalpies are given at 298.15 K.
                     % For cryogenic liquids, assigned enthalpies are given at their boiling points
                     % instead of 298.15 K
                 else
-                    Tref = tRange(1);
+                    Tref = Trange(1);
                     cp = 0;
                     cv = 0;
-                    h0(i) = hf0;
-                    e0(i) = hf0 - Delta_n * R0 * Tref;
+                    h0(i) = hf;
+                    e0(i) = hf - Delta_n * R0 * Tref;
                     g0(i) = DB.(species).Hf0;
                     s0(i) = 0;
                     DhT(i) = 0;
@@ -107,35 +137,43 @@ classdef NasaDatabase < combustiontoolbox.databases.Database
             g0 = g0 * 1e-3; % [kJ/mol]
             s0 = s0 * 1e-3; % [kJ/(mol-K)]
         end
-
-        function [a, b, tRange, tExponents, ctTInt, txFormula, phase] = unpack_NASA_coefficients(species, DB)
-            % Unpack NASA's polynomials coefficients from database
+        
+        function Tinterval = getIndexTempereratureInterval(species, T, DB, Trange, Tintervals)
+            % Compute interval NASA polynomials
             %
             % Args:
-            %     species (char): Chemical species
-            %     DB (struct): Database with custom thermodynamic polynomials functions generated from NASAs 9 polynomials fits
+            %     species (char) : Chemical species
+            %     T (float): Temperature [K]
+            %     DB (struct) : Database with custom thermodynamic polynomials functions generated from NASAs 9 polynomials fits
+            %     Trange (cell): Ranges of temperatures [K]
+            %     Tintervals (float): Number of intervals of temperatures
             %
             % Returns:
-            %     Tuple containing
-            %
-            %     * a (cell): Temperature coefficients
-            %     * b (cell): Integration constants
-            %     * tRange (cell): Ranges of temperatures [K]
-            %     * tExponents (cell): Exponent coefficients
-            %     * ctTInt (float): Number of intervals of temperatures
-            %     * txFormula (char): Chemical formula
-            %     * phase (float): 0 or 1 indicating gas or condensed phase, respectively
-            %
-            % Example:
-            %     [a, b, tRange, tExponents, ctTInt, txFormula, phase] = unpack_NASA_coefficients('H2O', DB)
+            %     Tinterval (float): Index of the interval of temperatures
         
-            a = DB.(species).a;
-            b = DB.(species).b;
-            tRange = DB.(species).tRange;
-            tExponents = DB.(species).tExponents;
-            ctTInt = DB.(species).ctTInt;
-            txFormula = DB.(species).txFormula;
-            phase = DB.(species).phase;
+            Tinterval = [];
+        
+            if DB.(species).Tintervals == 0
+                return
+            end
+        
+            for j = 1:Tintervals
+                if (T >= Trange{j}(1)) && (T <= Trange{j}(2))
+                    Tinterval = j;
+                end
+        
+            end
+        
+            if ~isempty(Tinterval)
+                return
+            end
+        
+            if T > Trange{j}(2)
+                Tinterval = Tintervals;
+            elseif T < Trange{j}(2)
+                Tinterval = 1;
+            end
+        
         end
 
     end

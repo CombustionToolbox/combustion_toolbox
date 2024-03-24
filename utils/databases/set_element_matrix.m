d5b67abec4cfca8fde824dd69944f6ae7267c81d
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-03-24 10:53:48 +0100

    Update: rename variables
    Former-commit-id: 99463adebffe1fd50b2b6dcb53eb25a918b9ed7a
    Former-commit-id: bf349c42ac280b70aee2e2cbc818b5c58f33a176

--- utils/databases/set_element_matrix.m
+++ utils/databases/set_element_matrix.m
@@ -1,16 +1,16 @@
-function element_matrix = set_element_matrix(txFormula, elements)
+function elementMatrix = set_element_matrix(formula, elements)
     % Compute element matrix of the given species
     %
     % Args:
-    %     txFormula (str): Chemical formula
+    %     formula (char): Chemical formula
     %
     % Returns:
-    %     element_matrix(float): Element matrix
+    %     elementMatrix(float): Element matrix
     %
     % Example:
     %     For CO2
     %
-    %     element_matrix = [7, 9; 1, 2]
+    %     elementMatrix = [7, 9; 1, 2]
     %
     %     That is, the species contains 1 atom of element 7 (C) and
     %     2 atoms of element 9 (O)
@@ -19,12 +19,13 @@ function element_matrix = set_element_matrix(txFormula, elements)
     N = 40;
     NE = 5;
     step = 8;
+    
     % Fill element matrix
     for i = 5:-1:1
         end0 = N - step * (NE - i);
         start0 = end0 - 5;
 
-        element_i = txFormula(start0 - 2:end0 - 6);
+        element_i = formula(start0 - 2:end0 - 6);
 
         if strcmp(element_i, '  ')
         else
@@ -33,8 +34,8 @@ function element_matrix = set_element_matrix(txFormula, elements)
                 element_i = element_i(1);
             end
 
-            element_matrix(1, i) = find(strcmpi(elements, element_i));
-            element_matrix(2, i) = sscanf(txFormula(start0:end0), '%f');
+            elementMatrix(1, i) = find(strcmpi(elements, element_i));
+            elementMatrix(2, i) = sscanf(formula(start0:end0), '%f');
         end
 
     end

d8d698a3c4dcee7b40fb8256a4cc2a7f3c14140e
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-03-24 10:57:34 +0100

    Update: include more properties
    Former-commit-id: f9a6b66aef3cf62f560180accd55b17b96a727eb
    Former-commit-id: ace7453786428ffb237d418903aa89d4185e97ef

--- +combustiontoolbox/+core/@Species/Species.m
+++ +combustiontoolbox/+core/@Species/Species.m
@@ -3,22 +3,25 @@ classdef Species
     properties
         name          % Name chemical species
         fullname      % Fullname chemical species
+        refCode       % Reference date code
         comments      % Additional comments from database
         formula       % Chemical formula
-        W             % Weight mass
-        hf            % Enthalpy of formation
-        ef            % Internal energy of formation
+        W             % Molecular weight [g/mol]
+        hf            % Enthalpy of formation at Tref from its reference species in their standard state [J/mol]
+        hftoh0        % Enthalpy of formation at Tref relative to molar enthalpy at 0 K for standard state [J/mol]
+        ef            % Internal energy of formation [J/mol]
         phase         % Phase
-        T             % Temperature
-        Tintervals    % Number temperature intervals 
-        Trange        % Polynomials temperature intervals
+        T             % Temperature [K]
+        Tref          % Temperature reference [K]
+        Trange        % Temperature range intervals
+        Tintervals    % Number of temperature intervals
         Texponents    % Exponents polynomials
         a             % Coefficients a polynomials
         b             % Coefficients b polynomials
         cpcurve       % Gridded interpolant object with specific heat at constant pressure of the individual species
         h0curve       % Gridded interpolant object with enthalpy of the individual species
-        s0curve       % Gridded interpolanb object with entropy of the individual species
-        g0curve       % Gridded interpolanb object with Gibbs free energy of the individual species
+        s0curve       % Gridded interpolant object with entropy of the individual species
+        g0curve       % Gridded interpolant object with Gibbs free energy of the individual species
         elementMatrix % Element matrix
     end
 

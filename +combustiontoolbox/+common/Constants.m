7c140a8c493aa0258a308e35d4e4976a37cc656c
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-03-29 23:31:20 +0100

    Add: class Constants
    Former-commit-id: 0507e43df139f46ef219cfb68e5ecb40a9e493f8

---
+++ +combustiontoolbox/+common/Constants.m
@@ -0,0 +1,19 @@
+classdef Constants
+    % Initialize struct with constants data
+    %
+    % Attributes:
+    %     release (char): Release of the Combustion Toolbox
+    %     date (char): Date of the release
+    %     R0 (float): Universal gas constant [J/(K mol)]
+    %     G (float): Standard gravity [m/s2]
+    %
+    % Returns:
+    %     self (struct): Struct with constants data
+    
+    properties (Constant)
+        R0      = 8.31446261815324 % Universal gas constant [J/(K mol)]
+        G       = 9.80665          % Standard gravity [m/s2]
+        NA      = 6.0221415e23     % Avogadro's number [molecule/mol]
+    end
+    
+end

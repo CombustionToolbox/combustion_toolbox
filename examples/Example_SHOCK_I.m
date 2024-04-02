1283eb5eb44fb0c51a1fa36ffced189489683b27
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-04-02 19:03:10 +0200

    Update: rewrite example using OOP
    Former-commit-id: 051260706ad02ff29bb2907bd773c92facea4499
    Former-commit-id: 5e9b4d594330a44e334d47761c8722b1949fa83b

--- examples/Example_SHOCK_I.m
+++ examples/Example_SHOCK_I.m
@@ -11,23 +11,43 @@
 % See wiki or list_species() for more predefined sets of species
 %
 % @author: Alberto Cuadra Lara
-%          PhD Candidate - Group Fluid Mechanics
+%          Postdoctoral researcher - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                 
-% Last update July 22 2022
+% Last update April 02 2024
 % -------------------------------------------------------------------------
 
-%% INITIALIZE
-self = App('Air');
-%% INITIAL CONDITIONS
-self = set_prop(self, 'TR', 300, 'pR', 1 * 1.01325);
-self.PD.S_Oxidizer = {'N2', 'O2', 'Ar', 'CO2'};
-self.PD.N_Oxidizer = [78.084, 20.9476, 0.9365, 0.0319] ./ 20.9476;
-sound_velocity = compute_sound(self.PD.TR.value, self.PD.pR.value, self.PD.S_Oxidizer, self.PD.N_Oxidizer, 'self', self);
-%% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
-u1 = logspace(2, 5, 500); u1 = u1(u1 < 20000); u1 = u1(u1 >= 1 * sound_velocity);
-self = set_prop(self, 'u1', u1);
-%% SOLVE PROBLEM
-self = solve_problem(self, 'SHOCK_I');
-%% DISPLAY RESULTS (PLOTS)
-post_results(self);
+% Import packages
+import combustiontoolbox.databases.NasaDatabase
+import combustiontoolbox.core.*
+import combustiontoolbox.shockdetonation.*
+
+% Get Nasa database
+DB = NasaDatabase();
+
+% Define chemical system
+system = ChemicalSystem(DB, 'air');
+
+% Initialize mixture
+mix = Mixture(system);
+
+% Define chemical state
+set(mix, {'N2', 'O2', 'Ar', 'CO2'}, [78.084, 20.9476, 0.9365, 0.0319] / 20.9476);
+
+% Define properties
+mixArray1 = setProperties(mix, 'temperature', 300, 'pressure', 1.01325, 'M1', 1:0.1:40);
+
+% Initialize solver
+solver = ShockSolver('problemType', 'SHOCK_I');
+
+% Solve problem
+[mixArray1, mixArray2] = solver.solveArray(mixArray1);
+
+% Plot Hugoniot curve
+plotFigure('\rho_1 / \rho_2', [mixArray1.rho] ./ [mixArray2.rho], 'p_2 / p_1', [mixArray2.p] ./ [mixArray1.p], 'xScale', 'log', 'yScale', 'log');
+
+% Plot post-shock temperature
+plotFigure('u', [mixArray2.u], 'T', [mixArray2.T]);
+
+% Plot molar fractions
+plotComposition(mixArray2(1), mixArray2, 'u', 'Xi', 'mintol', 1e-14);

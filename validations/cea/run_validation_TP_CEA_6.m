0574c470173b124ca277f1a27b6b0236c3785ccf
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-04 18:29:06 +0200

    Update: include benckmark option
    Former-commit-id: 6c26c484b96c78b0f15772bfb1eed36a1dbd574a
    Former-commit-id: b91bf2016bd2cfc7fe8e8a68bc08eebbd91ee91c

--- validations/cea/run_validation_TP_CEA_6.m
+++ validations/cea/run_validation_TP_CEA_6.m
@@ -1,4 +1,4 @@
-function problems_solved = run_validation_TP_CEA_6
+function mixArray = run_validation_TP_CEA_6(varargin)
     % Run test validation_TP_CEA_6:
     % Contrasted with: NASA's Chemical Equilibrium with Applications software
     % Problem type: Equilibrium composition at defined T and p
@@ -17,36 +17,20 @@ function problems_solved = run_validation_TP_CEA_6
     import combustiontoolbox.databases.NasaDatabase
     import combustiontoolbox.core.*
     import combustiontoolbox.equilibrium.*
+    
+    % Benchmark?
+    FLAG_BENCHMARK = false;
 
     % Definitions
-    listSpecies = {'C','C2','C2H','C10H8_naphthale','C2H2_acetylene',...
-        'C2H2_vinylidene','C2H3_vinyl','C2H4','C2H4O_ethylen_o','C2H5',...
-        'C2H5OH','C2H6','C2N2','C2O','C3','C3H3_1_propynl',...
-        'C3H3_2_propynl','C3H4_allene','C3H4_propyne','C3H5_allyl',...
-        'C3H6O_acetone','C3H6_propylene','C3H8','C3O2','C4',...
-        'C4H2_butadiyne','C4H6_1butyne','C4H6_2butyne','C4H6_butadiene',...
-        'C4H8_1_butene','C4H8_cis2_buten','C4H8_isobutene',...
-        'C4H8_tr2_butene','C4H9_i_butyl','C4H9_n_butyl','C4H9_s_butyl',...
-        'C4H9_t_butyl','C4N2','C5','C6H2','C6H5OH_phenol','C6H5O_phenoxy',...
-        'C6H5_phenyl','C6H6','C7H7_benzyl','C7H8','C8H18_isooctane',...
-        'C8H8_styrene','CH','CH2','CH2CO_ketene','CH2OH','CH3',...
-        'CH3CHO_ethanal','CH3CN','CH3COOH','CH3CO_acetyl','CH3O',...
-        'CH3OCH3','CH3OH','CH4','CN','CO','CO2','COOH','Cbgrb','H','H2',...
-        'H2O','H2ObLb','H2Obcrb','H2O2','HCCO','HCHO_formaldehy','HCN',...
-        'HCO','HCOOH','HNC','HNCO','HNO','HO2','N','N2','N2O','NCO',...
-        'NH','NH2','NH2OH','NH3','NO','NO2','O','O2','OCCN','OH','Si',...
-        'Si2','Si2C','Si3','SiC','SiC2','SiH','SiH2','SiH3',...
-        'SiH4','SiO','SiO2','SiCbbb','SiO2ba_qzb','SiO2bb_qzb','SiO2bb_crtb'};
-    
     displaySpecies = {'H2', 'H', 'CH4', 'C2H2_acetylene', 'SiC2',...
         'Si', 'C', 'C2H', 'C3', 'CO', 'CO2', 'Cbgrb', 'SiCbbb', 'H2O',...
         'SiO2ba_qzb','SiO2bb_qzb','SiO2bb_crtb','H2ObLb','H2Obcrb'};
 
     % Get Nasa database
-    DB = NasaDatabase();
+    DB = NasaDatabase('FLAG_BENCHMARK', FLAG_BENCHMARK);
     
     % Define chemical system
-    system = ChemicalSystem(DB, listSpecies);
+    system = ChemicalSystem(DB);
     
     % Initialize mixture
     mix = Mixture(system);
@@ -58,11 +42,15 @@ function problems_solved = run_validation_TP_CEA_6
     mixArray = setProperties(mix, 'temperature', 200:10:5000, 'pressure', 1 * 1.01325);
     
     % Initialize solver
-    solver = EquilibriumSolver('problemType', 'TP');
+    solver = EquilibriumSolver('problemType', 'TP', 'FLAG_RESULTS', false);
     
     % Solve problem
     solver.solveArray(mixArray);
     
+    if FLAG_BENCHMARK
+        return
+    end
+
     % Load results CEA 
     prefixDataName = 'C6H5OH_phenol_and_Si';
     filename = {strcat(prefixDataName, '_TP1.out')};

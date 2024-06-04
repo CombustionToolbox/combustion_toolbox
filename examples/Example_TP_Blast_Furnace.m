7ac35872f9d8625d7a8bec4c16663d44a500b51e
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-04 13:45:13 +0200

    Update: rewrite example using OOP version
    Former-commit-id: 70aeb542456a10872d2191b6121db8890f883355

--- examples/Example_TP_Blast_Furnace.m
+++ examples/Example_TP_Blast_Furnace.m
@@ -1,45 +1,62 @@
 % -------------------------------------------------------------------------
-% EXAMPLE: TP
+% EXAMPLE: TP_furnance
 %
 % Compute equilibrium composition of a blast furnace at defined temperature
 % 1050 K and pressure 1.01325 bar. A set of 10 gaseous species and 2 
 % condensed species are considered
 %
-% Reference:
-% W.D. Madeley & J.M. Toguri (1973) The application of free energy
-% minimization techniques to determine equilibrium compositions in systems
-% of metallurgical interest, Canadian Metallurgical Quarterly, 12:1, 71-78,
-% DOI: 10.1179/cmq.1973.12.1.71
+% This example is obtained from [1]
+% 
+% [1] W.D. Madeley & J.M. Toguri (1973) The application of free energy
+%     minimization techniques to determine equilibrium compositions in
+%     systems of metallurgical interest, Canadian Metallurgical Quarterly,
+%     12:1, 71-78. DOI: 10.1179/cmq.1973.12.1.71
 %   
 % Species == {'O2', 'N2', 'H2O', 'CH4', 'CO', 'CO2', 'H2', ...
 %             'CHO_M', 'CH2O_M', 'OH', 'Febab', 'CaObcrb'}
-%   
-% See wiki or list_species() for more predefined sets of species
 %
 % @author: Alberto Cuadra Lara
-%          PhD Candidate - Group Fluid Mechanics
+%          Postdoctoral researcher - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
-%                  
-% Last update July 22 2022
+%                 
+% Last update Jun 04 2024
 % -------------------------------------------------------------------------
 
-%% INITIALIZE
-self = App({'O2', 'N2', 'H2O', 'CH4', 'CO', 'CO2', 'H2', 'CHO_M', 'CH2O_M', 'OH', 'Febab', 'CaObcrb'});
-%% INITIAL CONDITIONS
-self = set_prop(self, 'TR', 1050, 'pR', 1 * 1.01325);
-self.PD.S_Fuel = {'O2', 'N2', 'H2O', 'CH4', 'Fe3O4bcrb', 'Febab', 'Cbgrb', 'CaCO3bcrb', 'CaObcrb'};
-self.PD.N_Fuel = [20.46, 187.1, 1.775, 2.2554, 13.1, 3.527, 85.59, 0.1499, 0.6063];
-%% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
-self = set_prop(self, 'pP', self.PD.pR.value, 'TP', 1050); 
-self.TN.tolN = 1e-30;
-%% SOLVE PROBLEM
-self = solve_problem(self, 'TP');
-%% DISPLAY RESULTS (PLOTS)
-post_results(self);
-%% GET RESULTS
-moles_CT = moles(self.PS.strP{1});
-moles_RAND = [1.793e-19; 1.871e2; 2.377e-1; 1.97e-3; 8.143e1; 6.865; 6.641; 9.46e-7; 1.264e-7; 8.411e-11; 4.283e1; 7.562e-1; 0; 0; 0];
-%% DISPLAY TABLE SPECIES AND MOLES
-T = table(self.S.LS', moles_CT, moles_RAND, moles_CT - moles_RAND);
-T.Properties.VariableNames = {'Species', 'Moles', 'Moles RAND', 'Error'}
+% Import packages
+import combustiontoolbox.databases.NasaDatabase
+import combustiontoolbox.core.*
+import combustiontoolbox.equilibrium.*
+import combustiontoolbox.utils.findIndex
+
+% Get Nasa database
+DB = NasaDatabase();
+
+% Define chemical system
+system = ChemicalSystem(DB);
+
+% Initialize mixture
+mix = Mixture(system);
+
+% Define chemical state
+set(mix, {'O2', 'N2', 'H2O', 'CH4', 'Fe3O4bcrb', 'Febab', 'Cbgrb', 'CaCO3bcrb', 'CaObcrb'},...
+    [20.46, 187.1, 1.775, 2.2554, 13.1, 3.527, 85.59, 0.1499, 0.6063]);
+
+% Define properties
+mixArray = setProperties(mix, 'temperature', 1050, 'pressure', 1 * 1.01325);
+
+% Initialize solver
+solver = EquilibriumSolver('problemType', 'TP', 'tolMoles', 1e-25, 'FLAG_RESULTS', true);
+
+% Solve problem
+solver.solveArray(mixArray);
+
+% Print results
+displaySpecies = {'O2', 'N2', 'H2O', 'CH4', 'CO', 'CO2', 'H2', 'OH', 'Febab', 'CaObcrb'};
+indexSpecies = findIndex(system.listSpecies, displaySpecies);
+moles_RAND = [1.793e-19; 1.871e2; 2.377e-1; 1.97e-3; 8.143e1; 6.865; 6.641; 8.414e-11; 4.283e1; 7.562e-1];
+moles_CT = mixArray.Xi(indexSpecies) * mixArray.N;
+
+% Display table with species and molar coposition
+T = table(system.listSpecies(indexSpecies)', moles_CT, moles_RAND, moles_RAND - moles_CT);
+T.Properties.VariableNames = {'Species', 'Moles', 'Moles RAND', 'Error [moles]'}
 

78c838685bbc330eeb3aa2908119bd6bfc8a6061
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-06 19:11:07 +0200

    Update: clearvars to clear cache between calls

--- utils/timeFunction.m
+++ utils/timeFunction.m
@@ -1,25 +1,26 @@
-function [tMean, tArray] = timeFunction(f, nFrec)
+function [tMean, tArray] = timeFunction(f, nFrec, varargin)
     % Function to compute mean evaluation time of a given function f
     %
     % Args:
-    %      * f (function): Function to be evaluated
-    %      * nFrec (float): Number of evaluations
+    %      f (function): Function to be evaluated
+    %      nFrec (float): Number of evaluations
     %
     % Returns:
+    %      Tuple containing
+    %
     %      * tMean (float): Average evaluation time
     %      * tArray (float): Array with the evaluation times
     %
     % Note: The function needs at least one input
     %  
     % Example:
-    %      Tuple containing
-    %
-    %      * [tMean, tArray] = timeFunction(@Example_TP_scoggins2015, 10)
+    %      [tMean, tArray] = timeFunction(@Example_TP_scoggins2015, 10)
 
     for i = nFrec:-1:1
         t = tic;
-        f(1);
+        f(varargin{:});
         tArray(i) = toc(t);
+        clearvars -except f nFrec varargin tArray
     end
     
     tMean = mean(tArray);

1b95477eba3cb718efd60425fb4d7ca5e9eaf9a0
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-07 20:59:11 +0200

    Update: include shockIncidentIdeal method in ShockSolver class and remove old functions
    Former-commit-id: d0946946cf3a3c2ac21b92a28c23c46f0c16437b
    Former-commit-id: acc668d74e73dbd6c6d47c6f2807feba67d3a772

--- +combustiontoolbox/+shockdetonation/@ShockSolver/shockIncidentIdeal.m
+++ +combustiontoolbox/+shockdetonation/@ShockSolver/shockIncidentIdeal.m
@@ -1,8 +1,9 @@
-function [R, P, T, Gammas, M1] = shock_ideal_gas(gamma, M1)
+function [R, P, T, Gammas, M1] = shockIncidentIdeal(obj, gamma, M1)
     % Compute jump conditions assuming a thermochemically frozen gas
     % (calorically perfect gas)
     %
     % Args:
+    %     obj (ShockSolver): ShockSolver object
     %     gamma (float): Adiabatic index [-]
     %     M1 (float): Pre-shock Mach number [-]
     %
@@ -22,17 +23,19 @@ function [R, P, T, Gammas, M1] = shock_ideal_gas(gamma, M1)
     P = (2 * gamma * M1.^2 - (gamma - 1)) ./ (gamma + 1);
     T = (((gamma - 1) .* M1.^2 + 2) .* (2 * gamma * M1.^2 - (gamma - 1))) ./ ((gamma + 1).^2 .* M1.^2);
 
-    if length(M1) > 1
-        % Compute Gamma numerically
-        % Gammas = compute_Gammas_frozen(M1, R, P);
-        Gammas = compute_Gammas(u2, rho2, p2);
-        % Remove last point of the dataset to match dimensions with Gammas
-        R = R(1:end-1);
-        P = P(1:end-1);
-        T = T(1:end-1);
-        M1 = M1(1:end-1);
-    else
+    if isscalar(M1)
         Gammas = [];
+        return
     end
+
+    % Compute Gamma numerically
+    % Gammas = obj.computeGammasFrozen(M1, R, P);
+    Gammas = obj.computeGammas(u2, rho2, p2);
+
+    % Remove last point of the dataset to match dimensions with Gammas
+    R = R(1:end-1);
+    P = P(1:end-1);
+    T = T(1:end-1);
+    M1 = M1(1:end-1);
 
 end

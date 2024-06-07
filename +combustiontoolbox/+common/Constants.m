df778c44af78f28d1eb84add6bbb48e6cb5e12a1
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-07 20:13:37 +0200

    Update: comments and add release and date as properties

--- +combustiontoolbox/+common/Constants.m
+++ +combustiontoolbox/+common/Constants.m
@@ -1,5 +1,5 @@
 classdef Constants < handle
-    % Initialize struct with constants data
+    % Class with constants data
     %
     % Attributes:
     %     release (char): Release of the Combustion Toolbox
@@ -7,13 +7,18 @@ classdef Constants < handle
     %     R0 (float): Universal gas constant [J/(K mol)]
     %     G (float): Standard gravity [m/s2]
     %
-    % Returns:
-    %     self (struct): Struct with constants data
+    % Examples:
+    %     * R0 = Constants.R0
+    %     * g = Constants.G
+    %     * release = Constants.release
+
     
     properties (Constant)
         R0      = 8.31446261815324 % Universal gas constant [J/(K mol)]
         G       = 9.80665          % Standard gravity [m/s2]
         NA      = 6.0221415e23     % Avogadro's number [molecule/mol]
+        release = 'v1.1.0'         % Release version
+        date    = '07 Jun 2024'    % Release date
     end
     
 end

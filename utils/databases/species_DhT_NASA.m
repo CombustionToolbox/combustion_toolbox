e12c08e80e387d196e407308b30a96d36f32facc
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-08 16:59:12 +0200

    Update: calculations are carried out using SI units except for the pressure [bar]
    Former-commit-id: 1d19461f3c108491a571182672abbead0e932fdc
    Former-commit-id: edeba948429bfcde70b6bf34824de152c41fb05c

--- utils/databases/species_DhT_NASA.m
+++ utils/databases/species_DhT_NASA.m
@@ -1,5 +1,5 @@
 function DhT = species_DhT_NASA(species, temperature, DB)
-    % Compute thermal enthalpy [kJ/mol] of the species at the given
+    % Compute thermal enthalpy [J/mol] of the species at the given
     % temperature [K] using NASA's 9 polynomials
     %
     % Args:
@@ -8,7 +8,7 @@ function DhT = species_DhT_NASA(species, temperature, DB)
     %     DB (struct): Database with custom thermodynamic polynomials functions generated from NASAs 9 polynomials fits
     %
     % Returns:
-    %     DhT (float): Thermal enthalpy in molar basis [kJ/mol]
+    %     DhT (float): Thermal enthalpy in molar basis [J/mol]
     %
     % Example:
     %     DhT = species_DhT_NASA('H2O', 300:100:6000, DB)

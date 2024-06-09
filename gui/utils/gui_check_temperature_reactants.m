8cba4028160bc2ede910e81b4b0a152d3164d407
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-09 20:56:44 +0200

    Update: rewrite function for OOP version

--- gui/utils/gui_check_temperature_reactants.m
+++ gui/utils/gui_check_temperature_reactants.m
@@ -1,15 +1,22 @@
-function [app, temperature, FLAG_FIXED] = gui_check_temperature_reactants(app, DB, species, temperature, Nspecies)
+function [app, temperature, FLAG_FIXED] = gui_check_temperature_reactants(app, listSpecies, temperature, numSpecies)
     % Check if is a condensed species with a fixed temperature
-    FLAG_FIXED = true;
-    for i = 1:Nspecies
-        if length(DB.(species{i}).T) == 1
-            temperature{i} = DB.(species{i}).T;
-        else
-            FLAG_FIXED = false;
+    FLAG_FIXED = false;
+    
+    
+    for i = 1:numSpecies
+        % Get Species object
+        species = app.database.species.(listSpecies{i});
+        
+        % Check if condensed species can be only evaluated a particular temperature
+        if isscalar(species.T)
+            temperature{i} = app.database.species.(listSpecies{i}).T;
+            FLAG_FIXED = true;
         end
+
     end
 
     if FLAG_FIXED
         app.PR1.Value = sprintf('%.4g', max(cell2mat(temperature))); 
     end
+
 end

c6a0d22c03ad13d0dbda80186473793d199656f3
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-09 20:56:44 +0200

    Update: rewrite function for OOP version
    Former-commit-id: 8cba4028160bc2ede910e81b4b0a152d3164d407

--- gui/utils/gui_update_frozen.m
+++ gui/utils/gui_update_frozen.m
@@ -4,18 +4,12 @@ function gui_update_frozen(app)
     % Args:
     %     app (object): Combustion Toolbox app object
     
-    % Default
-    app.PD.FLAG_FROZEN = false;
-
     % Check if frozen chemistry is selected
     if ~app.FrozenchemistryCheckBox.Value
         gui_ProductsValueChanged(app)
         return
     end
     
-    % Frozen chemistry
-    app.PD.FLAG_FROZEN = true;
-
     if contains(app.ProblemType.Value, 'ROCKET', 'IgnoreCase', true)
         return
     end
--- utils/get_typeSpecies.m
+++ utils/get_typeSpecies.m
@@ -1,14 +1,14 @@
-function typeSpecies = get_typeSpecies(self)
+function typeSpecies = get_typeSpecies(mix)
     % Create cell array with the type of species in the mixture
     %
     % Args:
-    %     self (struct): Data of the mixture, conditions, and databases
+    %     mix (Mixture): Mixture class
     %
     % Returns:
     %     typeSpecies (cell): Cell array with the type of species in the mixture
 
-    typeFuel = create_cell_ntimes('Fuel', length(self.PD.N_Fuel));
-    typeOxidizer = create_cell_ntimes('Oxidizer', length(self.PD.N_Oxidizer));
-    typeInert = create_cell_ntimes('Inert', length(self.PD.N_Inert));
+    typeFuel = create_cell_ntimes('Fuel', length(mix.listSpeciesFuel));
+    typeOxidizer = create_cell_ntimes('Oxidizer', length(mix.listSpeciesOxidizer));
+    typeInert = create_cell_ntimes('Inert', length(mix.listSpeciesInert));
     typeSpecies = [typeInert, typeOxidizer, typeFuel];
 end

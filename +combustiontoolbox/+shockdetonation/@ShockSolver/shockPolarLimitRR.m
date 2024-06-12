a22e7ab5a074170c5a31d0b9ad5304afa8345884
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-06-12 12:16:35 +0200

    Update: add solution polar diagrams in the limit of regular reflections

--- +combustiontoolbox/+shockdetonation/@ShockSolver/shockPolarLimitRR.m
+++ +combustiontoolbox/+shockdetonation/@ShockSolver/shockPolarLimitRR.m
@@ -17,26 +17,29 @@ function [mix1, mix2, mix2_1, mix3] = shockPolarLimitRR(obj, mix1, u1)
     %
     % Example:
     %    [mix1, mix2, mix2_1, mix3] = shockPolarLimitRR(ShockSolver(), mix1, u1)
+    
+    % Miscellaneous
+    temp = obj.FLAG_RESULTS;
+    obj.FLAG_RESULTS = false;
 
     % Compute first polar curve
     [mix1, mix2] = shockPolar(obj, mix1, u1);
 
     % Initialization
-    temp_mix1 = mix1.copy;
     mix2_1 = [];
     theta0 = mix2.thetaMax / 2; % [deg]
     fprime = -2;
 
     % Miscellaneous
-    it = 0; itMax = obj.it_limitRR; STOP = 1; tol_limitRR = obj.tol_limitRR;
+    it = 0; itMax = obj.itLimitRR; STOP = 1; tolLimitRR = obj.tolLimitRR;
 
     % Loop to find regular reflection limit, which is reached when
     % theta_min_polar2 == 0
-    while STOP > tol_limitRR && it < itMax
+    while STOP > tolLimitRR && it < itMax
         % Update iteration
         it = it + 1;
         % Compute oblique shock for the given deflection angle
-        [~, mix2_1] = shockObliqueTheta(obj, temp_mix1, u1, theta0, mix2_1);
+        [~, mix2_1] = shockObliqueTheta(obj, mix1, u1, theta0, mix2_1);
         % Compute polar curves departing from the post-shock state obtained
         [~, mix3] = shockPolar(obj, mix2_1, mix2_1.u);
         % Compute absolute error
@@ -56,4 +59,6 @@ function [mix1, mix2, mix2_1, mix3] = shockPolarLimitRR(obj, mix1, u1)
         theta0 = theta;
     end
 
+    % Miscellaneous
+    obj.FLAG_RESULTS = temp;
 end

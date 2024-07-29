ff3274a14bcafaf343e4bbb60ba55a79ac2b8112
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-07-29 10:00:13 +0200

    Update: rewrite examples using OOP version
    Former-commit-id: 1bb54d074ce6ce475c267559b50832e8b6dadb36
    Former-commit-id: 1fb0356e1a8e7b62657b10cf84dfc8fbec5ca88e

--- examples/temp/Example_ROCKET_IAC.m
+++ examples/temp/Example_ROCKET_IAC.m
@@ -3,7 +3,7 @@
 %
 % Compute rocket propellant performance considering an Infinite-Area-Chamber
 % for lean to rich LH2-LOX mixtures at 101.325 bar, a set of 11 species
-% considered, a set of equivalence ratios phi contained in (2, 5) [-], and
+% considered, a set of equivalence ratios phi contained in (1, 5) [-], and
 % an exit area ratio A_exit/A_throat = 3 
 %   
 % HYDROGEN_L == {'H','H2O','OH','H2','O','O3','O2','HO2','H2O2',...
@@ -16,19 +16,33 @@
 %          Postdoctoral researcher - Group Fluid Mechanics
 %          Universidad Carlos III de Madrid
 %                 
-% Last update Jul 30 2022
+% Last update Jul 29 2024
 % -------------------------------------------------------------------------
 
-%% INITIALIZE
-self = App('HYDROGEN_L');
-%% INITIAL CONDITIONS
-self = set_prop(self, 'TR', 90, 'pR', 100 * 1.01325, 'phi', 1:0.05:5);
-self.PD.S_Fuel     = {'H2bLb'};
-self.PD.S_Oxidizer = {'O2bLb'};
-self.PD.FLAG_IAC = true;
-%% ADDITIONAL INPUTS (DEPENDS OF THE PROBLEM SELECTED)
-self = set_prop(self, 'Aratio', 3);
-%% SOLVE PROBLEM
-self = solve_problem(self, 'ROCKET');
-%% DISPLAY RESULTS (PLOTS)
-post_results(self);
+% Import packages
+import combustiontoolbox.databases.NasaDatabase
+import combustiontoolbox.core.*
+import combustiontoolbox.rocket.*
+import combustiontoolbox.utils.display.*
+
+% Get Nasa database
+DB = NasaDatabase();
+
+% Define chemical system
+system = ChemicalSystem(DB, 'hydrogen_L');
+
+% Initialize mixture
+mix = Mixture(system);
+
+% Define chemical state
+set(mix, {'H2bLb'}, 'fuel', 1);
+set(mix, {'O2bLb'}, 'oxidizer', 1);
+
+% Define properties
+mixArray1 = setProperties(mix, 'temperature', 90, 'pressure', 100 * 1.01325, 'equivalenceRatio', 1:0.05:5, 'areaRatio', 3);
+
+% Initialize solver
+solver = RocketSolver('problemType', 'ROCKET_IAC');
+
+% Solve problem
+[mixArray1, mixArray2] = solver.solveArray(mixArray1);

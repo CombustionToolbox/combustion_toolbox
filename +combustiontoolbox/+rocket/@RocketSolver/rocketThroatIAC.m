a9e89fd26704119dc34876ff7518e61a7ca6ef54
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-07-30 12:00:42 +0200

    Update: rename RocketSolver methods
    Former-commit-id: e1636559f08746983bd98042480c679eaa19be3f
    Former-commit-id: 2d73f1ef8f57b3412bea2815125270adc2e09c2a

--- +combustiontoolbox/+rocket/@RocketSolver/rocketThroatIAC.m
+++ +combustiontoolbox/+rocket/@RocketSolver/rocketThroatIAC.m
@@ -1,4 +1,4 @@
-function mix3 = computeThroatIAC(obj, mix2, mix3_guess)
+function mix3 = rocketThroatIAC(obj, mix2, mix3_guess)
     % Compute thermochemical composition for the Infinite-Area-Chamber (IAC) model
     %
     % This method is based on the method outlined in Gordon, S., & McBride,
@@ -13,8 +13,8 @@ function mix3 = computeThroatIAC(obj, mix2, mix3_guess)
     %     mix3 (Mixture): Properties of the mixture at the throat
     %
     % Examples:
-    %     * mix3 = computeThroatIAC(RocketSolver(), mix2, mix3_guess)
-    %     * mix3 = computeThroatIAC(RocketSolver(), mix2, [])
+    %     * mix3 = rocketThroatIAC(RocketSolver(), mix2, mix3_guess)
+    %     * mix3 = rocketThroatIAC(RocketSolver(), mix2, [])
 
     % Definitions
     obj.equilibriumSolver.problemType = 'SP';
@@ -23,7 +23,7 @@ function mix3 = computeThroatIAC(obj, mix2, mix3_guess)
     mix3 = copy(mix2);
 
     % Compute pressure guess [bar] for Infinite-Area-Chamber (IAC)
-    mix3.p = guess_pressure_IAC_model(mix2);
+    mix3.p = obj.rocketGuessThroatIAC(mix2);
 
     % Initialization
     STOP = 1; it = 0;
@@ -37,14 +37,14 @@ function mix3 = computeThroatIAC(obj, mix2, mix3_guess)
         solve(obj.equilibriumSolver, mix3, mix3_guess);
 
         % Compute velocity at the throat
-        mix3.u = compute_velocity(mix2, mix3);
+        mix3.u = computeVelocity(mix2, mix3);
         mix3.mach = mix3.u / mix3.sound;
 
         % Compute new estimate
-        mix3.p = compute_pressure(mix3);
+        mix3.p = computePressure(mix3);
 
         % Compute STOP criteria
-        STOP = compute_STOP(mix3);
+        STOP = computeSTOP(mix3);
 
         % Update guess
         mix3_guess = mix3;
@@ -56,35 +56,39 @@ function mix3 = computeThroatIAC(obj, mix2, mix3_guess)
     mix3.problemType = mix2.problemType;
 end
 
-function pressure = compute_pressure(mix3)
-    % Compute pressure [bar]
+function pressure = computePressure(mix3)
+    % Compute the new pressure estimate [bar]
+    %
+    % Args:
+    %     mix3 (Mixture): Properties of the mixture at the throat
+    %
+    % Returns:
+    %     pressure (float): Updated pressure estimate [bar]
+    
     pressure = mix3.p * (1 + mix3.gamma_s * mix3.mach^2) / (1 + mix3.gamma_s); % [bar]
 end
 
-function velocity = compute_velocity(mix2, mix3)
-    % Compute velocity
+function velocity = computeVelocity(mix2, mix3)
+    % Compute velocity at the throat [m/s]
+    %
+    % Args:
+    %     mix2 (Mixture): Properties of the mixture at the chamber outlet
+    %     mix3 (Mixture): Properties of the mixture at the throat
+    %
+    % Returns:
+    %     velocity (float): Computed velocity at the throat [m/s]
+
     velocity = sqrt(2 * (enthalpy_mass(mix2) - enthalpy_mass(mix3)) * 1e3); % [m/s]
 end
 
-function STOP = compute_STOP(mix3)
-    % Compute STOP criteria
-    STOP = abs((mix3.u^2 - mix3.sound^2) / mix3.u^2); % [-]
-end
-
-function pressure = guess_pressure_IAC_model(mix)
-    % Compute pressure guess [bar] at the throat considering an Infinite-Area-Chamber (IAC)
-    %
-    % This method is based on the method outlined in Gordon, S., & McBride,
-    % B. J. (1994). NASA reference publication, 1311.
+function STOP = computeSTOP(mix3)
+    % Compute the STOP criteria for convergence
     %
     % Args:
-    %     mix (Mixture): Properties of the mixture
+    %     mix3 (Mixture): Properties of the mixture at the throat
     %
     % Returns:
-    %     pressure (float): Pressure at the throat [bar]
-    %
-    % Example:
-    %     pressure = guess_pressure_IAC_model(mix)
+    %     STOP (float): Convergence criteria value [-]
 
-    pressure = mix.p / ((mix.gamma_s + 1) / 2)^(mix.gamma_s / (mix.gamma_s - 1));
+    STOP = abs((mix3.u^2 - mix3.sound^2) / mix3.u^2); % [-]
 end

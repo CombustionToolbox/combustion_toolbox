1ec8fa980f541a6ecca3fc86998d0b18e9264c50
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-07-31 17:17:20 +0200

    Add: run_validation_TEA
    Former-commit-id: 8a9f1345473ed5d7f220990115ff0c9cf085ba50

---
+++ validations/run_validation_TEA.m
@@ -0,0 +1,11 @@
+function run_validation_TEA
+    % Function that runs several tests to evaluate the quality and
+    % performance of the code compare with the Thermochemical Equilibrium 
+    % Abundances (TEA) code
+
+    % TP:
+    run_validation_TP_TEA_1;
+    run_validation_TP_TEA_2;
+    run_validation_TP_TEA_3;
+    run_validation_TP_TEA_4;
+end

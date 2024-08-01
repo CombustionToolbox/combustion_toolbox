0e2cb9c06e9cc2462ed0b0450884375d5b19234c
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-01 09:24:32 +0200

    Update: change call to openWebsite function

--- utils/website_CT.m
+++ utils/website_CT.m
@@ -1,5 +1,13 @@
 function website_CT()
     % Open Combustion Toolbox's website in default web browser
+    
+    % Import packages
+    import combustiontoolbox.common.Constants
+    import combustiontoolbox.utils.openWebsite
 
-    system('start https://combustion-toolbox-website.readthedocs.io/');
+    % Definitions
+    url = Constants.url.website;
+    
+    % Open website
+    openWebsite(url)
 end

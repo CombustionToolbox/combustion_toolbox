ab6abd0f088539b4934cb4efd523f50041332c1b
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-01 13:23:33 +0200

    Update: include plotProperties and plotPropertiesBasis properties in PlotConfig class
    Former-commit-id: fc0f98d5bc2e528ca3b599c496cda28c84da90bc

--- +combustiontoolbox/+utils/+display/@PlotConfig/PlotConfig.m
+++ +combustiontoolbox/+utils/+display/@PlotConfig/PlotConfig.m
@@ -39,44 +39,57 @@ classdef PlotConfig < handle
     %     id_polar3 (float): Axes id for velocity polar diagrams
     
     properties
-        position;                             % Default figure position [pixels]
-        innerposition = [0.05 0.05 0.9 0.9];  % Set figure inner position [normalized]
-        outerposition = [0.05 0.05 0.9 0.9];  % Set figure outer position [normalized]
-        linestyle = '-';                      % Set line style for plots
-        symbolstyle = 'o';                    % Set symbol style for plots
-        linewidth = 1.8;                      % Set line width for plots
-        fontsize = 22;                        % Set fontsize
-        colorpalette = 'Seaborn';             % Set Color palette (see brewermap function for more options)
-        colorpaletteLenght = 11;              % Set Maximum number of colors to use in the color palette
-        box = 'off';                          % Display axes outline
-        grid = 'off';                         % Display or hide axes grid lines
-        hold = 'on';                          % Retain current plot when adding new plots
-        axis_x = 'tight';                     % Set x-axis limits
-        axis_y = 'auto';                      % Set y-axis limits
-        xscale = 'linear';                    % Set x-axis scale (linear or log)
-        yscale = 'linear';                    % Set y-axis scale (linear or log)
-        xdir = 'normal';                      % Set x-axis direction (normal or reverse)
-        ydir = 'normal';                      % Set y-axis direction (normal or reverse)
-        title = [];                           % Set title
-        label_type = 'short';                 % Set label with variable (short), name (medium), or name and variable (long)
-        labelx = [];                          % Set x label
-        labely = [];                          % Set y label
-        legend_name = [];                     % Set legend labels
-        legend_location = 'northeastoutside'; % Set legend location
-        colorline = [44, 137, 160]/255;       % Default colorline
+        position                              % Default figure position [pixels]
+        innerposition = [0.05 0.05 0.9 0.9]   % Set figure inner position [normalized]
+        outerposition = [0.05 0.05 0.9 0.9]   % Set figure outer position [normalized]
+        linestyle = '-'                       % Set line style for plots
+        symbolstyle = 'o'                     % Set symbol style for plots
+        linewidth = 1.8                       % Set line width for plots
+        fontsize = 22                         % Set fontsize
+        colorpalette = 'Seaborn'              % Set Color palette (see brewermap function for more options)
+        colorpaletteLenght = 11               % Set Maximum number of colors to use in the color palette
+        box = 'off'                           % Display axes outline
+        grid = 'off'                          % Display or hide axes grid lines
+        hold = 'on'                           % Retain current plot when adding new plots
+        axis_x = 'tight'                      % Set x-axis limits
+        axis_y = 'auto'                       % Set y-axis limits
+        xscale = 'linear'                     % Set x-axis scale (linear or log)
+        yscale = 'linear'                     % Set y-axis scale (linear or log)
+        xdir = 'normal'                       % Set x-axis direction (normal or reverse)
+        ydir = 'normal'                       % Set y-axis direction (normal or reverse)
+        title = []                            % Set title
+        label_type = 'short'                  % Set label with variable (short), name (medium), or name and variable (long)
+        labelx = []                           % Set x label
+        labely = []                           % Set y label
+        legend_name = []                      % Set legend labels
+        legend_location = 'northeastoutside'  % Set legend location
+        colorline = [44, 137, 160]/255        % Default colorline
         colorlines = [135, 205, 222;...       % Default colorlines
                       95, 188, 211;...        % Default colorlines
                       44, 137, 160;...        % Default colorlines
-                      22,  68,  80]/255;      % Default colorlines
-        blue = [0.3725, 0.7373, 0.8275];      % Default colorline
-        gray = [0.50, 0.50, 0.50];            % Default colorline
-        red = [0.64,0.08,0.18];               % Default colorline
-        orange = [212, 85, 0]/255;            % Default colorline
-        brown = [200, 190, 183]/255;          % Default colorline
-        brown2 = [72, 55, 55]/255;            % Default colorline
-        id_polar1 = 1001;                     % Axes id for pressure-deflection polar diagrams
-        id_polar2 = 1002;                     % Axes id for wave-deflection polar diagrams
-        id_polar3 = 1003;                     % Axes id for velocity polar diagrams
+                      22,  68,  80]/255       % Default colorlines
+        blue = [0.3725, 0.7373, 0.8275]       % Default colorline
+        gray = [0.50, 0.50, 0.50]             % Default colorline
+        red = [0.64,0.08,0.18]                % Default colorline
+        orange = [212, 85, 0]/255             % Default colorline
+        brown = [200, 190, 183]/255           % Default colorline
+        brown2 = [72, 55, 55]/255             % Default colorline
+        id_polar1 = 1001                      % Axes id for pressure-deflection polar diagrams
+        id_polar2 = 1002                      % Axes id for wave-deflection polar diagrams
+        id_polar3 = 1003                      % Axes id for velocity polar diagrams
+        mintolDisplay = 1e-14
+        plotProperties = {'T', 'rho', 'h', 'e', 'g', 'cp', 's', 'gamma_s', 'sound'}
+        plotPropertiesBasis = {[], [], 'mi', 'mi', 'mi', 'mi', 'mi', [], []}
+    end
+
+    properties (Dependent)
+        numPlotProperties
+    end
+
+    methods
+        function value = get.numPlotProperties(obj)
+            value = length(obj.plotProperties);
+        end
     end
 
     methods (Access = public)

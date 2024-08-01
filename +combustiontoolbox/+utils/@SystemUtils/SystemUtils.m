80bbdf31ed54167be0d43ab1bc1d844974e6a642
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-01 11:29:52 +0200

    Update: add websiteCT method
    Former-commit-id: f84c3b28ffff4c899a40ff7282f6bb6455a0847a
    Former-commit-id: bfb52e3fb1acbcaa608729ff5fa630b300388705

--- +combustiontoolbox/+utils/@SystemUtils/SystemUtils.m
+++ +combustiontoolbox/+utils/@SystemUtils/SystemUtils.m
@@ -58,6 +58,19 @@ classdef SystemUtils < handle
         
         end
 
+        function websiteCT()
+            % Open Combustion Toolbox's website in default web browser
+            
+            % Import packages
+            import combustiontoolbox.utils.SystemUtils
+        
+            % Definitions
+            url = SystemUtils.url.website;
+            
+            % Open website
+            SystemUtils.openWebsite(url)
+        end
+
     end
 
 end

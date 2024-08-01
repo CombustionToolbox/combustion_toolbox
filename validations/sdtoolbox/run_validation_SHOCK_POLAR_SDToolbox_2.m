1a28136509f6deff91a91a243709338998527fa6
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-01 16:10:45 +0200

    Add: validations shock polar diagrams with SD-Toolbox

--- validations/sdtoolbox/run_validation_SHOCK_POLAR_SDToolbox_2.m
+++ validations/sdtoolbox/run_validation_SHOCK_POLAR_SDToolbox_2.m
@@ -1,4 +1,4 @@
-function problems_solved = run_validation_SHOCK_POLAR_SDToolbox_2
+function run_validation_SHOCK_POLAR_SDToolbox_2
     % Run test validation_SHOCK_POLAR_SDToolbox_2
     % Contrasted with: Caltech's SD Toolbox and CANTERA
     % Problem type: Shock polar
@@ -6,41 +6,64 @@ function problems_solved = run_validation_SHOCK_POLAR_SDToolbox_2
     % Pressure    [bar] = 1.01325
     % Initial mixture: AIR (79% N2 + 21% O2)
     % List of species considered: list_species('AIR_IONS')
-    
-    % Inputs
 
-%     Oxidizer = {'N2', 'O2', 'Ar', 'CO2'};
-%     moles = [3.7276, 1.0000, 0.0447, 0.0015];
+    % Import packages
+    import combustiontoolbox.databases.NasaDatabase
+    import combustiontoolbox.core.*
+    import combustiontoolbox.shockdetonation.*
+    import combustiontoolbox.utils.display.*
 
-    Oxidizer = {'N2', 'O2', 'Ar'};
-    moles = [78, 21, 1]/21;
-%     LS = 'AIR_IONS';
+    % Benchmark?
+    FLAG_BENCHMARK = false;
 
-    LS = {'O2', 'O', 'N2', 'N', 'NO', 'eminus', 'Nplus', 'NOplus',...
+    % Definitions
+    prefixDataName = 'shock_polar_equilwithions_ar_SDToolbox';
+
+    for i = 6:-1:1
+        filename{i} = sprintf('%s%d.out', prefixDataName, i);
+    end
+
+    listSpecies = {'O2', 'O', 'N2', 'N', 'NO', 'eminus', 'Nplus', 'NOplus',...
           'N2plus', 'Oplus', 'O2plus', 'Ar'};
+    M1 = [2, 3, 5, 14];
+    numPointsPolar = 300;
+    filename = 'shock_polar_equilwithions_SDToolbox';
     
-    u1 = 3.472107491008314e+02 * [2, 3, 5, 14];
-    filename = 'shock_polar_equilwithions_ar_SDToolbox';
-    % Tunning parameters
-    tolN = 1e-14;
-    N_points_polar = 300;
-    % Combustion Toolbox
-    results_CT = run_CT('ProblemType', 'SHOCK_POLAR',...
-                        'Species', LS,...
-                        'S_Oxidizer', Oxidizer,...
-                        'N_Oxidizer', moles,...
-                        'u1', u1,...
-                        'tolN', tolN,...
-                        'N_points_polar', N_points_polar);
-    problems_solved = get_problems_solved(results_CT.PS.strP, 'polar', 'theta');
+    % Get Nasa database
+    DB = NasaDatabase('FLAG_BENCHMARK', FLAG_BENCHMARK);
+    
+    % Define chemical system
+    system = ChemicalSystem(DB, listSpecies);
+    
+    % Initialize mixture
+    mix = Mixture(system);
+    
+    % Define chemical state
+    set(mix, {'N2', 'O2', 'Ar'}, [78, 21, 1] / 21);
+    
+    % Define properties
+    mixArray1 = setProperties(mix, 'temperature', 300, 'pressure', 1, 'M1', M1);
+    
+    % Initialize solver
+    solver = ShockSolver('problemType', 'SHOCK_POLAR', 'numPointsPolar', numPointsPolar, 'FLAG_RESULTS', false);
+    
+    % Solve problem
+    [mixArray1, mixArray2] = solver.solveArray(mixArray1);
+    
+    if FLAG_BENCHMARK
+        return
+    end
+
     % Load results SDToolbox 
-    results_SDToolbox = load_struct(filename, 'data');
-    % Display validation (plot)
-    [fig1, fig2] = plot_validation_shock_polar_SDToolbox(results_CT, results_SDToolbox, results_CT.Misc.config);
+    resultsSDToolbox = load_struct(filename, 'data');
+
+    % Plot polars
+    [ax1, ax2] = plot_validation_shock_polar_SDToolbox(mixArray1, mixArray2, resultsSDToolbox, PlotConfig());
+
     % Save plots
-    folderpath = strcat(pwd,'\Validations\Figures\');
+    folderpath = strcat(fullfile(pwd, 'validations', 'figures'));
     stack_trace = dbstack;
     filename = stack_trace.name;
-    saveas(fig1, strcat(folderpath, filename, '_properties_1'), 'svg');
-    saveas(fig2, strcat(folderpath, filename, '_properties_2'), 'svg');
+    saveas(ax1, fullfile(folderpath, strcat(filename, '_properties_1')), 'svg');
+    saveas(ax2, fullfile(folderpath, strcat(filename, '_properties_2')), 'svg');
 end

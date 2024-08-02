ab7e158715c1196f403bc4f87128dde007f406a4
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-02 21:52:15 +0200

    Update: rename variables and functions

--- gui/utils/gui_update_UITable_R.m
+++ gui/utils/gui_update_UITable_R.m
@@ -8,12 +8,12 @@ function gui_update_UITable_R(app)
     molar_fraction = moles / sum(moles);
 
     % Catalog chemical species (fuel, oxidizer, or inert)
-    typeSpecies = get_typeSpecies(app.mixture);
+    typeSpecies = getTypeSpecies(app.mixture);
 
     if ~isempty(app.UITable_R.Data) && numSpecies == length(app.UITable_R.Data(:, 1))
         temperatures = app.UITable_R.Data(:, 5)';
     else
-        temperatures = create_cell_ntimes(app.mixture.T, numSpecies);
+        temperatures = repmat({app.mixture.T}, [1, numSpecies]);
     end
 
     % Check if is a condensed species with a fixed temperatures

be76578baaf42c590a61744a09e342c4b34488a1
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-03 11:11:37 +0200

    Add: calculations for RocketSolver class
    Former-commit-id: 285a84c9c427a8db5912bac888f2504284fd5a04

--- gui/utils/gui_write_results.m
+++ gui/utils/gui_write_results.m
@@ -39,44 +39,21 @@ function update_properties(app, results, i)
         if contains(results(i).ProblemType, 'OBLIQUE', 'IgnoreCase', true)
             update_properties_oblique(app, mix2, '_2');
         end
-
+        
         if contains(results(i).ProblemType, 'ROCKET', 'IgnoreCase', true)
-            app.text_Aratio_2.Value = mix2.Aratio;
+            % IAC: Throat   |   FAC: Combustor end (c)
+            mix3 = results(i).mix3;
             
-            if isfield(results(i), 'mix2_c')
-                % Combustor end (c)
-                mix3 = results(i).mix2_c;
-                FLAG_IAC = false;
-            else
-                % Throat
-                mix3 = results(i).mix3;
-                FLAG_IAC = true;
-            end
-            
+            % Update values
             update_properties_common(app, mix3, '_3');
             update_properties_velocities(app, mix3, '_3')
             update_properties_rocket(app, mix3, '_3')
-            
-            if ~FLAG_IAC || (~isempty(results(i).strP) && FLAG_IAC)
-                
-                if ~FLAG_IAC
-                    % Throat
-                    mix4 = results(i).mix3;
-                else
-                    % Exit
-                    mix4 = results(i).strP;
-                end
 
-                % Update values
-                update_properties_common(app, mix4, '_4');
-                update_properties_velocities(app, mix4, '_4')
-                update_properties_rocket(app, mix4, '_4')
-
-                % Get max error method
-                app.text_error_problem.Value = max([mix2.error_problem, mix3.error_problem, mix4.error_problem]);
-
+            if isfield(results(i), 'mix4')
+                % IAC: Exit   |   FAC: Throat
+                mix4 = results(i).mix4;
             else
-                % Exit
+                % IAC: Exit
                 app.text_T_4.Value = 0;
                 app.text_p_4.Value = 0;
                 app.text_r_4.Value = 0;
@@ -93,24 +70,20 @@ function update_properties(app, results, i)
                 app.text_Cstar_4.Value = 0;
                 app.text_Ivac_4.Value = 0;
                 app.text_Isp_4.Value = 0;
-            end
-            
-            if FLAG_IAC
+
+                % Get max error method
+                app.text_error_problem.Value = max([mix2.errorProblem, mix3.errorProblem]);
                 return
             end
 
-            if ~isempty(results(i).strP)
-                % Exit
-                mix5 = results(i).strP;
-                
-                % Update values
-                update_properties_common(app, mix5, '_5');
-                update_properties_velocities(app, mix5, '_5')
-                update_properties_rocket(app, mix5, '_5')
-
-                % Get max error method
-                app.text_error_problem.Value = max([mix2.error_problem, mix3.error_problem, mix4.error_problem, mix5.error_problem]);
+            % Update values
+            update_properties_common(app, mix4, '_4');
+            update_properties_velocities(app, mix4, '_4')
+            update_properties_rocket(app, mix4, '_4')
             
+            % FAC: Exit
+            if isfield(results(i), 'mix5')
+                mix5 = results(i).mix5;
             else
                 app.text_T_5.Value = 0;
                 app.text_p_5.Value = 0;
@@ -130,10 +103,19 @@ function update_properties(app, results, i)
                 app.text_Isp_5.Value = 0;
 
                 % Get max error method
-                app.text_error_problem.Value = max([mix2.error_problem, mix3.error_problem, mix4.error_problem]);
+                app.text_error_problem.Value = max([mix2.errorProblem, mix3.errorProblem, mix4.errorProblem]);
+                return
             end
 
+            % Update values
+            update_properties_common(app, mix5, '_5');
+            update_properties_velocities(app, mix5, '_5')
+            update_properties_rocket(app, mix5, '_5')
+
+            % Get max error method
+            app.text_error_problem.Value = max([mix2.errorProblem, mix3.errorProblem, mix4.errorProblem, mix5.errorProblem]);
         end
+
     end
 
 end

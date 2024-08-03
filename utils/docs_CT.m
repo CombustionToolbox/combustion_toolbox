12dcde04aa7696dc53f31a0f9903e863468f3978
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-03 17:06:10 +0200

    Update: remove old functions
    Former-commit-id: 23246572146e5f200e390e166ede5e7f4f639c04
    Former-commit-id: c95276b5030c8eca76622a5dfb7a2105b4c8b319

--- utils/docs_CT.m
+++
@@ -1,5 +0,0 @@
-function docs_CT()
-    % Open Combustion Toolbox's documentation in default web browser
-
-    system('start https://combustion-toolbox-website.readthedocs.io/en/latest/documentation/');
-end

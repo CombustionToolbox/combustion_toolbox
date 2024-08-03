71240a8c08a799bc268e22dba2000105e1dd08c7
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-03 15:07:42 +0200

    Update: remove old
    Former-commit-id: 7bced55d212d5180a51dfeef668438dc5d052977

--- gui/utils/gui_edit_phiValueChanged.m
+++ gui/utils/gui_edit_phiValueChanged.m
@@ -55,22 +55,6 @@ function gui_edit_phiValueChanged(app, event)
         % Check List of products (only in case of ListProducts == Complete reaction)
         check_ListProducts(app, equivalenceRatio);
 
-        % % Create temporaly app required for preliminary calculations
-        % temp_app = gui_create_temp_app(app, event, app.FLAG_EQUIVALENCE_RATIO);
-        % 
-        % % Update UITable classes
-        % gui_update_UITable_R(app, temp_app);
-        % app.UITable_P.Data = app.UITable_R.Data(:, 1);    % (species, numer of moles, mole fractions, temperature)
-        % app.UITable_R2.Data = app.UITable_R.Data(:, 1:3); % (species, numer of moles, mole fractions)
-        % 
-        % % Update GUI: equivalence ratio, O/F, percentage Fuel, and
-        % % temperature of reactants
-        % app.edit_phi2.Value = app.edit_phi.Value;
-        % app.edit_phi3.Value = app.edit_phi.Value;
-        % app.edit_OF.Value = temp_app.PS.strR{1}.OF;
-        % app.edit_F.Value = temp_app.PS.strR{1}.percentage_Fuel;
-        % app.PR1.Value = sprintf('%g', round(temp_app.PD.TR.value, 2));
-
     catch ME
         message = {sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
         ME.stack(1).name, ME.stack(1).line, ME.message)};

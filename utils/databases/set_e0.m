b7942420f9a11f771af287d2291d457795e02d66
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-11-09 17:43:44 +0100

    Update: miscellaneous - these function will be removed in a future update

--- utils/databases/set_e0.m
+++ utils/databases/set_e0.m
@@ -1,9 +1,9 @@
-function e0 = set_e0(LS, T, DB)
+function e0 = set_e0(listSpecies, T, DB)
     % Function that computes the vector of internal energy for the given
     % set of species [J/mol]
     %
     % Args:
-    %     LS (cell): List of species
+    %     listSpecies (cell): List of species
     %     T (float): Temperature [K]
     %     DB (struct): Database with custom thermodynamic polynomials functions generated from NASAs 9 polynomials fits
     %
@@ -13,8 +13,8 @@ function e0 = set_e0(LS, T, DB)
     % Example:
     %     e0 = set_e0({'H2O', 'CO2'}, 298.15, DB)
 
-    for i = length(LS):-1:1
-        species = LS{i};
+    for i = length(listSpecies):-1:1
+        species = listSpecies{i};
         e0(i, 1) = species_DeT(species, T, DB) - DB.(species).ef;
     end
 

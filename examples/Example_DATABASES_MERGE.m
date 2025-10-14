76ddc392b64a8c7b877e79e2b6bf15784551c821
AlbertoCuadra <acuadra@ing.uc3m.es>
2025-10-14 16:55:33 +0200

    Add: include example of merging two databases

---
+++ examples/Example_DATABASES_MERGE.m
@@ -0,0 +1,26 @@
+% -------------------------------------------------------------------------
+% EXAMPLE: DATABASES MERGE
+%
+% Example of merging two databases. In this case, a NASA database and a
+% Burcat database using NASA9 format, which has been previously converted
+% using the static method thermoMillennium2thermoNASA9 from the Burcat
+% class
+%
+% @author: Alberto Cuadra Lara
+%                 
+% Last update October 14 2025
+% -------------------------------------------------------------------------
+
+% Import packages
+import combustiontoolbox.databases.*
+import combustiontoolbox.core.*
+import combustiontoolbox.equilibrium.*
+
+% Get Nasa database
+DB_NASA = NasaDatabase('thermoFile', 'thermo_NASA.inp');
+
+% Generate Burcat database using NASA9 format
+DB_BURCAT = NasaDatabase('thermoFile', 'thermo_millennium_2_thermoNASA9.inp');
+
+% Merge databases
+DB = DB_NASA + DB_BURCAT;

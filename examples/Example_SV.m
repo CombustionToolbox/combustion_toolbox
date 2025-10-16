55c1c13db6c340ea7e450d0673a15480ba709933
AlbertoCuadra <acuadra@ing.uc3m.es>
2025-10-16 12:49:00 +0200

    Update: modify Example_SP and Example_SV

--- examples/Example_SV.m
+++ examples/Example_SV.m
@@ -1,15 +1,16 @@
 % -------------------------------------------------------------------------
 % EXAMPLE: SV
 % Compute Isentropic compression/expansion and equilibrium composition at 
-% a defined set of specific volume (0.5, 2) m3/kg for a lean CH4-air mixture at
-% an initial temperature 700 K, and a equivalence ratio phi 0.5 [-]
+% a defined set of specific volume (vSpecific = 0.5:2 m3/kg) for a lean 
+% CH4-air mixture at defined specific entropy, and an equivalence ratio
+% phi 0.5 [-]
 %   
 % See wiki or setListspecies method from ChemicalSystem class for predefined
 % sets of species
 %
 % @author: Alberto Cuadra Lara
 %                 
-% Last update October 06 2025
+% Last update October 16 2025
 % -------------------------------------------------------------------------
 
 % Import packages
@@ -31,7 +32,7 @@ set(mix, {'CH4'}, 'fuel', 1);
 set(mix, {'N2', 'O2', 'Ar', 'CO2'}, 'oxidizer', [78.084, 20.9476, 0.9365, 0.0319] / 20.9476);
 
 % Define properties
-mixArray = setProperties(mix, 'temperature', 700, 'volume', 0.5:0.01:2, 'equivalenceRatio', 0.5);
+mixArray = setProperties(mix, 'entropySpecific', mix.sSpecific, 'volume', 0.5:0.01:2, 'equivalenceRatio', 0.5);
 
 % Initialize solver
 solver = EquilibriumSolver('problemType', 'SV');

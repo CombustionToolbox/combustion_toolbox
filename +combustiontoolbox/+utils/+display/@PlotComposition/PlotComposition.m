7e5d94e9a66a516e8c51cb3d75aeeeb03843183e
AlbertoCuadra <acuadra@ing.uc3m.es>
2025-12-16 12:10:12 +0100

    Add: include new display classes `Canvas`, `PlotComposition`, and `PlotFigure`

---
+++ +combustiontoolbox/+utils/+display/@PlotComposition/PlotComposition.m
@@ -0,0 +1,317 @@
+classdef PlotComposition < combustiontoolbox.utils.display.Canvas
+    % The :mat:class:`PlotComposition` class extends :mat:class:`Canvas` to generate
+    % species composition plots (e.g., molar fractions `Xi`) against an independent variable.
+    %
+    % A :mat:class:`PlotComposition` object can be initialized as follows: ::
+    %
+    %      config = combustiontoolbox.utils.display.PlotConfig();
+    %      fig = combustiontoolbox.utils.display.PlotComposition(config);
+    %      fig.plot(mixtureArray, 'equivalenceRatio', 'Xi');
+    %
+    % This creates a formatted figure with species composition plotted over the selected parameter.
+    %
+    % See also: :mat:class:`Canvas`, :mat:class:`PlotFigure`, :mat:class:`PlotConfig`
+
+    properties (Access = private)
+        FLAG_PLOT_VALIDATION = false; % Flag to indicate to plot validation data
+    end
+
+    methods
+
+        function obj = PlotComposition(config)
+            % Constructor
+
+            if nargin < 1
+                % Default values
+                defaultInnerPosition = [0.15 0.05 0.7 0.9];
+                defaultOuterPosition = [0.15 0.05 0.7 0.9];
+
+                % Get default configuration
+                config = combustiontoolbox.utils.display.PlotConfig();
+                config.innerposition = defaultInnerPosition;
+                config.outerposition = defaultOuterPosition;
+            end
+
+            obj@combustiontoolbox.utils.display.Canvas(config);
+        end
+
+        function [ax, fig] = plot(obj, mixture, x_var, x_field, y_field, varargin)
+            % Plot molar fractions againts any variable
+            %
+            % Args:
+            %     obj (PlotComposition): Instance of the PlotComposition class
+            %     mixture (Mixture): Data of the mixture, conditions, and databases
+            %     x_var (cell): Properties of the mixture for all the cases
+            %     x_field (char): Field name for the x-axis data
+            %     y_field (char): Field name for the y-axis data
+            %
+            % Optional Name-Value Pair Args:
+            %     * validation (struct): Struct that contains validations with (x_field, y_field)
+            %     * nfrec (float): Frequency points to plot validations
+            %     * mintol (float): Minimum limit i-axis with the composition of the mixture
+            %     * displaySpecies (cell): List of species to plot
+            %     * y_var (cell): Get y-axis data from a different mixture
+            %     * config (struct): Struct with the configuration for plots
+            %     * axis_x (char): Set x-axis limits
+            %     * axis_y (char): Set y-axis limits
+            %     * xscale (char): Set x-axis scale (linear or log)
+            %     * yscale (char): Set y-axis scale (linear or log)
+            %     * xdir (char): Set x-axis direction (normal or reverse)
+            %     * ydir (char): Set y-axis direction (normal or reverse)
+            %     * ax (object): Handle of the axes to plot on
+            %
+            % Returns:
+            %     Tuple containing
+            %
+            %     * ax (object): Handle of the axes
+            %     * fig (object): Handle of the figure
+            %
+            % Examples:
+            %     * [ax, fig] = plotComposition(mixArray(1), mixArray, 'equivalenceRatio', 'Xi')
+            %     * [ax, fig] = plotComposition(mixArray(1), mixArray, 'equivalenceRatio', 'Xi', 'y_var', mixArray2)
+            %     * [ax, fig] = plotComposition(mixArray(1), mixArray, 'equivalenceRatio', 'Xi', 'y_var', mixArray2, 'validation', resultsCEA)
+            %     * [ax, fig] = plotComposition(mixArray(1), mixArray, 'equivalenceRatio', 'Xi', 'y_var', mixArray2, 'validation', resultsCEA, 'displaySpecies', displaySpecies)
+            
+            % Import packages
+            import combustiontoolbox.utils.findIndex
+            import combustiontoolbox.utils.display.*
+            
+            % Initialization
+            config = obj.config;
+            mintolDisplay = config.mintolDisplay;
+            displaySpecies = config.displaySpecies;
+
+            % Default values
+            ax = [];
+            results2 = [];
+            nfrec = 1;
+            config.labelx = interpreterLabel(x_field, config.label_type);
+            config.labely = interpreterLabel(y_field, config.label_type);
+            config.yscale = 'log';
+            y_var = x_var;
+
+            % Unpack
+            for i = 1:2:nargin - 5
+
+                switch lower(varargin{i})
+                    case {'validation', 'results'}
+                        results2 = varargin{i + 1};
+                        obj.FLAG_PLOT_VALIDATION = true;
+                    case {'nfrec'}
+                        nfrec = varargin{i + 1};
+                    case {'mintol', 'mintol_display', 'toln'}
+                        mintolDisplay = varargin{i + 1};
+                    case {'ls', 'species', 'displayspecies', 'display species', 'display_species'}
+                        displaySpecies = varargin{i + 1};
+                    case {'y', 'y_var', 'yvar', 'y var', 'y_data', 'ydata', 'y data'}
+                        y_var = varargin{i + 1};
+                    case {'config'}
+                        config = varargin{i + 1};
+                    case {'xscale'}
+                        config.xscale = varargin{i + 1};
+                    case {'yscale'}
+                        config.yscale = varargin{i + 1};
+                    case {'xdir'}
+                        config.xdir = varargin{i + 1};
+                    case {'ydir'}
+                        config.ydir = varargin{i + 1};
+                    case {'title'}
+                        config.title = varargin{i + 1};
+                    case {'ax', 'axes'}
+                        ax = varargin{i + 1};
+                end
+
+            end
+            
+            % Set species to displaySpecies
+            [species, listSpecies] = obj.getDisplaySpecies(mixture, displaySpecies);
+
+            % Read data
+            FLAG_Y_AXIS = strcmpi(y_field, 'Xi');
+
+            if isfloat(x_var) && isfloat(y_var)
+                mix1.(x_field) = x_var;
+                mix1.(y_field) = y_var;
+            elseif ~(iscell(x_var) && iscell(y_var))
+                mix1.(x_field) = [x_var.(x_field)];
+                mix1.(y_field) = [y_var.(y_field)];
+            else
+                mix1.(x_field) = cell2vector(x_var, x_field);
+                mix1.(y_field) = cell2vector(y_var, y_field);
+            end
+            
+            % Check lenghts
+            if length(mix1.(x_field)) < 2
+                return
+            end
+
+            % Get index species
+            index_species_CT = findIndex(listSpecies, species);
+
+            % Remove species that do not appear
+            [species, index_species_CT] = obj.cleanDisplaySpecies(mix1.Xi, listSpecies, index_species_CT, mintolDisplay, obj.FLAG_PLOT_VALIDATION);
+
+            % Set figure
+            if isempty(ax)
+                ax = obj.getAxis();
+            end
+
+            fig = gcf;
+
+            % Set axis limits
+            if FLAG_Y_AXIS
+                xlim(ax, [min(mix1.(x_field)), max(mix1.(x_field))])
+                ylim(ax, [mintolDisplay, 1])
+            else
+                xlim(ax, [mintolDisplay, 1])
+                ylim(ax, [min(mix1.(y_field)), max(mix1.(y_field))])
+            end
+
+            % Set default style
+            NE = length(species);
+            maxLdisplay = config.colorpaletteLenght;
+
+            if NE > maxLdisplay
+                numColors = maxLdisplay;
+            else
+                numColors = NE;
+            end
+
+            colorbw = combustiontoolbox.utils.extensions.brewermap(numColors, config.colorpalette);
+
+            % Plot main results
+            k = 1;
+            z = 1;
+
+            for i = 1:length(species)
+
+                if FLAG_Y_AXIS
+                    plot(ax, mix1.(x_field), mix1.(y_field)(index_species_CT(i), :), 'LineWidth', config.linewidth, 'color', colorbw(k, :), 'LineStyle', config.lineStyles{z});
+                else
+                    plot(ax, mix1.(x_field)(index_species_CT(i), :), mix1.(y_field), 'LineWidth', config.linewidth, 'color', colorbw(k, :), 'LineStyle', config.lineStyles{z});
+                end
+
+                k = k + 1;
+
+                if k == maxLdisplay
+                    k = 1;
+                    z = z + 1;
+
+                    if z > config.numStyles
+                        z = 1;
+                    end
+
+                end
+
+            end
+
+            % Plot validations
+            if obj.FLAG_PLOT_VALIDATION
+
+                if iscell(results2)
+                    mix2.(x_field) = cell2vector(results2, x_field);
+                    mix2.(y_field) = cell2vector(results2, y_field);
+                else
+                    mix2 = results2;
+                end
+
+                k = 1;
+                z = 1;
+
+                for i = 1:length(species)
+
+                    if FLAG_Y_AXIS
+                        plot(ax, mix2.(x_field)(1:nfrec:end), mix2.(y_field)(i, 1:nfrec:end), config.symbolStyles{z}, 'LineWidth', config.linewidth, 'color', colorbw(k, :), 'MarkerFaceColor', 'white');
+                    else
+                        plot(ax, mix2.(x_field)(i, 1:nfrec:end), mix2.(y_field)(1:nfrec:end), config.symbolStyles{z}, 'LineWidth', config.linewidth, 'color', colorbw(k, :), 'MarkerFaceColor', 'white');
+                    end
+
+                    k = k + 1;
+
+                    if k == maxLdisplay
+                        k = 1;
+                        z = z + 1;
+
+                        if z > config.numStyles
+                            z = 1;
+                        end
+
+                    end
+
+                end
+
+                % Plot symbols
+                k = 1;
+                z = 1;
+                h = zeros(1, length(species));
+
+                for i = 1:length(species)
+                    h(i) = plot(ax, NaN, NaN, [config.lineStyles{z}, config.symbolStyles{z}], 'LineWidth', config.linewidth, 'color', colorbw(k, :), 'MarkerFaceColor', 'auto');
+                    k = k + 1;
+
+                    if k == maxLdisplay
+                        k = 1;
+                        z = z + 1;
+
+                        if z > config.numStyles
+                            z = 1;
+                        end
+
+                    end
+
+                end
+
+            else
+                h = ax;
+            end
+
+            % Set legend
+            for i = length(species):-1:1
+                legendname{i} = species2latex(species{i});
+            end
+
+            legend(h, legendname, 'FontSize', config.fontsize - 4, 'Location', 'northeastoutside', 'interpreter', 'latex');
+            
+            % Set title
+            if isempty(config.title)
+                config.title = getTitle(mixture);
+            else
+                config.title = sprintf('%s - %s', config.title, getTitle(mixture));
+            end
+            
+            title(config.title, 'Interpreter', 'latex', 'FontSize', config.fontsize + 4);
+        end
+
+    end
+
+    methods (Access = private, Static)
+
+        function [species, listSpecies] = getDisplaySpecies(mixture, displaySpecies)
+            listSpecies = mixture.chemicalSystem.listSpecies;
+
+            if isempty(displaySpecies)
+                species = mixture.chemicalSystem.listProducts;
+            else
+                species = displaySpecies;
+            end
+
+        end
+
+        function [species, index_pass] = cleanDisplaySpecies(molar_fractions, species, index_species, mintolDisplay, FLAG_PLOT_VALIDATION)
+            % Remove species that do not appear
+
+            % Checks
+            if FLAG_PLOT_VALIDATION
+                mintolDisplay = 0;
+            end
+
+            index_all_pass = find(any(molar_fractions > mintolDisplay, 2));
+            FLAG_PASS = ismember(index_species, index_all_pass);
+            % Update species
+            index_pass = index_species(FLAG_PASS);
+            species = species(index_pass);
+        end
+
+    end
+
+end
